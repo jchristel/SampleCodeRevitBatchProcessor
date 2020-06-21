c8971aacaf9c5ec5434f603ebe8d350ca3355cc3
Jan <jan.r.christel@gmail.com>
2020-06-22 08:50:33 +1000

    Create ReportMatts_executeThis.py

---
+++ ReportMatts_executeThis.py
@@ -0,0 +1,122 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#License:
+#
+#
+# Revit Batch Processor Sample Code
+#
+# Copyright (c) 2020  Jan Christel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+import clr
+import System
+
+# flag whether this runs in debug or not
+debug = False
+
+# --------------------------
+#default file path locations
+# --------------------------
+#store output here:
+rootPath = r'C:\temp'
+#path to Common.py
+commonlibraryDebugLocation = r'C:\temp'
+#debug mode revit project file name
+debugRevitFileName = r'C:\temp\Test.rvt'
+
+# Add batch processor scripting references
+if not debug:
+    import revit_script_util
+    from revit_script_util import Output
+    import revit_file_util
+    clr.AddReference('RevitAPI')
+    clr.AddReference('RevitAPIUI')
+    doc = revit_script_util.GetScriptDocument()
+    revitFilePath = revit_script_util.GetRevitFilePath()
+else:
+    #set path to common library
+    import sys
+    sys.path.append(commonlibraryDebugLocation)
+    #get default revit file name
+    revitFilePath = debugRevitFileName
+
+clr.AddReference('System.Core')
+clr.ImportExtensions(System.Linq)
+
+#import common library
+import Common
+from Common import *
+
+from Autodesk.Revit.DB import *
+
+#output messages either to batch processor (debug = False) or console (debug = True)
+def Output(message = ''):
+    if not debug:
+        revit_script_util.Output(str(message))
+    else:
+        print (message)
+
+# -------------
+# my code here:
+# -------------
+def WriteType (action, description, fileName, doc):
+    status = True
+    collector = action()
+    print ('Writing ' + description +'....')
+    f = open(fileName, 'w')
+    try:
+        f.write('Materials...start'+ '\n')
+        for wt in collector:
+            try:
+                paras = wt.GetOrderedParameters()
+                for p in paras:
+                    paraName = p.Definition.Name
+                    pValue = 'no Value'
+                    if(p.StorageType == StorageType.ElementId or p.StorageType == StorageType.Double or p.StorageType == StorageType.Integer):
+                        if(p.AsValueString()!= None and p.AsValueString() != ''):
+                            pValue = p.AsValueString()
+                    elif(p.StorageType == StorageType.String):
+                        if(p.AsString() != None and p.AsString() != ''):
+                            pValue = p.AsString()                    
+                    f.write('\t'.join([fileName, EncodeAscii(Element.Name.GetValue(wt)), EncodeAscii(paraName), EncodeAscii(pValue), '\n']))
+            except Exception as e:
+                Output('Failed to get material data')
+                f.write(f.write('\t'.join([fileName, EncodeAscii(Element.Name.GetValue(wt)),'\n']))
+    except:
+        status = False
+        Output('Failed to write data file!' + fileName)
+    f.write('Materials...end')
+    f.close()
+    return status
+
+#gets all materials in a model
+def actionMat():  
+    collector = FilteredElementCollector(doc)
+    collector.OfClass(Material)
+    return collector
+
+# -------------
+# main:
+# -------------
+
+#build output file name
+fileName = rootPath + '\\'+ GetOutPutFileName(revitFilePath)
+
+Output('Writing Material Data.... start')
+result = WriteType (actionMat, 'Materials', fileName, doc)
+Output('Writing Material Data.... status: ' + str(result))

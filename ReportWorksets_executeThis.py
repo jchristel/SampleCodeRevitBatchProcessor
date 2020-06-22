4f2de4b87a4654b54447a7b09510e60c96d73440
Jan Christel <jan.r.christel@gmail.com>
2020-06-23 09:29:34 +1000

    Created ReportWorksets_ExecuteThis.py

---
+++ ReportWorksets_executeThis.py
@@ -0,0 +1,106 @@
+﻿#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+#License:
+#
+#
+# Revit Batch Processor Sample Code
+#
+# Copyright (c) 2020  Jan Christel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+import clr
+import System
+
+# flag whether this runs in debug or not
+debug = True
+
+# --------------------------
+#default file path locations
+# --------------------------
+#store output here:
+rootPath = r'C:\temp'
+#path to Common.py
+commonlibraryDebugLocation = r'C:\Project\Git\RB'
+#debug mode revit project file name
+debugRevitFileName = r'C:\temp\Test_worksets.rvt'
+
+# Add batch processor scripting references
+if not debug:
+    import revit_script_util
+    import revit_file_util
+    clr.AddReference('RevitAPI')
+    clr.AddReference('RevitAPIUI')
+     # NOTE: these only make sense for batch Revit file processing mode.
+    doc = revit_script_util.GetScriptDocument()
+    revitFilePath = revit_script_util.GetRevitFilePath()
+else:
+    #set path to common library
+    import sys
+    sys.path.append(commonlibraryDebugLocation)
+    #get default revit file name
+    revitFilePath = debugRevitFileName
+
+#import common library
+import Common
+from Common import *
+
+clr.AddReference('System.Core')
+clr.ImportExtensions(System.Linq)
+
+from Autodesk.Revit.DB import *
+
+#output messages either to batch processor (debug = False) or console (debug = True)
+def Output(message = ''):
+    if not debug:
+        revit_script_util.Output(str(message))
+    else:
+        print (message)
+
+# -------------
+# my code here:
+# -------------
+
+#build output file name
+fileName = rootPath + '\\'+ GetOutPutFileName(revitFilePath)
+
+#method writing out shared parameter information
+def writeWorksetData(doc, fileName):
+    status = True
+    try:
+        f = open(fileName, 'w')
+        f.write('\t'.join(['ID', 'NAME', '\n']))
+        for p in FilteredWorksetCollector(doc).OfKind(WorksetKind.UserWorkset):
+            f.write('\t'.join([str(p.Id.IntegerValue), EncodeAscii(p.Name), '\n']))
+        f.close()
+    except Exception as e:
+        status = False
+        Output('Failed to write data file!' + fileName)
+        Output (str(e))
+    return status
+
+# -------------
+# main:
+# -------------
+
+Output('Writing Workset Data.... start')
+
+#write out shared parameter data
+result = writeWorksetData(doc, fileName)
+
+Output('Writing Workset Data.... status: ' + str(result))
+Output('Writing Workset Data.... finished ' + fileName)

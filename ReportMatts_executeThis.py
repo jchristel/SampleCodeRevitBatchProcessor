9f4d1adf16178db4b96e7f0379c4e19c264bfd2f
Jan Christel <jan.r.christel@gmail.com>
2020-07-15 20:01:14 +1000

    Update ReportMatts_executeThis.py

--- ReportMatts_executeThis.py
+++ ReportMatts_executeThis.py
@@ -21,7 +21,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#
+
+# sample description
+# how to report on materials
 
 import clr
 import System

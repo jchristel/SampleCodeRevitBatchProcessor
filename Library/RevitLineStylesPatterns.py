0106749fea0e24b1d4e2be18b4887e39b0808291
Jan <jan.r.christel@gmail.com>
2022-05-17 20:43:01 +1000

    Update RevitLineStylesPatterns.py
    added docstring

--- Library/RevitLineStylesPatterns.py
+++ Library/RevitLineStylesPatterns.py
@@ -38,22 +38,61 @@ clr.ImportExtensions(System.Linq)
 
 # ------------------------------------------------ DELETE LINE PATTERNS ----------------------------------------------
 
-# deletes all line patterns where the names contains a provided string
 def DeleteLinePatternsContains(doc, contains):
+    '''
+    Deletes all line patterns where the names contains a provided string
+
+    :param doc: Current Revit model document.
+    :type doc: Autodesk.Revit.DB.Document
+    :param contains: Filter: pattern name needs to contain this tring to be deleted.
+    :type contains: str
+
+    :return: Result class instance.
+           .result = True if line pattern where deleted succesfully. Otherwise False.
+           .message will contain delete status per pattern.
+    :rtype: SampleCodeBatchProcessor.Result
+    '''
+
     lps = rdb.FilteredElementCollector(doc).OfClass(rdb.LinePatternElement).ToList()
     ids = list(lp.Id for lp in lps if lp.GetLinePattern().Name.Contains(contains)).ToList[rdb.ElementId]()
     result = com.DeleteByElementIds(doc,ids, 'Deleting line patterns where name contains: ' + str(contains),'line patterns containing: ' + str(contains))
     return result
 
-# deletes all line patterns where the name starts with provided string
 def DeleteLinePatternStartsWith(doc, startsWith):
+    '''
+    Deletes all line patterns where the name starts with provided string.
+
+    :param doc: Current Revit model document.
+    :type doc: Autodesk.Revit.DB.Document
+    :param startsWith: Filter: pattern name needs to start with this string to be deleted.
+    :type startsWith: str
+
+    :return: Result class instance.
+           .result = True if line pattern where deleted succesfully. Otherwise False.
+           .message will contain delete status per pattern.
+    :rtype: SampleCodeBatchProcessor.Result
+    '''
+
     lps = rdb.FilteredElementCollector(doc).OfClass(rdb.LinePatternElement).ToList()
     ids = list(lp.Id for lp in lps if lp.GetLinePattern().Name.StartsWith(startsWith)).ToList[rdb.ElementId]()
     result = com.DeleteByElementIds(doc,ids, 'Delete line patterns where name starts with: ' + str(startsWith),'line patterns starting with: ' + str(startsWith))
     return result
 
-# deletes all line patterns where the name does not contain the provided string
 def DeleteLinePatternsWithout(doc, contains):
+    '''
+    Deletes all line patterns where the name does not contain the provided string.
+
+    :param doc: Current Revit model document.
+    :type doc: Autodesk.Revit.DB.Document
+    :param contains: Filter: pattern name needs not to contain this string to be deleted.
+    :type contains: str
+
+    :return: Result class instance.
+           .result = True if line pattern where deleted succesfully. Otherwise False.
+           .message will contain delete status per pattern.
+    :rtype: SampleCodeBatchProcessor.Result
+    '''
+
     lps = rdb.FilteredElementCollector(doc).OfClass(rdb.LinePatternElement).ToList()
     ids = list(lp.Id for lp in lps).ToList[rdb.ElementId]()
     idsContain = list(lp.Id for lp in lps if lp.GetLinePattern().Name.Contains(contains)).ToList[rdb.ElementId]()
@@ -61,15 +100,29 @@ def DeleteLinePatternsWithout(doc, contains):
     result = com.DeleteByElementIds(doc,deleteids, 'Delete line patterns where name does not contain: ' + str(contains),'line patterns without: ' + str(contains))
     return result
 
-# return all line patterns in the model
-# doc:      curren document
 def GetAllLinePatterns(doc):
+    '''
+    Gets all line patterns in the model.
+
+    :param doc: _description_
+    :type doc: _type_
+
+    :return: List of all line pattern elements in model.
+    :rtype: list of Autodesk.Revit.DB.LinePatternElement
+    '''
     return rdb.FilteredElementCollector(doc).OfClass(rdb.LinePatternElement).ToList()
 
-
-# doc     current model document
 def BuildPatternsDictionaryByName(doc):
-    '''builds a dictionary wher line pattern name is key, values are all ids of line patterns with the exact same name'''
+    '''
+    Returns a dictionary where line pattern name is key, values are all ids of line patterns with the exact same name.
+
+    :param doc: Current Revit model document.
+    :type doc: Autodesk.Revit.DB.Document
+
+    :return: A dictionary where line pattern name is key, values are all ids of line patterns with the exact same name
+    :rtype: dictinonary(key str, value list of Autodesk.Revit.DB.ElementId)
+    '''
+
     lpDic = {}
     lps = rdb.FilteredElementCollector(doc).OfClass(rdb.LinePatternElement)
     for lp in lps:
@@ -79,9 +132,19 @@ def BuildPatternsDictionaryByName(doc):
             lpDic[lp.GetLinePattern().Name] = [lp.Id]
     return lpDic
 
-# doc     current model document
 def DeleteDuplicatLinePatterNames(doc):
-    '''deletes all but the first line pattern by Id with the exact same name'''
+    '''
+    Deletes all but the first line pattern by Id with the exact same name.
+
+    :param doc: Current Revit model document.
+    :type doc: Autodesk.Revit.DB.Document
+
+    :return: Result class instance.
+           .result = True if all views where deleted. Otherwise False.
+           .message will contain deletion status.
+    :rtype: SampleCodeBatchProcessor.Result
+    '''
+
     returnvalue = res.Result()
     returnvalue.AppendMessage('Deletes all but the first line pattern by Id with the exact same name...start')
     # get a dictionary: Key pattern name, value all ids of line patterns with the same name
@@ -97,23 +160,52 @@ def DeleteDuplicatLinePatterNames(doc):
 
 # ------------------------------------------------ DELETE LINE STYLES ----------------------------------------------
 
-# deletes all line styles where the name starts with provided string
 def DeleteLineStylesStartsWith(doc, startsWith):
+    '''
+    Deletes all line styles where the name starts with provided string
+
+    :param doc: Current Revit model document.
+    :type doc: Autodesk.Revit.DB.Document
+    :param startsWith: Filter: style name needs to start with this string to be deleted.
+    :type startsWith: str
+
+    :return: Result class instance.
+           .result = True if all views where deleted. Otherwise False.
+           .message will contain deletion status.
+    :rtype: SampleCodeBatchProcessor.Result
+    '''
+
     lc = doc.Settings.Categories[rdb.BuiltInCategory.OST_Lines]
     ids = list(c.Id for c in lc.SubCategories if c.Name.StartsWith(startsWith)).ToList[rdb.ElementId]()
     result = com.DeleteByElementIds(doc,ids, 'Delete line styles where name starts with: ' + str(startsWith),'line styles starting with: ' + str(startsWith))
     return result
 
-# return all line styles ids in the model
-# doc:      curren document
 def GetAllLineStyleIds(doc):
+    '''
+    Gets all line styles ids in the model.
+
+    :param doc: Current Revit model document.
+    :type doc: Autodesk.Revit.DB.Document
+
+    :return: A list of all line style ids.
+    :rtype: list of Autodesk.Revit.DB.ElementId
+    '''
+
     lc = doc.Settings.Categories[rdb.BuiltInCategory.OST_Lines]
     ids = list(c.Id for c in lc.SubCategories).ToList[rdb.ElementId]()
     return ids
 
 # ------------------------------------------------ Fill Patterns ----------------------------------------------
 
-# return all fill pattern ids in the model
-# doc:      curren document
 def GetAllFillPattern(doc):
+    '''
+    Gets all fill pattern element ids in the model.
+
+    :param doc: Current Revit model document.
+    :type doc: Autodesk.Revit.DB.Document
+
+    :return: A list of all fill pattern elements.
+    :rtype: list of Autodesk.Revit.DB.FillPatternElement
+    '''
+
     return rdb.FilteredElementCollector(doc).OfClass(rdb.FillPatternElement).ToList()

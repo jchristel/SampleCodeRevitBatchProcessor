35845e6f56bb3415b53b156c6bdd85745218d21d
Jan <jan.r.christel@gmail.com>
2022-05-18 21:05:56 +1000

    Update RevitCeilings.py
    added docstring

--- Library/RevitCeilings.py
+++ Library/RevitCeilings.py
@@ -40,7 +40,7 @@ import Autodesk.Revit.DB as rdb
 clr.ImportExtensions(System.Linq)
 
 # -------------------------------------------- common variables --------------------
-# header used in reports
+#: header used in reports
 REPORT_CEILINGS_HEADER = ['HOSTFILE', 'CEILINGTYPEID', 'CEILINGTYPENAME']
 
 COMPOUND_CEILING_FAMILY_NAME = 'Compound Ceiling'

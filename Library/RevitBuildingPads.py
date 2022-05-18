4f56094523ed73e17674aed4aad660a1684e7a49
Jan <jan.r.christel@gmail.com>
2022-05-18 21:06:04 +1000

    Update RevitBuildingPads.py
    added docstring

--- Library/RevitBuildingPads.py
+++ Library/RevitBuildingPads.py
@@ -38,7 +38,7 @@ import Autodesk.Revit.DB as rdb
 clr.ImportExtensions(System.Linq)
 
 # -------------------------------------------- common variables --------------------
-# header used in reports
+#: header used in reports
 REPORT_BUILDINGPAD_HEADER = ['HOSTFILE', 'BUILDINGPADTYPEID', 'BUILDINGPADTYPENAME']
 
 BASIC_BUILDINGPAD_FAMILY_NAME = 'Pad'

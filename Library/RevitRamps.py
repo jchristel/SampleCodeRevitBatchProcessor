f86613277e996528504ebb7488fdea057158ae63
Jan <jan.r.christel@gmail.com>
2022-05-25 18:07:31 +1000

    Update RevitRamps.py
    added docstring

--- Library/RevitRamps.py
+++ Library/RevitRamps.py
@@ -1,5 +1,7 @@
 ﻿'''
-This module contains a number of helper functions relating to Revit ramps. 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+This module contains a number of helper functions relating to Revit ramps.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 '''
 #
 #License:
@@ -29,11 +31,7 @@ import System
 
 import RevitCommonAPI as com
 
-BASIC_RAMP_FAMILY_NAME = 'Ramp'
 
-BUILTIN_RAMP_TYPE_FAMILY_NAMES = [
-    BASIC_RAMP_FAMILY_NAME,
-]
 
 # import Autodesk
 import Autodesk.Revit.DB as rdb
@@ -41,24 +39,48 @@ import Autodesk.Revit.DB as rdb
 clr.ImportExtensions(System.Linq)
 
 # -------------------------------------------- common variables --------------------
-# header used in reports
+#: header used in reports
 REPORT_RAMPS_HEADER = ['HOSTFILE', 'RAMPTYPEID', 'RAMPTYPENAME']
+
+
+#: Built in ramp family name for ramp
+BASIC_RAMP_FAMILY_NAME = 'Ramp'
+#: List of all built in ramp family names
+BUILTIN_RAMP_TYPE_FAMILY_NAMES = [
+    BASIC_RAMP_FAMILY_NAME,
+]
 
 # --------------------------------------------- utility functions ------------------
 
-# doc:   current model document
 def GetAllRampTypesByCategory(doc):
-    ''' this will return a filtered element collector of all Ramp types in the model:
-    - Ramp
     '''
+    Gets a filtered element collector of all Ramp types in the model.
+
+    :param doc: _description_
+    :type doc: _type_
+
+    :return: A filtered element collector containing ramp types.
+    :rtype: Autodesk.Revit.DB.FilteredElementCollector
+    '''
+
     collector = rdb.FilteredElementCollector(doc).OfCategory(rdb.BuiltInCategory.OST_Ramps).WhereElementIsElementType()
     return collector
 
-
-# collector   fltered element collector containing Ramp type elments of family symbols representing in place families
-# dic         dictionary containing key: ramp type family name, value: list of ids
 def BuildRampTypeDictionary(collector, dic):
-    '''returns the dictioanry passt in with keys and or values added retrieved from collector passt in'''
+    '''
+    Returns the dictionary passt in with keys and or values added retrieved from collector passt in.
+
+    TODO: similar function exists in Walls module. Consider more generic function.
+
+    :param collector: A filtered element collector containing ramp type elments of family symbols representing in place families
+    :type collector: Autodesk.Revit.DB.FilteredElementCollector
+    :param dic: dictionary containing key: ramp type family name, value: list of ids
+    :type dic: Dictionary {str:[Autodesk.Revit.DB.ElementId]}
+
+    :return: A dictionary where key is the family name and values are ids of types belonging to that family.
+    :rtype: Dictionary {str:[Autodesk.Revit.DB.ElementId]}
+    '''
+
     for c in collector:
         if(dic.has_key(c.FamilyName)):
             if(c.Id not in dic[c.FamilyName]):
@@ -67,8 +89,19 @@ def BuildRampTypeDictionary(collector, dic):
             dic[c.FamilyName] = [c.Id]
     return dic
 
-# doc   current model document
 def SortRampTypesByFamilyName(doc):
+    '''
+    Returns a dictionary where key is the family name and values are ids of types belonging to that family.
+
+    TODO: similar function exists in Walls module. Consider more generic function.
+
+    :param doc: Current Revit model document.
+    :type doc: Autodesk.Revit.DB.Document
+
+    :return: A dictionary where key is the family name and values are ids of types belonging to that family.
+    :rtype: Dictionary {str:[Autodesk.Revit.DB.ElementId]}
+    '''
+
     # get all ramp types including in place ramp families
     wts_two = GetAllRampTypesByCategory(doc)
     usedWts = {}
@@ -77,29 +110,67 @@ def SortRampTypesByFamilyName(doc):
 
 # -------------------------------- none in place Ramp types -------------------------------------------------------
 
-# doc   current model document
 def GetAllRampInstancesInModelByCategory(doc):
-    ''' returns all Ramp elements placed in model...ignores in place families'''
+    '''
+    Gets all ramp elements placed in model...ignores in place families (to be confirmed!)
+
+    :param doc: Current Revit model document.
+    :type doc: Autodesk.Revit.DB.Document
+
+    :return: A filtered element collector containing ramp instances.
+    :rtype: Autodesk.Revit.DB.FilteredElementCollector
+    '''
+
     return rdb.FilteredElementCollector(doc).OfCategory(rdb.BuiltInCategory.OST_Ramps).WhereElementIsNotElementType()
 
-# doc   current model document
 def GetAllRampTypeIdsInModelByCategory(doc):
-    ''' returns all Ramp element type ids by category available in model '''
+    '''
+    Gets all ramp element type ids available in model.
+
+    :param doc: Current Revit model document.
+    :type doc: Autodesk.Revit.DB.Document
+
+    :return: List of element ids of ramp types.
+    :rtype: List Autodesk.Revit.DB.ElementId
+    '''
+
     ids = []
     colCat = GetAllRampTypesByCategory(doc)
     ids = com.GetIdsFromElementCollector (colCat)
     return ids
 
-# doc   current document
 def GetUsedRampTypeIds(doc):
-    ''' returns all used in Ramp type ids '''
+    '''
+    Gets all used ramp element type ids available in model.
+
+    Used: at least one instance of this type is placed in the model.
+
+    :param doc: Current Revit model document.
+    :type doc: Autodesk.Revit.DB.Document
+
+    :return: List of element ids of ramp types.
+    :rtype: List Autodesk.Revit.DB.ElementId
+    '''
+
     ids = com.GetUsedUnusedTypeIds(doc, GetAllRampTypeIdsInModelByCategory, 1, 4)
     return ids
 
-# famTypeIds        symbol(type) ids of a family
-# usedTypeIds       symbol(type) ids in use in a project
 def FamilyNoTypesInUse(famTypeIds,unUsedTypeIds):
-    ''' returns false if any symbols (types) of a family are in use in a model'''
+    '''
+    Compares two lists of element ids and returnds False if any element id in first list is not in the second list.
+    
+    Returns False if any symbols (types) of a family (first lists) are in use in a model (second list).
+    TODO: repetetive code...Consider generic function!
+
+    :param famTypeIds: List of family symbols (types).
+    :type famTypeIds: List of Autodesk.Revit.DB.ElementId
+    :param unUsedTypeIds: List of unused family symbols (types)
+    :type unUsedTypeIds: List of Autodesk.Revit.DB.ElementId
+
+    :return: True if all ids in first list are also in second list, otherwise False.
+    :rtype: bool
+    '''
+
     match = True
     for famTypeId in famTypeIds:
         if (famTypeId not in unUsedTypeIds):
@@ -107,11 +178,27 @@ def FamilyNoTypesInUse(famTypeIds,unUsedTypeIds):
             break
     return match
  
-# doc   current document
 def GetUnusedNonInPlaceRampTypeIdsToPurge(doc):
-    ''' returns all unused Ramp type ids for:
-    - Ramp 
-    ramps do not have in place families ...'''
+    '''
+    Gets all unused ramp type ids in the model.
+
+    This method can be used to safely delete unused ramp types. In the case that no ramp\
+        instance using any of the types is placed, this will return all but one type id since\
+        Revit requires at least one ramp type definition to be in the model.
+
+    Unused: Not one instance of this type is placed in the model.
+
+    - Ramp
+
+    It will therefore not return any in place family types. (No such thing in Revit?)
+
+    :param doc: Current Revit model document.
+    :type doc: Autodesk.Revit.DB.Document
+
+    :return: List of element ids of roof types.
+    :rtype: List Autodesk.Revit.DB.ElementId
+    '''
+
     # get unused type ids
     ids = com.GetUsedUnusedTypeIds(doc, GetAllRampTypeIdsInModelByCategory, 0, 4)
     # make sure there is at least on Ramp type per system family left in model

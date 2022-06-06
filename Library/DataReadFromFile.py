775cf220afbb3d7af89589913171daa2a22e292e
Jan <jan.r.christel@gmail.com>
2022-06-06 20:37:35 +1000

    Update DataReadFromFile.py
    updated docstring

--- Library/DataReadFromFile.py
+++ Library/DataReadFromFile.py
@@ -1,3 +1,9 @@
+'''
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Data storage reader class.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+'''
+
 #
 #License:
 #
@@ -32,20 +38,27 @@ import DataRoom as dr
 class ReadDataFromFile:
     def __init__(self, filePath):
         '''
-        _summary_
+        Class constructor.
 
-        :param filePath: _description_
-        :type filePath: _type_
+        :param filePath: Fully qualified file path to json formatted data file.
+        :type filePath: str
         '''
+
         self.dataFilePath = filePath
         self.dataType = ''
         self.data = []
 
-    # filePath      fully qualified file path to tab separated file
     def _read_tab_separated_file(self, filePath):
         '''
-        read a tab delimited files into a list of rows
+        Read a tab delimited files into a list of rows
+
+        :param filePath: Fully qualified file path to tab separated file.
+        :type filePath: str
+
+        :return: List of List [str] representing rows and columns in text file.
+        :rtype: list[list[str]]
         '''
+
         rowList = []
         try:
             with codecs.open (filePath,'r',encoding='utf-8') as f:
@@ -60,8 +73,15 @@ class ReadDataFromFile:
 
     def load_Data(self):
         '''
-        load json formatted rows into data objects
+        Load json formatted rows into data objects and stores them in this class.
+
+        In the moment the following data objects are supported:
+
+        - :class: `.DataRoom`
+        - :class: `.DataCeiling`
+
         '''
+
         dataJson = self._read_tab_separated_file(self.dataFilePath)
         dataObjects = []
         for d in dataJson:
@@ -82,13 +102,30 @@ class ReadDataFromFile:
     
     def get_data_by_level(self, levelName):
         '''
-        returns all data objects where level name equals passt in value
+        Returns all data objects where level name equals passt in value.
+
+        :param levelName: The building level name.
+        :type levelName: str
+
+        :return: A list of room and ceiling data objects
+        :rtype: list [data objects]
         '''
+
         return (list(filter(lambda x: (x.levelName == levelName ) , self.data)))
     
     def get_data_by_level_and_dataType(self, levelName, dataType):
         '''
-        returns all data objects where level name and datattype equal passt in values
+        Returns all data objects where level name and datat type equal passt in values.
+
+        :param levelName: The building level name.
+        :type levelName: str
+        :param dataType: A string describing the data type\
+            refer to property .dataType on data object class
+        :type dataType: str
+
+        :return: A list of data objects
+        :rtype: list [data objects]
         '''
+
         return (list(filter(lambda x: (x.levelName == levelName and x.dataType == dataType), self.data)))
 

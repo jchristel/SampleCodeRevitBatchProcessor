9849c512d19470f579d73862a3089b636d60aff1
Jan <jan.r.christel@gmail.com>
2023-02-21 20:11:06 +1100

    updated readne abd setup.py
    Hopefully this will show the readme on pypi next time around

--- duHast/setup.py
+++ duHast/setup.py
@@ -3,9 +3,16 @@
 
 from setuptools import setup
 from setuptools import find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 setup(
     # ...
     packages=find_packages(),
-    package_dir={"": "src"}
+    package_dir={"": "src"},
     # ...
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )

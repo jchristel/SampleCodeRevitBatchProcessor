b1035dc8ed58f62aaffed99460fd7f920541153e
Jan Christel <jan.r.christel@gmail.com>
2023-03-31 09:27:28 +1100

    added stump for utilities modules
    in views name space

---
+++ duHast/src/duHast/APISamples/Views/Utility/DataCategoryOverride.py
@@ -0,0 +1,59 @@
+'''
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Category override data storage class.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+'''
+#
+#License:
+#
+#
+# Revit Batch Processor Sample Code
+#
+# Copyright (c) 2022  Jan Christel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+import json
+from duHast.Utilities import Base
+
+class DataCategoryOverride(Base.Base):
+
+    def __init__(self, data_type, j = {}):
+        '''
+        Class constructor
+
+        :param j:  json formatted dictionary of this class, defaults to {}
+        :type j: dict, optional
+        '''
+
+        # store data type  in base class
+        super(DataCategoryOverride, self).__init__()
+        
+        # check if any data was past in with constructor!
+        if(j != None and len(j) > 0 ):
+            # check type of data that came in: 
+            if(type(j) == str):
+                # a string
+                j = json.loads(j)
+            elif(type(j) == dict):
+                # no action required
+                pass
+            else:
+                print('j', j)
+                raise  ValueError ('Argument supplied must be of type string or type dictionary')
+            
+        else:
+            pass

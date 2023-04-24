b83c54abbfece119f25401b8f9185ed80b4e6a74
Jan <jan.r.christel@gmail.com>
2023-04-24 19:59:02 +1000

    Update files_get.py

--- test/Utility/files_get.py
+++ test/Utility/files_get.py
@@ -11,10 +11,17 @@ from duHast.Utilities.files_get import get_files_from_directory_walker_with_filt
 
 
 def test_get_files_from_directory_walker_with_filters(tmpdir):
+    '''
+    get_files_from_directory_walker_with_filters test
+
+    :param tmpdir: temp directory
+    :type tmpdir: str
+    :return: True if all tests past, otherwise False
+    :rtype: _bool
+    '''
+    
     flag = True
     try:
-        # Create a temporary directory for testing
-        test_dir = tmpdir.mkdir("test_dir")
         # Create some files with different prefixes, suffixes, and extensions
         test_files = [
             "file1_prefix_suffix.csv",
@@ -29,40 +36,46 @@ def test_get_files_from_directory_walker_with_filters(tmpdir):
             "file10_prefix_suffix.jpg",
         ]
         for file_name in test_files:
-            file_path = test_dir.join(file_name)
-            file_path.write("test content")
+            file_path = os.path.join(tmp_dir, file_name)
+            with open(file_path, "w") as f1:
+                f1.write("test content")
 
         # Set up the input parameters for the function
-        folder_path = str(test_dir)
+        folder_path = tmpdir
         file_prefix = "prefix"
         file_suffix = "suffix"
         file_extension = ".csv"
 
         # Call the function to get the actual output
-        actual_output = get_files_from_directory_walker_with_filters(
+        result = get_files_from_directory_walker_with_filters(
             folder_path, file_prefix, file_suffix, file_extension
         )
 
         # Check the actual output matches the expected output
-        expected_output = [
+        expected_result = [
             os.path.join(folder_path, "prefix_file5_suffix.csv"),
             os.path.join(folder_path, "prefix_file9_suffix.csv"),
         ]
-        assert sorted(actual_output) == sorted(expected_output)
 
-        # Remove the temporary directory and files
-        shutil.rmtree(str(test_dir))
-    except Excption as e:
+        assert sorted(result) == sorted(expected_result)
+
+    except Exception as e:
         flag = False
         print ('An exception occurred in function test_get_files_from_directory_walker_with_filters {}'.format(e))
     return flag
 
 def test_get_files_with_filter(tmpdir):
-    
+    '''
+    get_files_with_filter test
+
+    :param tmpdir: temp directory
+    :type tmpdir: str
+    :return: True if all tests past, otherwise False
+    :rtype: _bool
+    '''
+
     flag = True
     try:
-        # Create a temporary directory for testing
-        test_dir = tmpdir.mkdir("test_dir")
         # Create some files with different extensions and names
         test_files = [
             "file1.rvt",
@@ -73,27 +86,28 @@ def test_get_files_with_filter(tmpdir):
             "something2.rvt",
             "nothing.txt",
         ]
+
         for file_name in test_files:
-            file_path = test_dir.join(file_name)
-            file_path.write("test content")
+            file_path = os.path.join(tmp_dir, file_name)
+            with open(file_path, "w") as f1:
+                f1.write("test content")
 
         # Set up the input parameters for the function
-        folder_path = str(test_dir)
+        folder_path = tmpdir
         file_extension = ".rvt"
         filter = "something*"
 
         # Call the function to get the actual output
-        actual_output = get_files_with_filter(folder_path, file_extension, filter)
+        result = get_files_with_filter(folder_path, file_extension, filter)
 
         # Check the actual output matches the expected output
-        expected_output = [
+        expected_result = [
             os.path.join(folder_path, "something1.rvt"),
             os.path.join(folder_path, "something2.rvt"),
         ]
-        assert sorted(actual_output) == sorted(expected_output)
-
-        # Remove the temporary directory and files
-        shutil.rmtree(str(test_dir))
+        
+        assert sorted(result) == sorted(expected_result)
+       
     except Exception as e:
         flag = False
         print ('An exception occurred in function test_get_files_with_filter {}'.format(e))

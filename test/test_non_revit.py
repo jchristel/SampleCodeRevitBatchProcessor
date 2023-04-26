7f33516f3f6a9f01f43faa1a901ecf4ea962b34b
Jan <jan.r.christel@gmail.com>
2023-04-26 19:52:07 +1000

    Update test_non_revit.py
    formatting updated, docs updated

--- test/test_non_revit.py
+++ test/test_non_revit.py
@@ -9,31 +9,46 @@ import datetime
 #: overall debug flag. If False no messages from tests will be printed to console. If True messages will be printed.
 IS_DEBUG = False
 
+
 def _date_time():
-    '''
+    """
     Get the current time string in format 2022-08-09 19:09:19 :
 
     :return: current date and time.
     :rtype: str
-    '''
+    """
 
     d = datetime.datetime.now()
-    timestamp = d.strftime('%y-%m-%d %H_%M_%S : ')
-    return timestamp 
+    timestamp = d.strftime("%y-%m-%d %H_%M_%S : ")
+    return timestamp
 
-def _pad_string(message, padding_length = 70):
-    if (len(message) < padding_length):
+
+def _pad_string(message, padding_length=70):
+    """
+    Pads a string message to be formatted: left hand side message, right hand side status (if any)
+    Maximum length 70 characters (excludes time stamp!)
+    If message is longer then 70-2 characters it will be returned un-changed.
+
+    :param message: The message to be padded
+    :type message: str
+    :param padding_length: Length of message string after padding, defaults to 70
+    :type padding_length: int, optional
+    :return: Padded message
+    :rtype: str
+    """
+
+    if len(message) < padding_length:
         status_length = 0
-        status = ''
-        if("[False]" in message):
+        status = ""
+        if "[False]" in message:
             status_length = len("[False]")
             status = "[False]"
-        elif("[True]" in message):
+        elif "[True]" in message:
             status_length = len("[True]")
             status = "[True]"
-        if(status_length > 0):
+        if status_length > 0:
             message_left = message[:-status_length]
-            message_left = message_left.ljust(padding_length-status_length, ".")
+            message_left = message_left.ljust(padding_length - status_length, ".")
             message = message_left + status
             return message
         else:
@@ -41,8 +56,9 @@ def _pad_string(message, padding_length = 70):
     else:
         return message
 
-def _header(header_name, padding_length = 70):
-    '''
+
+def _header(header_name, padding_length=70):
+    """
     Prints a padded header row to console.
     Header will be padded equally to both sides with '-' characters.
 
@@ -50,18 +66,24 @@ def _header(header_name, padding_length = 70):
     :type header_name: str
     :param padding_length: The overall row length, defaults to 70
     :type padding_length: int, optional
-    '''
+    """
 
-    if(padding_length > len(header_name) + 2):
+    if padding_length > len(header_name) + 2:
         sides = (padding_length - len(header_name)) // 2
-        print('\n' + _date_time() + '-'.ljust(sides,"-") + header_name + '-'.ljust(sides,"-"))
+        print(
+            "\n"
+            + _date_time()
+            + "-".ljust(sides, "-")
+            + header_name
+            + "-".ljust(sides, "-")
+        )
     else:
-        print('\n' + header_name)
+        print("\n" + header_name)
 
 
-def output(message = ''):
-    '''
-    Print message to console. 
+def output(message=""):
+    """
+    Print message to console.
 
     Note:
 
@@ -71,29 +93,32 @@ def output(message = ''):
 
     :param message: The message, defaults to ''
     :type message: str, optional
-    '''
+    """
 
     # make sure message is a string:
-    if(type(message) != str):
+    if type(message) != str:
         message = str(message)
 
     timestamp = _date_time()
 
     # check for multi row messages
-    if('/n' in message):
-        message_chunks = message.split('\n')
+    if "/n" in message:
+        message_chunks = message.split("\n")
         for message_chunk in message_chunks:
-            if('False' in message_chunk):
-                print (Fore.RED + '{} {}'.format(timestamp,  _pad_string(message_chunk)))
-            elif('True' in message_chunk):
-                print (Fore.GREEN + '{} {}'.format(timestamp, _pad_string(message_chunk)))
+            if "False" in message_chunk:
+                print(Fore.RED + "{} {}".format(timestamp, _pad_string(message_chunk)))
+            elif "True" in message_chunk:
+                print(
+                    Fore.GREEN + "{} {}".format(timestamp, _pad_string(message_chunk))
+                )
             else:
-                print ('{} {}'.format(timestamp, message_chunk))
+                print("{} {}".format(timestamp, message_chunk))
     else:
-        print ('{} {}'.format(timestamp,_pad_string(message)))
+        print("{} {}".format(timestamp, _pad_string(message)))
+
 
 def out(func_name, result_flag, message):
-    '''
+    """
     Output to console
 
     :param func_name: the function name to be printed
@@ -102,11 +127,12 @@ def out(func_name, result_flag, message):
     :type result_flag: bool
     :param message: Any messages from the test function to be printed in debug mode.
     :type message: str
-    '''
+    """
 
-    output('{} [{}]'.format(func_name, result_flag))
-    if(IS_DEBUG):
+    output("{} [{}]".format(func_name, result_flag))
+    if IS_DEBUG:
         output(message)
+
 
 #: colour coding for console output
 init()
@@ -116,25 +142,25 @@ OVERALL_STATUS = True
 
 
 # run tests in all modules
-_header('date_stamps')
+_header("date_stamps")
 status = date_stamps.run_tests(output=out)
 OVERALL_STATUS = OVERALL_STATUS & status
-out('date_stamps completed with status',status,'')
+out("date_stamps completed with status", status, "")
 
-_header('files_get')
+_header("files_get")
 status = files_get.run_tests(output=out)
 OVERALL_STATUS = OVERALL_STATUS & status
-out('files_get completed with status' ,status,'')
+out("files_get completed with status", status, "")
 
-_header('files_io')
+_header("files_io")
 status = files_io.run_tests(output=out)
 OVERALL_STATUS = OVERALL_STATUS & status
-out('files_io completed with status',status,'')
+out("files_io completed with status", status, "")
 
-_header('utility')
+_header("utility")
 status = utility.run_tests(output=out)
 OVERALL_STATUS = OVERALL_STATUS & status
-out('utility completed with status',status,'')
+out("utility completed with status", status, "")
 
-_header('FINISHED')
-print('All tests completed with [{}]'.format(OVERALL_STATUS))
+_header("FINISHED")
+print("All tests completed with [{}]".format(OVERALL_STATUS))

039fb25ea9f3cbda6f60c641aec461bca82a66c3
Jan Christel <jan.r.christel@gmail.com>
2023-04-30 15:54:41 +1000

    Update run_tests.py

--- test/Revit/Revision/run_tests.py
+++ test/Revit/Revision/run_tests.py
@@ -4,7 +4,10 @@ SAMPLES_PATH = (
     r"C:\Users\jchristel\Documents\GitHub\SampleCodeRevitBatchProcessor\duHast\src"
 )
 TEST_PATH = r"C:\Users\jchristel\Documents\GitHub\SampleCodeRevitBatchProcessor"
-sys.path += [SAMPLES_PATH, TEST_PATH]
+TEST_PATH_TEST = (
+    r"C:\Users\jchristel\Documents\GitHub\SampleCodeRevitBatchProcessor\test"
+)
+sys.path += [SAMPLES_PATH, TEST_PATH, TEST_PATH_TEST]
 
 
 from test.utils.rbp_setup import add_rbp_ref, output as rbp_out
@@ -12,7 +15,7 @@ from test.utils.rbp_setup import add_rbp_ref, output as rbp_out
 from test.Revit.Revision import revision as revTest
 from test.Revit.Revision import sequence as seqTest
 
-from test.utils.output import pad_header
+from test.utils.padding import pad_header_no_time_stamp, pad_string
 
 #: Type of test run flag. If False run in revit python shell. If True runs in revit batch processor.
 IS_RBP_RUN = False
@@ -30,23 +33,36 @@ def run_revision_tests(doc, rbp_run_type=IS_RBP_RUN):
     """
 
     output_function = out
-    if IS_RBP_RUN == True:
+    output_function_header = out_header
+    if rbp_run_type == True:
         # add revit batch processor references and get the current document
         doc = add_rbp_ref()
         output_function = rbp_out
+        output_function_header = rbp_out
 
     #: overall test status
     overall_status = True
 
     # start tests
-    output_function(pad_header('Revisions'))
+    output_function_header(pad_header_no_time_stamp("Revisions"))
     result = revTest.run_tests(doc, output_function)
     overall_status = overall_status & result
-    output_function(pad_header('Revision Sequence'))
+    output_function_header(pad_header_no_time_stamp("Revision Sequence"))
     result = seqTest.run_tests(doc, output_function)
     overall_status = overall_status & result
 
     return overall_status
+
+
+def out_header(header):
+    """
+    Just prints header.
+
+    :param header: A header line.
+    :type header: str
+    """
+
+    print(header)
 
 
 def out(func_name, result_flag, message):
@@ -61,9 +77,10 @@ def out(func_name, result_flag, message):
     :type message: str
     """
 
-    print("{} [{}]".format(func_name, result_flag))
+    message = "{} [{}]".format(func_name, result_flag)
+    print(pad_string(message))
     if IS_RBP_RUN:
-        print(message)
+        print(pad_string(message))
 
 
 if __name__ == "__main__":

6cd953f5b479fcc2120633fe8daf40748dfb9915
Jan <jan.r.christel@gmail.com>
2023-04-30 15:19:31 +1000

    Update run_tests.py

--- test/Revit/Revision/run_tests.py
+++ test/Revit/Revision/run_tests.py
@@ -1,45 +1,70 @@
 import sys
-SAMPLES_PATH = r'C:\Users\jchristel\Documents\GitHub\SampleCodeRevitBatchProcessor\duHast\src'
-TEST_PATH = r'C:\Users\jchristel\Documents\GitHub\SampleCodeRevitBatchProcessor'
+
+SAMPLES_PATH = (
+    r"C:\Users\jchristel\Documents\GitHub\SampleCodeRevitBatchProcessor\duHast\src"
+)
+TEST_PATH = r"C:\Users\jchristel\Documents\GitHub\SampleCodeRevitBatchProcessor"
 sys.path += [SAMPLES_PATH, TEST_PATH]
 
 
-from test.utils.rbp_setup import add_rbp_ref, output
+from test.utils.rbp_setup import add_rbp_ref, output as rbp_out
 
 from test.Revit.Revision import revision as revTest
-from  test.Revit.Revision import sequence as seqTest
+from test.Revit.Revision import sequence as seqTest
+
+from test.utils.output import pad_header
+
+#: Type of test run flag. If False run in revit python shell. If True runs in revit batch processor.
+IS_RBP_RUN = False
 
 
-#: overall debug flag. If False no messages from tests will be printed to console. If True messages will be printed.
-IS_DEBUG = True
+def run_revision_tests(doc, rbp_run_type=IS_RBP_RUN):
+    """
+    Runs all revision related tests.
 
-def run_revision_tests(doc):
+    :param doc: Current Revit document.
+    :type doc: Autodesk.Revit.DB.Document
 
-    # add revit batch processor references and get the current document
-    doc = add_rbp_ref()
+    :return: True if all tests completed successfully, otherwise False.
+    :rtype: bool
+    """
+
+    output_function = out
+    if IS_RBP_RUN == True:
+        # add revit batch processor references and get the current document
+        doc = add_rbp_ref()
+        output_function = rbp_out
 
     #: overall test status
     overall_status = True
 
     # start tests
-    result = revTest.run_tests(doc, output)
-    overall_status = overall_status  & result
-    result = seqTest.run_tests(doc, output)
-    overall_status = overall_status  & result
+    output_function(pad_header('Revisions'))
+    result = revTest.run_tests(doc, output_function)
+    overall_status = overall_status & result
+    output_function(pad_header('Revision Sequence'))
+    result = seqTest.run_tests(doc, output_function)
+    overall_status = overall_status & result
 
     return overall_status
 
+
 def out(func_name, result_flag, message):
-    print ("{} [{}]".format(func_name, result_flag))
-    if IS_DEBUG:
+    """
+    print messages to screen when debugging
+
+    :param func_name: The function tested name.
+    :type func_name: str
+    :param result_flag: True if test completed successfully, otherwise False
+    :type result_flag: bool
+    :param message: Any debug message coming back from the test function.
+    :type message: str
+    """
+
+    print("{} [{}]".format(func_name, result_flag))
+    if IS_RBP_RUN:
         print(message)
 
-if __name__ == "__main__":
-    #: overall test status
-    overall_status = True
 
-    # start tests
-    result = revTest.run_tests(doc, out)
-    overall_status = overall_status  & result
-    result = seqTest.run_tests(doc, out)
-    overall_status = overall_status  & result
+if __name__ == "__main__":
+    run_revision_tests(doc)

236d4d56377b7ab1ba793a0eb8b71fbf42b1aa1a
Jan <jan.r.christel@gmail.com>
2023-05-04 08:37:37 +1000

    added csv text file related tests

--- test/Utility/temp_files_dirs.py
+++ test/Utility/temp_files_dirs.py
@@ -31,3 +31,21 @@ def write_test_files(file_names, tmp_dir):
         file_path = os.path.join(tmp_dir, file_name)
         with open(file_path, "w") as f1:
             f1.write("test content")
+
+
+def write_file_with_data(file_name, tmp_dir, data):
+    """
+    Function writing out a text file with given data.
+
+    :param file_name: The file name.
+    :type file_name: str
+    :param tmp_dir: The directory path.
+    :type tmp_dir: str
+    :param data: data to be written to file
+    :type data: [str]
+    """
+
+    with open(os.path.join(tmp_dir, file_name), "w") as f:
+        for d in data:
+            f.write(d + "\n")
+        f.close()

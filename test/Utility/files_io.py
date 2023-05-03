e6286939dac8be905c49f1e82e5281b4957c1d65
Jan Christel <jan.r.christel@gmail.com>
2023-05-03 20:23:20 +1000

    Update files_io.py
    tests added

--- test/Utility/files_io.py
+++ test/Utility/files_io.py
@@ -13,6 +13,8 @@ from Utility.temp_files_dirs import write_test_files, call_with_temp_directory
 from duHast.Utilities.files_io import (
     file_exist,
     file_delete,
+    get_directory_path_from_file_path,
+    rename_file,
     get_file_name_without_ext,
     get_file_size,
     FILE_SIZE_IN_KB,
@@ -22,6 +24,14 @@ from duHast.Utilities.files_io import (
 
 
 def test_file_exist(tmp_dir):
+    """
+    Test file_exist.
+
+    :param tmpdir: temp directory
+    :type tmpdir: str
+    :return: True if all tests past, otherwise False
+    :rtype: bool
+    """
     flag = True
     message = "-"
     try:
@@ -49,6 +59,15 @@ def test_file_exist(tmp_dir):
 
 
 def test_file_delete(tmp_dir):
+    """
+    file_delete test
+
+    :param tmpdir: temp directory
+    :type tmpdir: str
+    :return: True if all tests past, otherwise False
+    :rtype: bool
+    """
+
     flag = True
     message = "-"
     try:
@@ -64,7 +83,7 @@ def test_file_delete(tmp_dir):
         expected_result = True
         expected_result_check = file_exist(os.path.join(tmp_dir, test_files[0]))
         message = "File deleted: {} vs  expected: {} vs file check {}".format(
-            result, expected_result, expected_result_check
+            result, expected_result, not expected_result_check
         )
         assert result == expected_result
         # file exist should have returned a false!
@@ -79,6 +98,149 @@ def test_file_delete(tmp_dir):
         )
     return flag, message
 
+
+def test_get_directory_path_from_file_path():
+    """
+    get_directory_path_from_file_path test
+
+    :param tmpdir: temp directory
+    :type tmpdir: str
+    :return: True if all tests past, otherwise False
+    :rtype: bool
+    """
+
+    flag = True
+    message = "-"
+    try:
+        # test data
+        test_files = [
+            ["/path/to/file.txt", "/path/to"],
+            ["invalid/file/path", "invalid/file"],
+            ["/path/to/directory/", "/path/to/directory"],
+        ]
+
+        # test valid file path
+        result = get_directory_path_from_file_path(test_files[0][0])
+        expected_result = test_files[0][1]
+        message = " {} vs {}".format(result, expected_result)
+        assert result == expected_result
+
+        # tet invalid file path
+        result = get_directory_path_from_file_path(test_files[1][0])
+        expected_result = test_files[1][1]
+        message = message + "\n" + " {} vs {}".format(result, expected_result)
+        assert result == expected_result
+
+        # test directory path
+        result = get_directory_path_from_file_path(test_files[2][0])
+        expected_result = test_files[2][1]
+        message = message + "\n" + " {} vs {}".format(result, expected_result)
+        assert result == expected_result
+
+    except Exception as e:
+        flag = False
+        message = (
+            message
+            + "\n"
+            + (
+                "An exception occurred in function test_get_directory_path_from_file_path {}".format(
+                    e
+                )
+            )
+        )
+    return flag, message
+
+
+def test_rename_file(tmp_dir):
+    """
+    rename_files test
+
+    :param tmpdir: temp directory
+    :type tmpdir: str
+    :return: True if all tests past, otherwise False
+    :rtype: bool
+    """
+
+    flag = True
+    message = "-"
+    try:
+        # test data
+        test_files = [
+            ["file.txt", "file_after.txt"],
+            ["file_2.text", "file_after.txt"],
+            ["file_2.text", ""],
+        ]
+
+        test_file_to_be_written = list(entry[0] for entry in test_files)
+        # write out test files
+        write_test_files(test_file_to_be_written, tmp_dir)
+
+        # test valid scenario
+        result = rename_file(
+            os.path.join(tmp_dir, test_files[0][0]),
+            os.path.join(tmp_dir, test_files[0][1]),
+        )
+        result_file_exist = file_exist(os.path.join(tmp_dir, test_files[0][1]))
+        message = (
+            "rename result: {} vs expected result: {} vs file check result: {}".format(
+                result, True, result_file_exist
+            )
+        )
+        assert result == True
+        assert result == result_file_exist
+
+        result = rename_file(
+            os.path.join(tmp_dir, test_files[1][0]),
+            os.path.join(tmp_dir, test_files[1][1]),
+        )
+        message = message + "\n" + " {} vs {}".format(result, False)
+        assert result == False
+
+        result = rename_file(
+            os.path.join(tmp_dir, test_files[2][0]),
+            os.path.join(tmp_dir, test_files[2][1]),
+        )
+        message = message + "\n" + " {} vs {}".format(result, False)
+        assert result == False
+
+        # check non existing source file
+        result = rename_file(
+            os.path.join(tmp_dir, "not here.txt"),
+            os.path.join(tmp_dir, test_files[2][1]),
+        )
+        message = message + "\n" + " {} vs {}".format(result, False)
+        assert result == False
+
+    except Exception as e:
+        flag = False
+        message = (
+            message
+            + "\n"
+            + (
+                "An exception occurred in function test_get_directory_path_from_file_path {}".format(
+                    e
+                )
+            )
+        )
+    return flag, message
+
+def test_copy_file(tmp_dir):
+    flag = True
+    message = "-"
+    try:
+        # test data
+        test_files = [
+            "test_file_size.txt",
+        ]
+    
+    except Exception as e:
+        flag = False
+        message = (
+            message
+            + "\n"
+            + ("An exception occurred in function test_copy_file {}".format(e))
+        )
+    return flag, message
 
 def test_file_size(tmp_dir):
     """
@@ -110,19 +272,19 @@ def test_file_size(tmp_dir):
 
         # Test file size in KB
         result = get_file_size(file_path, unit=FILE_SIZE_IN_KB)
-        expected_result = file_size / FILE_SIZE_IN_KB
+        expected_result = file_size / 1024
         message = message + "\n" + (" {} vs {}".format(result, expected_result))
         assert expected_result == result
 
         # Test file size in MB
         result = get_file_size(file_path, unit=FILE_SIZE_IN_MB)
-        expected_result = file_size / FILE_SIZE_IN_MB
+        expected_result = file_size / (1024 * 1024)
         message = message + "\n" + (" {} vs {}".format(result, expected_result))
         assert expected_result == result
 
         # Test file size in GB
         result = get_file_size(file_path, unit=FILE_SIZE_IN_GB)
-        expected_result = file_size / FILE_SIZE_IN_GB
+        expected_result = file_size / (1024 * 1024 * 1024)
         message = message + "\n" + (" {} vs {}".format(result, expected_result))
         assert expected_result == result
 
@@ -212,12 +374,17 @@ def run_tests(output):
     # lists of tests to be executed
     tests = [
         ["test_get_file_name_without_ext", test_get_file_name_without_ext],
+        [
+            "test_get_directory_path_from_file_path",
+            test_get_directory_path_from_file_path,
+        ],
     ]
 
     tests_temp_files = [
         ["test_file_size", test_file_size],
         ["test_file_exist", test_file_exist],
         ["test_file_delete", test_file_delete],
+        ["test_rename_file", test_rename_file],
     ]
 
     # execute tests

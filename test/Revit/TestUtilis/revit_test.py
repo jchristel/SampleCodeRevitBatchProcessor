a978fd0672b4df2a59397dd77cd547531dd3af5e
Jan <jan.r.christel@gmail.com>
2023-05-15 09:01:26 +1000

    revit test class updates
    to include csv file tests

--- test/Revit/TestUtilis/revit_test.py
+++ test/Revit/TestUtilis/revit_test.py
@@ -26,12 +26,16 @@ This module contains revit test base class .
 #
 #
 
+import os
+
 # import Autodesk
 import Autodesk.Revit.DB as rdb
 
 import tempfile
 from duHast.Utilities import base
-from duHast.Utilities.directory_io import directory_delete
+from duHast.Utilities.directory_io import directory_delete, directory_exists
+from duHast.Utilities.files_io import file_exist
+from duHast.Utilities.files_csv import read_csv_file
 from duHast.Revit.Common.revit_version import get_revit_version_number
 from duHast.Utilities import result as res
 
@@ -56,7 +60,7 @@ class RevitTest(base.Base):
             self.tmp_dir = self.create_tmp_dir()
         else:
             self.tmp_dir = None
-    
+
     def test(self):
         return_value = res.Result()
         return return_value
@@ -84,10 +88,91 @@ class RevitTest(base.Base):
         return return_value
 
     def create_temp_dir(self):
-        # set up a temp dir and test file path
+        """
+        Set up a temp dir and return the directory path
+
+        :return: Temp directory path
+        :rtype: str
+        """
+
         tmp_dir = tempfile.mkdtemp()
+        return tmp_dir
 
     def clean_up(self):
-        # remove temp directory
-        if self.tmp_dir is not None:
-            directory_delete(self.tmp_dir)
+        """
+        Remove temp directory
+        """
+        try:
+            if self.tmp_dir is not None:
+                del_temp_dir = directory_delete(self.tmp_dir)
+                assert del_temp_dir == True
+                temp_dir_exists = directory_exists(self.tmp_dir)
+                assert temp_dir_exists == False
+                return True
+            else:
+                return True
+        except Exception as e:
+            # this should not be happening!
+            print(e)
+            return False
+
+    def get_full_file_path(self, file_name):
+        """
+        returns fully qualified file path of file in temp directory if temp directory exists. Otherwise will return file name unchanged.
+
+        :param file_name: A file name
+        :type file_name: str
+        :return: If temp directory exists: fully qualified file path of file in temp directory, otherwise the file name unchanged.
+        :rtype: str
+        """
+        if self.tmp_dir == None:
+            return file_name
+        else:
+            return os.path.join(self.tmp_dir, file_name)
+
+    def test_csv_file(self, test_file_path, expected_file_content):
+        """
+            Checks whether a given CSV file exists and if so reads it content and compares it to an expected content
+
+            :param test_file_path: The fully qualified csv file path.
+            :type test_file_path: str
+            :param expected_file_content: The expected content of csv file.
+            :type expected_file_content: [[str,...]]
+            :return:
+                Result class instance.
+                - Status True if csv file exists and content matches expected content, otherwise False.
+                - result.message will contain file exist check outcome as well as content comparison outcome.
+                On exception:
+                - result.status (bool) will be False.
+                - result.message will contain the exception message.
+        :rtype: :class:`.Result`
+        """
+
+        return_value = res.Result()
+        try:
+            # double check...
+            file_exist_status = file_exist(test_file_path)
+            return_value.append_message(
+                " file exists check: {}".format(file_exist_status)
+            )
+            assert file_exist_status == True
+
+            # read file content
+            result_file_read = read_csv_file(test_file_path)
+            return_value.result = result_file_read
+            return_value.append_message(
+                " file read results: \n {} \n vs \n {}".format(
+                    sorted(result_file_read), sorted(expected_file_content)
+                )
+            )
+            assert sorted(result_file_read) == sorted(expected_file_content)
+
+        except Exception as e:
+            return_value.update_sep(
+                False,
+                "Failed to check CSV file: {} with exception: {}".format(
+                    test_file_path, e
+                ),
+            )
+
+        return return_value

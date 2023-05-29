f4881748ba0dbc7a788834b8d608c59a21d6f62b
Jan Christel <jan.r.christel@gmail.com>
2023-05-29 17:17:34 +1000

    Update files_csv.py

--- duHast/src/duHast/Utilities/files_csv.py
+++ duHast/src/duHast/Utilities/files_csv.py
@@ -29,6 +29,38 @@ Helper functions relating to comma separated text files.
 import codecs
 import csv
 
+from duHast.Utilities.files_io import get_file_name_without_ext
+
+def get_unique_headers(files):
+    '''
+    Gets a list of alphabetically sorted headers retrieved from text files.
+    Assumes:
+
+    - first row in each file is the header row
+    - headers are separated by <tab> character
+
+    :param files: List of file path from which the headers are to be returned.
+    :type files: list of str
+    :return: List of headers.
+    :rtype: list of str
+    '''
+
+    headers_in_all_files = {}
+    for f in files:
+        # get unmodified row data and remove the next line character at the end
+        data = get_first_row_in_csv_file(f)
+        headers_in_all_files[get_file_name_without_ext(f)] = data
+    headers_unique = []
+    for header_by_file in headers_in_all_files:
+        empty_header_counter = 0
+        for header in headers_in_all_files[header_by_file]:
+            # reformat any empty headers to be unique
+            if(header == ''):
+                header = header_by_file +  '.Empty.' + str(empty_header_counter)
+                empty_header_counter = empty_header_counter + 1
+            if(header not in headers_unique):
+                headers_unique.append(header)
+    return sorted(headers_unique)
 
 def read_csv_file(filepathCSV, increaseMaxFieldSizeLimit = False):
     '''
@@ -77,21 +109,21 @@ def get_first_row_in_csv_file(filePath):
     return return_value
 
 
-def write_report_data_as_csv (fileName, header, data, writeType = 'w'):
+def write_report_data_as_csv (file_name, header, data, write_type = 'w'):
     '''
     Function writing out report information as CSV file.
-    :param fileName: The reports fully qualified file path.
-    :type fileName: str
+    :param file_name: The reports fully qualified file path.
+    :type file_name: str
     :param header: list of column headers
     :type header: list of str
     :param data: list of list of strings representing row data
     :type data: [[str,str,..]]
-    :param writeType: Flag indicating whether existing report file is to be overwritten 'w' or appended to 'a', defaults to 'w'
-    :type writeType: str, optional
+    :param write_type: Flag indicating whether existing report file is to be overwritten 'w' or appended to 'a', defaults to 'w'
+    :type write_type: str, optional
     '''
 
     # open the file in the write mode
-    with codecs.open(fileName, writeType, encoding='utf-8') as f:
+    with codecs.open(file_name, write_type, encoding='utf-8') as f:
         # create the csv writer
         writer = csv.writer(f)
         # check header

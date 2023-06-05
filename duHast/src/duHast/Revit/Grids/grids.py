be85514defbd4d8d0dd3c6bfbc917604865d0d55
Jan Christel <jan.r.christel@gmail.com>
2023-06-05 14:16:07 +1000

    Update grids.py
    applied black format,
    added get min extent
    updated get max extent

--- duHast/src/duHast/Revit/Grids/grids.py
+++ duHast/src/duHast/Revit/Grids/grids.py
@@ -1,10 +1,10 @@
-'''
+"""
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Revit grids helper functions.
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-'''
+"""
 #
-#License:
+# License:
 #
 #
 # Revit Batch Processor Sample Code
@@ -30,6 +30,7 @@ import clr
 
 clr.AddReference("System.Core")
 from System import Linq
+
 clr.ImportExtensions(Linq)
 
 import System
@@ -41,63 +42,77 @@ from duHast.Revit.Common import parameter_get_utils as rParaGet
 # import Autodesk
 import Autodesk.Revit.DB as rdb
 
+
 def get_grids_in_model(doc):
-  '''
-  Get all grids in model
+    """
+    Get all grids in model
 
-  :param doc: The current model document.
-  :type doc: Autodesk.Revit.DB.Document
-  :return: A collector with all grids in model.
-  :rtype: _type_
-  '''
+    :param doc: The current model document.
+    :type doc: Autodesk.Revit.DB.Document
+    :return: A collector with all grids in model.
+    :rtype: _type_
+    """
 
-  collector = rdb.FilteredElementCollector(doc).OfClass(rdb.Grid)
-  return collector
+    collector = rdb.FilteredElementCollector(doc).OfClass(rdb.Grid)
+    return collector
+
 
 # --------------------------------------------- utility functions ------------------
 
+
 def get_all_grid_heads_by_category(doc):
-    '''
+    """
     Gets all grid head types in the model.
 
     :param doc: Current Revit model document.
     :type doc: Autodesk.Revit.DB.Document
     :return: A filtered element collector with grid head types
     :rtype: Autodesk.Revit.DB.FilteredElementCollector
-    '''
+    """
 
-    collector = rdb.FilteredElementCollector(doc).OfCategory(rdb.BuiltInCategory.OST_GridHeads).WhereElementIsElementType()
+    collector = (
+        rdb.FilteredElementCollector(doc)
+        .OfCategory(rdb.BuiltInCategory.OST_GridHeads)
+        .WhereElementIsElementType()
+    )
     return collector
 
+
 def get_all_grid_types_by_category(doc):
-    '''
+    """
     Gets all grid types in the model
 
     :param doc: Current Revit model document.
     :type doc: Autodesk.Revit.DB.Document
     :return: A filtered element collector with grid types
     :rtype: Autodesk.Revit.DB.FilteredElementCollector
-    '''
+    """
 
-    collector = rdb.FilteredElementCollector(doc).OfCategory(rdb.BuiltInCategory.OST_Grids).WhereElementIsElementType()
+    collector = (
+        rdb.FilteredElementCollector(doc)
+        .OfCategory(rdb.BuiltInCategory.OST_Grids)
+        .WhereElementIsElementType()
+    )
     return collector
 
+
 def get_all_grid_type_ids_by_category(doc):
-    '''
+    """
     Gets all grid types ids in the model.
 
     :param doc: Current Revit model document.
     :type doc: Autodesk.Revit.DB.Document
     :return: A filtered element collector with grid type ids
     :rtype: Autodesk.Revit.DB.FilteredElementCollector
-    '''
+    """
 
     collector = get_all_grid_types_by_category(doc)
     ids = com.get_ids_from_element_collector(collector)
     return ids
 
-def get_grid_type_names (doc, g):
-    '''
+
+def get_grid_type_names(doc, g):
+    """
     Gets all valid grid types, based on a past in grid, available in model.
 
     Uses grid.GetValidTypes() to get the grid types.
@@ -108,7 +123,7 @@ def get_grid_type_names (doc, g):
     :type g: Autodesk.Revit.DB.Grid
     :return: A nested set of lists containing grid type id and grid type name
     :rtype: list of lists [[GridTypeId as Revit ElementId, grid type name as string],[...]]
-    '''
+    """
 
     valid_grid_types = []
     valid_grid_type_ids = g.GetValidTypes()
@@ -120,8 +135,9 @@ def get_grid_type_names (doc, g):
         valid_grid_types.append(grid_data)
     return valid_grid_types
 
-def get_grid_type_name (doc, g):
-    '''
+
+def get_grid_type_name(doc, g):
+    """
     Gets the grid type name of a grid.
 
     :param doc: Current Revit model document.
@@ -130,15 +146,16 @@ def get_grid_type_name (doc, g):
     :type g: Autodesk.Revit.DB.Grid
     :return: The grid type name.
     :rtype: str
-    '''
+    """
 
-    value = 'unknown'
+    value = "unknown"
     grid_type_T = doc.GetElement(g.GetTypeId())
     value = rdb.Element.Name.GetValue(grid_type_T)
     return value
 
-def get_grid_type_id_by_name (doc, grid_type_name):
-    '''
+
+def get_grid_type_id_by_name(doc, grid_type_name):
+    """
     Gets the grid type Id based on it's name, if no match found it returns the Revit Invalid Element Id
 
     :param doc: Current Revit model document.
@@ -147,22 +164,23 @@ def get_grid_type_id_by_name (doc, grid_type_name):
     :type grid_type_name: str
     :return: The grids type Id or if not match is found Autodesk.Revit.DB.ElementId.InvalidElementId
     :rtype: Autodesk.Revit.DB.ElementId
-    '''
+    """
 
     id = rdb.ElementId.InvalidElementId
     grids = rdb.FilteredElementCollector(doc).OfClass(rdb.Grid).ToList()
-    if(len(grids) > 0):
+    if len(grids) > 0:
         g = grids[0]
         valid_grid_type_ids = g.GetValidTypes()
         for grid_typ_id in valid_grid_type_ids:
             g_type_name = rdb.Element.Name.GetValue(doc.GetElement(grid_typ_id))
-            if(g_type_name ==  grid_type_name):
+            if g_type_name == grid_type_name:
                 id = grid_typ_id
                 break
     return id
 
+
 def grid_check_parameter_value(g, para_name, para_condition, condition_value):
-    '''
+    """
     Returns true if a given parameter on a grid has a value meeting the parameter condition.
 
     :param g: A grid.
@@ -175,37 +193,62 @@ def grid_check_parameter_value(g, para_name, para_condition, condition_value):
     :type condition_value: var
     :return: True if parameter value is evaluated to True otherwise False.
     :rtype: bool
-    '''
+    """
 
     rule_match = False
-    p_value = rParaGet.get_parameter_value_by_name (g, para_name)
-    if (p_value != None):
+    p_value = rParaGet.get_parameter_value_by_name(g, para_name)
+    if p_value != None:
         rule_match = rParaGet.check_parameter_value(g, para_condition, condition_value)
     return rule_match
 
+
 def get_max_extent_as_string(g):
-    '''
+    """
     Gets the maximum extent of a grid.
 
     :param g: A grid.
     :type g: Autodesk.Revit.DB.Grid
-    :return: A string in format [maxX,maxY,maxZ]<tab>[minX,minY,minZ]
+    :return: A string in format [maxX,maxY,maxZ]
     :rtype: str
-    '''
+    """
 
     ex = g.GetExtents()
-    max = '['+ ','.join([str(ex.MaximumPoint.X), str(ex.MaximumPoint.Y), str(ex.MaximumPoint.Z)]) + ']'
-    min = '['+ ','.join([str(ex.MinimumPoint.X), str(ex.MinimumPoint.Y), str(ex.MinimumPoint.Z)]) + ']'    
-    return '\t'.join([min, max])
+    max = "[{}]".format(
+        ",".join(
+            [str(ex.MaximumPoint.X), str(ex.MaximumPoint.Y), str(ex.MaximumPoint.Z)]
+        )
+    )
+    return max
+
+
+def get_min_extent_as_string(g):
+    """
+    Gets the minimum extent of a grid.
+
+    :param g: A grid.
+    :type g: Autodesk.Revit.DB.Grid
+    :return: A string in format [minX,minY,minZ]
+    :rtype: str
+    """
+
+    ex = g.GetExtents()
+    min = "[{}]".format(
+        ",".join(
+            [str(ex.MinimumPoint.X), str(ex.MinimumPoint.Y), str(ex.MinimumPoint.Z)]
+        )
+    )
+    return min
 
 
 def get_all_grid_head_family_type_ids(doc):
-    ''' 
+    """
     This will return all ids grid head family types in the model
-    '''
+    """
 
     ids = []
     filter = rdb.ElementCategoryFilter(rdb.BuiltInCategory.OST_GridHeads)
-    col = rdb.FilteredElementCollector(doc).OfClass(rdb.FamilySymbol).WherePasses(filter)
+    col = (
+        rdb.FilteredElementCollector(doc).OfClass(rdb.FamilySymbol).WherePasses(filter)
+    )
     ids = com.get_ids_from_element_collector(col)
     return ids

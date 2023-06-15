9833795b1f3001fd8b232c42545999ba01806c2d
Jan Christel <jan.r.christel@gmail.com>
2023-06-15 14:54:46 +1000

    Update independent_tags_elbow_properties.py
    added points as double option

--- duHast/src/duHast/Revit/Annotation/independent_tags_elbow_properties.py
+++ duHast/src/duHast/Revit/Annotation/independent_tags_elbow_properties.py
@@ -29,13 +29,16 @@ A number of functions around Revit independent tags elbow properties.
 
 import Autodesk.Revit.DB as rdb
 from duHast.Revit.Common.revit_version import get_revit_version_number
+from duHast.Revit.Common.Geometry.geometry import get_point_as_doubles
 
 ELBOW_LOCATION = "elbow_location"
 LEADER_END = "leader_end"
 LEADER_REFERENCE = "leader_reference"
+LEADER_ELEMENT_REFERENCE_ID = "leader_element_reference_id"
+LEADER_LINKED_ELEMENT_REFERENCE_ID = "leader_linked_element_reference_id"
 
 
-def get_elbow_properties(doc, tag):
+def get_elbow_properties(doc, tag, points_as_double=False):
     """
     Returns elbow properties depending on revit version.
 
@@ -52,13 +55,13 @@ def get_elbow_properties(doc, tag):
     revit_version = get_revit_version_number(doc)
     data = None
     if revit_version <= 2021:
-        data = get_elbow_properties_2021(tag)
+        data = get_elbow_properties_2021(tag, points_as_double)
     elif revit_version >= 2022:
-        data = get_elbow_properties_2022(tag)
+        data = get_elbow_properties_2022(tag, points_as_double)
     return data
 
 
-def get_elbow_properties_2021(tag):
+def get_elbow_properties_2021(tag, points_as_double):
     """
     Get elbow properties in revit versions up to revit 2022
 
@@ -72,13 +75,22 @@ def get_elbow_properties_2021(tag):
     data = []
     elbow_properties = {}
     try:
-        if(tag.HasElbow):
-            elbow_properties[LEADER_REFERENCE]=None
-            elbow_properties[ELBOW_LOCATION] = tag.LeaderElbow
-            if(tag.LeaderEndCondition == rdb.LeaderEndCondition.Free):
-                elbow_properties[LEADER_END] = tag.LeaderEnd
+        if tag.HasElbow:
+            elbow_properties[LEADER_REFERENCE] = {
+                LEADER_ELEMENT_REFERENCE_ID: None,
+                LEADER_LINKED_ELEMENT_REFERENCE_ID: None,
+            }
+            if points_as_double:
+                elbow_properties[ELBOW_LOCATION] = get_point_as_doubles(tag.LeaderElbow)
             else:
-                elbow_properties[LEADER_END]=None
+                elbow_properties[ELBOW_LOCATION] = tag.LeaderElbow
+            if tag.LeaderEndCondition == rdb.LeaderEndCondition.Free:
+                if points_as_double:
+                    elbow_properties[LEADER_END] = get_point_as_doubles(tag.LeaderEnd)
+                else:
+                    elbow_properties[LEADER_END] = tag.LeaderEnd
+            else:
+                elbow_properties[LEADER_END] = None
         else:
             elbow_properties[ELBOW_LOCATION] = None
             elbow_properties[LEADER_END] = None
@@ -90,7 +102,7 @@ def get_elbow_properties_2021(tag):
     return data
 
 
-def get_elbow_properties_2022(tag):
+def get_elbow_properties_2022(tag, points_as_double):
     """
     Get elbow properties in revit versions from revit 2023 onwards
 
@@ -100,24 +112,38 @@ def get_elbow_properties_2022(tag):
     :return: A list of dictionaries with 3 keys: ELBOW_LOCATION, LEADER_END and LEADER_REFERENCE.
     :rtype: [{str:Autodesk.Revit.DB.XYZ, str:Autodesk.Revit.DB.XYZ, str:Autodesk.Revit.DB Reference }]
     """
-    
+
     data = []
     tagged_references = tag.GetTaggedReferences()
     for tag_ref in tagged_references:
         elbow_properties = {}
-        elbow_properties[LEADER_REFERENCE]=tag_ref
+        # store references to linked element or the element in same model which got tagged only
+        elbow_properties[LEADER_REFERENCE] = {
+            LEADER_ELEMENT_REFERENCE_ID: tag_ref.ElementId.IntegerValue,
+            LEADER_LINKED_ELEMENT_REFERENCE_ID: tag_ref.LinkedElementId.IntegerValue,
+        }
         try:
-            if(tag.HasLeaderElbow(tag_ref)):
-                elbow_properties[ELBOW_LOCATION] = tag.GetLeaderElbow(tag_ref)
-                if(tag.LeaderEndCondition == rdb.LeaderEndCondition.Free):
-                    elbow_properties[LEADER_END]=tag.GetLeaderEnd(tag_ref)
+            if tag.HasLeaderElbow(tag_ref):
+                if points_as_double:
+                    elbow_properties[ELBOW_LOCATION] = get_point_as_doubles(
+                        tag.GetLeaderElbow(tag_ref)
+                    )
                 else:
-                    elbow_properties[LEADER_END]=None
+                    elbow_properties[ELBOW_LOCATION] = tag.GetLeaderElbow(tag_ref)
+                if tag.LeaderEndCondition == rdb.LeaderEndCondition.Free:
+                    if points_as_double:
+                        elbow_properties[LEADER_END] = get_point_as_doubles(
+                            tag.GetLeaderEnd(tag_ref)
+                        )
+                    else:
+                        elbow_properties[LEADER_END] = tag.GetLeaderEnd(tag_ref)
+                else:
+                    elbow_properties[LEADER_END] = None
             else:
-                elbow_properties[ELBOW_LOCATION]=None
-                elbow_properties[LEADER_END]=None
+                elbow_properties[ELBOW_LOCATION] = None
+                elbow_properties[LEADER_END] = None
         except:
-            elbow_properties[ELBOW_LOCATION]=None
-            elbow_properties[LEADER_END]=None
+            elbow_properties[ELBOW_LOCATION] = None
+            elbow_properties[LEADER_END] = None
         data.append(elbow_properties)
     return data

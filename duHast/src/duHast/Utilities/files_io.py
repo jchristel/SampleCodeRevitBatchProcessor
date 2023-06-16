761be9209a2600ce25485340b1a8eb0861fd3bcf
Jan Christel <jan.r.christel@gmail.com>
2023-06-16 20:31:51 +1000

    Update files_io.py

--- duHast/src/duHast/Utilities/files_io.py
+++ duHast/src/duHast/Utilities/files_io.py
@@ -1,10 +1,10 @@
-'''
+"""
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 Helper functions relating to file IO operations. 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-'''
+"""
 #
-#License:
+# License:
 #
 #
 # Revit Batch Processor Sample Code
@@ -27,20 +27,19 @@ Helper functions relating to file IO operations.
 #
 
 import os
-import os.path
 import shutil
 
-#from System.IO import Path
+# from System.IO import Path
 
 
 def file_exist(full_file_path):
-    '''
+    """
     Checks whether a file exists
     :param full_file_path: Fully qualified file path
     :type full_file_path: str
     :return: True file exists, otherwise False
     :rtype: bool
-    '''
+    """
 
     try:
         value = os.path.exists(full_file_path)
@@ -50,14 +49,14 @@ def file_exist(full_file_path):
 
 
 def file_delete(full_file_path):
-    '''
+    """
     Deletes file.
 
     :param full_file_path: Fully qualified file path
     :type full_file_path: str
     :return: True if deleted, otherwise False
     :rtype: bool
-    '''
+    """
     try:
         os.remove(full_file_path)
         value = True
@@ -67,23 +66,23 @@ def file_delete(full_file_path):
 
 
 def get_directory_path_from_file_path(file_path):
-    '''
+    """
     Extracts directory from file path.
-    
+
     :param file_path: A fully qualified file path.
     :type file_path: str
     :return: If no exception occurs : A fully qualified directory path,else an empty string.
     :rtype: str
-    '''
+    """
     try:
         value = os.path.dirname(file_path)
     except Exception:
-        value = ''
+        value = ""
     return value
 
 
 def rename_file(old_name, new_name):
-    '''
+    """
     Renames a file.
     :param old_name: Fully qualified file path to file to be renamed.
     :type old_name: str
@@ -91,7 +90,7 @@ def rename_file(old_name, new_name):
     :type new_name: str
     :return: True file renamed, otherwise False
     :rtype: bool
-    '''
+    """
 
     try:
         os.rename(old_name, new_name)
@@ -102,7 +101,7 @@ def rename_file(old_name, new_name):
 
 
 def copy_file(old_name, new_name):
-    '''
+    """
     Copies a file
     :param old_name: Fully qualified file path to file to be copied.
     :type old_name: str
@@ -110,7 +109,7 @@ def copy_file(old_name, new_name):
     :type new_name: str
     :return: True file copied, otherwise False
     :rtype: bool
-    '''
+    """
 
     value = True
     try:
@@ -119,15 +118,17 @@ def copy_file(old_name, new_name):
         value = False
     return value
 
+
 #: file size in KB conversion
 FILE_SIZE_IN_KB = 1024
 #: file size in MB conversion
-FILE_SIZE_IN_MB = 1024*1024
+FILE_SIZE_IN_MB = 1024 * 1024
 #: file size in GB conversion
-FILE_SIZE_IN_GB = 1024*1024*1024
+FILE_SIZE_IN_GB = 1024 * 1024 * 1024
 
-def get_file_size(file_path, unit = FILE_SIZE_IN_MB):
-    '''
+
+def get_file_size(file_path, unit=FILE_SIZE_IN_MB):
+    """
     Get the file size in given units (default is MB)
     :param file_path: Fully qualified file path
     :type file_path: str
@@ -135,7 +136,7 @@ def get_file_size(file_path, unit = FILE_SIZE_IN_MB):
     :type unit: int
     :return: The file size.
     :rtype: float
-    '''
+    """
 
     # default value if anything goes wrong
     size = -1
@@ -149,7 +150,7 @@ def get_file_size(file_path, unit = FILE_SIZE_IN_MB):
 
 
 def convert_relative_path_to_full_path(relative_file_path, full_file_path):
-    '''
+    """
     removes '../..' or '../' from relative file path string and replaces it with full path derived path past in sample path.
     - relative path sample: 'C:/temp/../myfile.ext'
     - full file path sample: 'C:/temp/Sample/someOtherFile.ext'
@@ -160,49 +161,91 @@ def convert_relative_path_to_full_path(relative_file_path, full_file_path):
     :type full_file_path: str
     :return: A fully qualified file path.
     :rtype: str
-    '''
+    """
 
-    if( r'..\..' in relative_file_path):
-        two_up = path.abspath(path.join(full_file_path ,r'..\..'))
+    if r"..\.." in relative_file_path:
+        two_up = os.path.abspath(os.path.join(full_file_path, r"..\.."))
         return two_up + relative_file_path[5:]
-    elif('..' in relative_file_path):
-        one_up = path.abspath(path.join(full_file_path ,'..'))
+    elif ".." in relative_file_path:
+        one_up = os.path.abspath(os.path.join(full_file_path, ".."))
         return one_up + relative_file_path[2:]
     else:
         return relative_file_path
 
 
 def get_file_name_without_ext(file_path):
-    '''
+    """
     Returns the file name without the file extension.
 
     :param file_path: Fully qualified file path to file
     :type file_path: str
     :return: The file name.
     :rtype: str
-    '''
+    """
 
     file_name = os.path.basename(file_path)
-    name_chunks = file_name.split('.')
-    name = ''
-    if(len(name_chunks)>1):
-        name = '.'.join(name_chunks[:-1])
+    name_chunks = file_name.split(".")
+    name = ""
+    if len(name_chunks) > 1:
+        name = ".".join(name_chunks[:-1])
     else:
         name = file_name
     return name
 
 
+def read_text_file(file_path):
+    """
+    Reads a text file and returns its content as a single string.
+
+    :param file_path: The fully qualified file path.
+    :type file_path: str
+    :return: The content of a text file.
+    :rtype: str
+    """
+
+    file_content = None
+    try:
+        with open(file_path, "r") as file:
+            file_content = file.read()
+            file.close()
+    except Exception:
+        file_content = None
+    return file_content
+
+
+def read_text_file_into_list(file_path):
+    """
+    Reads a text file and returns its content as a list of string.
+
+    It will return
+
+    - one string per row
+    - removed line break characters
+
+    :param file_path: The fully qualified file path.
+    :type file_path: str
+    :return: The content of a text file. Can be an empty list if an exception occurred during file read.
+    :rtype: [str]
+    """
+
+    lines = []
+    file_content = read_text_file(file_path)
+    if file_content != None:
+        lines = file_content.splitlines()
+    return lines
+
+
 def get_first_row_in_file(file_path):
-    '''
+    """
     Reads the first line of a text file and returns it as a single string with any leading or trailing white spaces stripped!
-    
+
     :param file_path: The fully qualified file path.
     :type file_path: str
     :return: The first row of a text file.
     :rtype: str
-    '''
+    """
 
-    row = ''
+    row = ""
     try:
         with open(file_path) as f:
             row = f.readline().strip()
@@ -210,19 +253,20 @@ def get_first_row_in_file(file_path):
         row = None
     return row
 
+
 def get_first_row_in_file_no_strip(file_path):
-    '''
+    """
     Reads the first line of a text file and returns it as a single string.
 
     Note this may contain a new line character at the end! ('\n')
-    
+
     :param file_path: The fully qualified file path.
     :type file_path: str
     :return: The first row of a text file.
     :rtype: str
-    '''
+    """
 
-    row = ''
+    row = ""
     try:
         with open(file_path) as f:
             row = f.readline()

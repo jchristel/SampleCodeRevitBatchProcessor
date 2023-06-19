d3802086cb51efb2ab219b9fbbf15de6134bc923
Jan Christel <jan.r.christel@gmail.com>
2023-06-19 16:48:58 +1000

    Update tags_independent_report.py
    bug fixes

--- duHast/src/duHast/Revit/Annotation/Reporting/tags_independent_report.py
+++ duHast/src/duHast/Revit/Annotation/Reporting/tags_independent_report.py
@@ -136,8 +136,8 @@ def get_tag_instances_report_data(doc, revit_file_path, custom_element_filter):
                     if revit_version >= 2023:
                         row[
                             props.TAG_PRESENTATION_MODE
-                        ] = tag_instance.LeadersPresentationMode
-                        row[props.MERGE_ELBOWS] = str(tag_instance.MergeElbows)
+                        ] = str(tag_instance.LeadersPresentationMode)
+                        row[props.MERGE_ELBOWS] = tag_instance.MergeElbows
                     data.append(row)
         except Exception as e:
             data.append(

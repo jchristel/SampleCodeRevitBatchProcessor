25e3cbe13ea61950b088ebbfdb45bc8bc0ad3f56
Jan <jan.r.christel@gmail.com>
2023-07-14 08:27:25 +1000

    files out flow updates

--- Samples/Flows/Modify_FilesOut/_Script/Post_FilesOut.py
+++ Samples/Flows/Modify_FilesOut/_Script/Post_FilesOut.py
@@ -13,7 +13,7 @@ It:
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-#License:
+# License:
 #
 #
 # Revit Batch Processor Sample Code
@@ -39,17 +39,19 @@ It:
 # Imports
 # --------------------------
 
-#import clr
-#import System
+# import clr
+# import System
 import os.path
 from System.IO import Path
 
 
-import settings as settings # sets up all commonly used variables and path locations!
+import settings as settings  # sets up all commonly used variables and path locations!
 from utils import utils as utilLocal
+from utils.meta_data import meta_data
 
 # import common library
 from duHast.Utilities import Utility as util
+
 # import log utils
 from duHast.Utilities import BatchProcessorLogUtils as logutils
 from duHast.Utilities import WorksharingMonitorProcess as wsmp
@@ -58,38 +60,45 @@ from duHast.Utilities import WorksharingMonitorProcess as wsmp
 # my code here:
 # -------------
 
+
 # output messages
-def Output(message = ''):
-    print (message)
+def Output(message=""):
+    print(message)
+
 
 # writes out the document data file back in the script folder
 # with updated revision information retrieved from marker files
 def write_new_file_data(doc_files):
     flag = False
-    # compare lists 
+    # compare lists
     doc_files_sorted = []
     for cfd in doc_files:
         match = False
         for nfd in marker_file_data_:
-            if (nfd.existingFileName == cfd.existingFileName and nfd.fileExtension == cfd.fileExtension):
+            if (
+                nfd.existingFileName == cfd.existingFileName
+                and nfd.fileExtension == cfd.fileExtension
+            ):
                 match = True
                 doc_files_sorted.append(nfd)
                 break
-        if (match == False):
+        if match == False:
             doc_files_sorted.append(cfd)
     data = convert_class_to_string(doc_files_sorted)
     flag = write_new_data(settings.REVISION_DATA_FILEPATH, data)
     return flag, doc_files_sorted
 
+
 # write new revision data out to file
 def write_new_data(path, data):
     status = True
     try:
-       util.writeReportDataAsCSV(path, [], data)
+        util.writeReportDataAsCSV(path, [], data)
     except Exception as e:
         status = False
-        Output('Failed to write data file: {} with exception: {}'.format(path, e))
+        Output("Failed to write data file: {} with exception: {}".format(path, e))
     return status
+
 
 def convert_class_to_string(doc_files):
     data = []
@@ -97,69 +106,6 @@ def convert_class_to_string(doc_files):
         data.append(doc_file.getData())
     return data
 
-def write_meta_data():
-    aconex_meta_data = []
-    header = settings.ACONEX_METADATA_HEADER
-    aconex_meta_data.append(header)
-    flag = True
-    # get files in todays output folder
-    files = util.GetFiles(root_path_, fileExtension = '.*')
-    # loop over files found and build aconex list
-    if(len(files) > 0):
-        for file in files:
-            file_name, file_extension = os.path.splitext(file)
-            file_name = Path.GetFileNameWithoutExtension(file)
-            # print('filename: '+str(filename) + ' file extension: ' + str(fileExtension))
-            for docs in doc_files_:
-                # print('doc filename: '+str(docs.existingFileName) + ' doc file extension: ' + str(docs.fileExtension))
-                if(file_name.startswith(docs.AconexDocNumber) and file_extension == docs.fileExtension):
-                    # print('match: ' + str(filename)+ str(fileExtension))
-                    row=[]
-                    row.append(docs.AconexDocNumber)
-                    row.append(str(docs.revision))
-                    row.append(docs.AconexDocName)
-                    row.append(settings.ACONEX_METADATA_DOC_TYPE)
-                    row.append(settings.ACONEX_METADATA_DOC_STATUS)
-                    row.append(settings.ACONEX_METADATA_DISCIPLINE)
-                    row.append(settings.ACONEX_METADATA_PROJECT_PHASE)
-                    row.append(str(file_name)+str(file_extension))
-                    row.append(settings.ACONEX_METADATA_NOT_APPLICABLE)
-                    row.append('')
-                    row.append(util.GetDateStamp(settings.ACONEX_METADATA_DATE_FORMAT))
-                    row.append(settings.ACONEX_METADATA_COMPANY)
-                    row.append('')
-                    row.append('')
-                    row.append('')
-                    row.append('')
-                    aconex_meta_data.append(row)
-                    # update revision for file where we found a match only??
-                    # in cases where we issue .rvt only but .rvt and .ifc revision need to stay the same....
-                    docs.NewRevision = True
-                elif(file_name.startswith(docs.existingFileName) and not(file_name.startswith(docs.AconexDocNumber)) and file_extension == docs.fileExtension):
-                    # this condition applies to NWC's only on this job...
-                    # print('match: ' + str(filename)+ str(fileExtension))
-                    row=[]
-                    row.append(docs.AconexDocNumber)
-                    row.append(str(docs.revision))
-                    row.append(docs.AconexDocName)
-                    row.append(settings.ACONEX_METADATA_DOC_TYPE)
-                    row.append(settings.ACONEX_METADATA_DOC_STATUS)
-                    row.append(settings.ACONEX_METADATA_DISCIPLINE)
-                    row.append(settings.ACONEX_METADATA_PROJECT_PHASE)
-                    row.append(str(file_name)+str(file_extension))
-                    row.append(settings.ACONEX_METADATA_NOT_APPLICABLE)
-                    row.append('')
-                    row.append(util.GetDateStamp(settings.ACONEX_METADATA_DATE_FORMAT))
-                    row.append(settings.ACONEX_METADATA_COMPANY)
-                    row.append('')
-                    row.append('')
-                    row.append('')
-                    row.append('')
-                    aconex_meta_data.append(row)
-                    docs.NewRevision = True
-    # write out aconex list
-    flag = write_new_data(root_path_ + '\\' + settings.ACONEX_METADATA_FILE_NAME, aconex_meta_data)
-    return flag
 
 # deletes back up folders and revit project files
 def clean_up_export_folder():
@@ -167,43 +113,46 @@ def clean_up_export_folder():
     # delete sub directories
     try:
         sub_dirs = util.GetChildDirectories(settings.ROOT_PATH_EXPORT)
-        if(len(sub_dirs)>0):
+        if len(sub_dirs) > 0:
             for d in sub_dirs:
                 status = status & util.DirectoryDelete(d)
     except Exception as e:
-        Output('Failed to delete sub directory with exception {}'.format(e))
+        Output("Failed to delete sub directory with exception {}".format(e))
         status = status & False
     try:
         # delete rvt files
         revit_files = util.GetFilesWithFilter(settings.ROOT_PATH_EXPORT)
-        if(len(revit_files)>0):
+        if len(revit_files) > 0:
             for f in revit_files:
                 status = status & util.FileDelete(f)
     except Exception as e:
-        Output('Failed to delete file with exception {}'.format(e))
+        Output("Failed to delete file with exception {}".format(e))
         status = status & False
-    
+
     try:
         # delete txt marker files
-        text_files = util.GetFilesWithFilter(root_path_, settings.MARKER_FILE_EXTENSION, '*')
-        if(len(text_files)>0):
+        text_files = util.GetFilesWithFilter(
+            root_path_, settings.MARKER_FILE_EXTENSION, "*"
+        )
+        if len(text_files) > 0:
             for f in text_files:
                 status = status & util.FileDelete(f)
     except Exception as e:
-        Output('Failed to delete file with exception {}'.format(e))
+        Output("Failed to delete file with exception {}".format(e))
         status = status & False
 
     try:
         # delete log files
-        text_files = util.GetFilesWithFilter(root_path_, '.log', '*')
-        if(len(text_files)>0):
+        text_files = util.GetFilesWithFilter(root_path_, ".log", "*")
+        if len(text_files) > 0:
             for f in text_files:
                 status = status & util.FileDelete(f)
     except Exception as e:
-        Output('Failed to delete log file with exception: {}'.format(e))
+        Output("Failed to delete log file with exception: {}".format(e))
         status = status & False
 
     return status
+
 
 # -------------
 # main:
@@ -218,31 +167,45 @@ doc_files_ = []
 doc_files_ = utilLocal.read_current_file(settings.REVISION_DATA_FILEPATH)
 
 # build out directory location
-root_path_ = root_path_ + '\\' + settings.MODEL_OUT_FOLDER_NAME
+root_path_ = root_path_ + "\\" + settings.MODEL_OUT_FOLDER_NAME
 
 # read file data from revit files processed
-marker_file_data_=[]
-marker_file_data_result = utilLocal.read_marker_files_from_revit_processed(root_path_, settings.MARKER_FILE_EXTENSION)
-if(marker_file_data_result.status):
+marker_file_data_ = []
+marker_file_data_result = utilLocal.read_marker_files_from_revit_processed(
+    root_path_, settings.MARKER_FILE_EXTENSION
+)
+if marker_file_data_result.status:
     marker_file_data_ = marker_file_data_result.result
     # writes out new file data file in script location
     flag_write_file_data_, doc_files_ = write_new_file_data(doc_files_)
-    Output('Saved new document data file:...[{}]'.format(flag_write_file_data_))
+    Output("Saved new document data file:...[{}]".format(flag_write_file_data_))
 
     # write out metadata file
-    flag_meta_data_ = write_meta_data()
-    Output('Saved new Aconex meta data file:...[{}]'.format(flag_meta_data_))
+    flag_meta_data_ = meta_data(
+        meta_data_header=settings.ACONEX_METADATA_HEADER,
+        doc_files=doc_files_,
+        root_path=root_path_,
+    )
+    Output("Saved new Aconex meta data file:...[{}]".format(flag_meta_data_))
 else:
-    Output('Failed to read marker data:...[{}]'.format(marker_file_data_result.message))
+    Output("Failed to read marker data:...[{}]".format(marker_file_data_result.message))
 
 # run export folder clean up
 flag_export_clean_up = clean_up_export_folder()
-Output('Clean up:...[{}]'.format(flag_export_clean_up))
+Output("Clean up:...[{}]".format(flag_export_clean_up))
 
 # process logs
 processing_results_ = logutils.ProcessLogFiles(settings.LOG_MARKER_DIRECTORY)
-Output('Log result: {} :: [{}]'.format(processing_results_.message, processing_results_.status))
+Output(
+    "Log result: {} :: [{}]".format(
+        processing_results_.message, processing_results_.status
+    )
+)
 
 # WSMP marker files clean up
 clean_up_wsm_files_ = wsmp.CleanUpWSMDataFiles(settings.WSM_MARKER_DIRECTORY)
-Output('WSM result: {} :: [{}]'.format(clean_up_wsm_files_.message, clean_up_wsm_files_.status))
+Output(
+    "WSM result: {} :: [{}]".format(
+        clean_up_wsm_files_.message, clean_up_wsm_files_.status
+    )
+)
---
+++ Samples/Flows/Modify_FilesOut/_Script/utils/meta_data.py
@@ -0,0 +1,202 @@
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Module containing meta data file functions functions.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Some document exchange platforms may use meta data files to allow for fast processing of uploaded documents. This 
+sample code uses aconex for that purpose.
+
+
+
+"""
+
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# License:
+#
+#
+# Revit Batch Processor Sample Code
+#
+# Copyright (c) 2023  Jan Christel
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# --------------------------
+# Imports
+# --------------------------
+
+import os
+
+# import settings
+import settings as settings  # sets up all commonly used variables and path locations!
+
+from duHast.Utilities.Objects import result as res
+from duHast.Utilities.files_get import get_files
+from duHast.Utilities.files_io import get_file_name_without_ext
+from duHast.Utilities.date_stamps import get_date_stamp
+from duHast.Utilities.files_csv import write_report_data_as_csv
+
+
+def meta_data(meta_data_header, doc_files, root_path):
+    """
+    Function building meta data of files exported and writing data to file.
+
+    :param meta_data_header: First (header) row in meta data file.
+    :type meta_data_header: [str]
+    :param doc_files: List of meta data information for all files in project.
+    :type doc_files: [:class:`.docFile`]
+    :param root_path: Fully qualified directory path to where exported files are located.
+    :type root_path: str
+
+    :return:
+        Result class instance.
+
+        - Write meta data status returned in result.status. False if an exception occurred, otherwise True.
+        - result.message will contain messages for each file match found.
+        - result.result will be a nested list containing meta data : [[[str,str,..],[str,str,..],..]]
+
+        On exception:
+
+        - result.status (bool) will be False.
+        - result.message will contain the exception message.
+        - result.result will be an empty list.
+
+    :rtype: :class:`.Result`
+    """
+
+    return_value = res.Result()
+    # build meta data
+    result_meta_data_build = build_meta_data(
+        meta_data_header=meta_data_header, doc_files=doc_files, root_path=root_path
+    )
+    return_value.update(result_meta_data_build)
+    if result_meta_data_build.status:
+        # attempt to write data to file
+        try:
+            meta_data_file_path = os.path.join(root_path,settings.ACONEX_METADATA_FILE_NAME)
+            write_report_data_as_csv(meta_data_file_path, [], result_meta_data_build.result[0])
+            return_value.append_message("Successfully wrote meta data file to: {}".format(meta_data_file_path))
+        except Exception as e:
+            return_value.update_sep(False, 'Failed to write data file: {} with exception: {}'.format(meta_data_file_path, e))
+    else:
+        # no action
+        pass
+
+    return return_value
+
+
+def build_meta_data(meta_data_header, doc_files, root_path):
+    """
+    Builds meta data based on doc file data past in and files fpound in root path location.
+
+    :param meta_data_header: First (header) row in meta data file.
+    :type meta_data_header: [str]
+    :param doc_files: List of meta data information for all files in project.
+    :type doc_files: [:class:`.docFile`]
+    :param root_path: Fully qualified directory path to where exported files are located.
+    :type root_path: str
+
+    :return:
+        Result class instance.
+
+        - Build meta data list status returned in result.status. False if an exception occurred, otherwise True.
+        - result.message will contain messages for each file match found.
+        - result.result will be a nested list containing meta data : [[[str,str,..],[str,str,..],..]]
+
+        On exception:
+
+        - result.status (bool) will be False.
+        - result.message will contain the exception message.
+        - result.result will be an empty list.
+
+    :rtype: :class:`.Result`
+    """
+
+    return_value = res.Result()
+    meta_data = []
+    meta_data.append(meta_data_header)
+    flag = True
+    # get files in todays output folder
+    files = get_files(root_path, fileExtension=".*")
+    # loop over files found and build aconex list
+    if len(files) > 0:
+        for file in files:
+            file_name, file_extension = os.path.splitext(file)
+            file_name = get_file_name_without_ext(file)
+            for docs in doc_files:
+                if (
+                    file_name.startswith(docs.aconex_doc_number)
+                    and file_extension == docs.file_extension
+                ):
+                    return_value.append_message(
+                        "Found match: {} {}".format(file_name, file_extension)
+                    )
+                    row = []
+                    row.append(docs.aconex_doc_number)
+                    row.append(str(docs.revision))
+                    row.append(docs.aconex_doc_name)
+                    row.append(settings.ACONEX_METADATA_DOC_TYPE)
+                    row.append(settings.ACONEX_METADATA_DOC_STATUS)
+                    row.append(settings.ACONEX_METADATA_DISCIPLINE)
+                    row.append(settings.ACONEX_METADATA_PROJECT_PHASE)
+                    row.append(str(file_name) + str(file_extension))
+                    row.append(settings.ACONEX_METADATA_NOT_APPLICABLE)
+                    row.append("")
+                    row.append(get_date_stamp(settings.ACONEX_METADATA_DATE_FORMAT))
+                    row.append(settings.ACONEX_METADATA_COMPANY)
+                    row.append("")
+                    row.append("")
+                    row.append("")
+                    row.append("")
+                    meta_data.append(row)
+                    # update revision for file where we found a match only??
+                    # in cases where we issue .rvt only but .rvt and .ifc revision need to stay the same....
+                    docs.new_revision = True
+                elif (
+                    file_name.startswith(docs.existing_file_name)
+                    and not (file_name.startswith(docs.aconex_doc_number))
+                    and file_extension == docs.file_extension
+                ):
+                    # this condition applies to NWC's only on this job...
+                    return_value.append_message(
+                        "Found match: {} {}".format(file_name, file_extension)
+                    )
+                    row = []
+                    row.append(docs.aconex_doc_number)
+                    row.append(str(docs.revision))
+                    row.append(docs.aconex_doc_name)
+                    row.append(settings.ACONEX_METADATA_DOC_TYPE)
+                    row.append(settings.ACONEX_METADATA_DOC_STATUS)
+                    row.append(settings.ACONEX_METADATA_DISCIPLINE)
+                    row.append(settings.ACONEX_METADATA_PROJECT_PHASE)
+                    row.append(str(file_name) + str(file_extension))
+                    row.append(settings.ACONEX_METADATA_NOT_APPLICABLE)
+                    row.append("")
+                    row.append(get_date_stamp(settings.ACONEX_METADATA_DATE_FORMAT))
+                    row.append(settings.ACONEX_METADATA_COMPANY)
+                    row.append("")
+                    row.append("")
+                    row.append("")
+                    row.append("")
+                    meta_data.append(row)
+                    docs.new_revision = True
+        # return meta data to caller
+        return_value.result.append(meta_data)
+    else:
+        return_value.update_sep(
+            False, "No files to add to meta data found in {}".format(root_path)
+        )
+    return return_value

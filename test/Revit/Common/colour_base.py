661c663784cdaf150d298b8e9af08aca87783773
Jan <jan.r.christel@gmail.com>
2023-07-26 08:28:03 +1000

    Create colour_base.py
    starting test code

---
+++ test/Revit/Common/colour_base.py
@@ -0,0 +1,90 @@
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+This module contains revit design set tests . 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+#
+# License:
+#
+#
+# Revit Batch Processor Sample Code
+#
+# BSD License
+# Copyright 2023, Jan Christel
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+# - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# - Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+#
+# This software is provided by the copyright holder "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed. 
+# In no event shall the copyright holder be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; 
+# or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
+#
+#
+#
+
+from test.Revit.TestUtils import revit_test
+from duHast.src.duHast.Revit.Common.Objects.colour_base import ColourBase
+from duHast.Utilities.Objects import result as res
+
+# import Autodesk
+import Autodesk.Revit.DB as rdb
+
+
+class ColourB(revit_test.RevitTest):
+    def __init__(self, doc):
+        # store document in base class
+        super(ColourB, self).__init__(doc=doc, test_name="colour base tests")
+
+    def test(self):
+        """
+        colour base tests
+
+        :param doc: Current Revit model document.
+        :type doc: Autodesk.Revit.DB.Document
+        :param test_name: The test name.
+        :type test_name: str
+
+        :return:
+            Result class instance.
+                - .result = True if colour base tests completed successfully, otherwise False.
+                - .message will contain result(s) vs expected result(s)
+                - . result (empty list)
+
+                on exception:
+
+                - .result Will be False
+                - .message will contain exception message.
+                - . result (empty list)
+        :rtype: :class:`.Result`
+        """
+
+        return_value = res.Result()
+        try:
+            data_set = [
+                {1:{"red":100,"green":100,"blue":100}},
+                {2:{"red":90,"green":100,"blue":100}},
+                {3:{"red":100,"green":90,"blue":100}},
+                {4:{"red":100,"green":100,"blue":90}},
+            ]
+
+            # test class initialization
+            for d in data_set:
+                test_colour = ColourBase(data_set[d])
+                return_value.append_message(
+                    " {} vs {}".format(sorted(test_colour.to_json()), sorted(data_set[d]))
+                )
+                assert(test_colour.to_json() == data_set[d])
+            
+            
+
+        except Exception as e:
+            return_value.update_sep(
+                False,
+                "An exception occurred in function {}: {}".format(self.test_name,e),
+            )
+
+        return return_value

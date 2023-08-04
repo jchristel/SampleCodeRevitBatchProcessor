aa204db678be93b23ff90161f62116cac47fba74
Jan <jan.r.christel@gmail.com>
2023-08-04 20:34:46 +1000

    attempting to fix build system
    removed duHast root folder

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 # setup.py is required for local installation of the duHast package through:
 # ipy -m pip install -e .
 
-from setuptools import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory/"README.md").read_text()
 
 setup(
-    # ...
-    packages=find_packages(),
-    package_dir={"": "src"},
-    # ...
+    package_dir={"": "./src"},
+    packages=find_packages('./src'),
     long_description=long_description,
     long_description_content_type='text/markdown'
 )

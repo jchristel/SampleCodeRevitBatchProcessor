b4ff874459a69d1e19caf2376b9b3258bd8983fd
punderscoresmithuk <51383696+petersmithfromengland@users.noreply.github.com>
2023-08-09 16:38:45 +0100

    Updates various. See comments
    - Renames element_functions.py to element_spatial_utils.py
    - Renames link_functions.py to link_visibility.py
    - Renames sheet_functions.py to sheet_parameters.py
    - Adds view_parameters.py
    - Adds element_copying.py
    - Adds get_sheet_num_to_elem_dict function to sheets.py

--- src/duHast/Revit/Common/Utility/element_spatial_utils.py
+++ src/duHast/Revit/Common/Utility/element_spatial_utils.py
@@ -1,6 +1,6 @@
 """
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-This module contains a number of lists containing loadable family categories. 
+This module contains a number of utilities for an elements spatial properties
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 #
@@ -19,8 +19,8 @@ This module contains a number of lists containing loadable family categories.
 # - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 # - Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 #
-# This software is provided by the copyright holder "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed. 
-# In no event shall the copyright holder be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; 
+# This software is provided by the copyright holder "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed.
+# In no event shall the copyright holder be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits;
 # or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
 #
 #
@@ -28,25 +28,26 @@ This module contains a number of lists containing loadable family categories.
 
 from Autodesk.Revit.DB import BuiltInParameter
 
+
 def get_room_element_is_in(element, phase_dict):
-	'''
-	Get the room an element is in
-	:param element: The element to check
-	:type element: FamilyInstance
-	:param phase_dict: The dictionary of phase name to phase element
-	:type phase_dict: dict
-	:return: The room the element is in
-	:rtype: Room
-	'''
-	el_phase = element.get_Parameter(BuiltInParameter.PHASE_CREATED)
-	if el_phase != None:
-		el_phase_name = el_phase.AsValueString()
-		try:
-			el_phase_elem = phase_dict[el_phase_name]
-		except:
-			return None
-		el_room = element.get_Room(el_phase_elem)
-		
-		return el_room
-	else:
-		return None
+    """
+    Get the room an element is in
+    :param element: The element to check
+    :type element: FamilyInstance
+    :param phase_dict: The dictionary of phase name to phase element
+    :type phase_dict: dict
+    :return: The room the element is in
+    :rtype: Room
+    """
+    el_phase = element.get_Parameter(BuiltInParameter.PHASE_CREATED)
+    if el_phase != None:
+        el_phase_name = el_phase.AsValueString()
+        try:
+            el_phase_elem = phase_dict[el_phase_name]
+        except:
+            return None
+        el_room = element.get_Room(el_phase_elem)
+
+        return el_room
+    else:
+        return None

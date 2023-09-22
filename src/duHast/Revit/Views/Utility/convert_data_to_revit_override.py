0a858ae482a413b901df261cbb6eb2116c9ca479
Jan <jan.r.christel@gmail.com>
2023-09-22 18:29:40 +1000

    Update convert_data_to_revit_override.py

--- src/duHast/Revit/Views/Utility/convert_data_to_revit_override.py
+++ src/duHast/Revit/Views/Utility/convert_data_to_revit_override.py
@@ -31,7 +31,7 @@ import clr
 # used to convert integer to byte value
 import System
 
-from Autodesk.Revit.DB import Color, ElementId, OverrideGraphicSettings, 
+from Autodesk.Revit.DB import Color, ElementId, OverrideGraphicSettings
 
 from duHast.Revit.Common.Utility.revit_to_data_conversion import (
     VIEW_DETAIL_LEVEL_NAME_MAPPING_REVERSE,
@@ -104,7 +104,7 @@ def get_line_pattern_id(doc, data_pattern):
         return ElementId.InvalidId
 
 
-def convert_to_revit_graphic_override(doc, data_override, is_filter_override = False):
+def convert_to_revit_graphic_override(doc, data_override, is_filter_override=False):
     """
     Creates a Revit OverrideGraphicSettings instance from settings stored in an OverrideByCategory object.
 
@@ -118,96 +118,100 @@ def convert_to_revit_graphic_override(doc, data_override, is_filter_override = F
     :rtype: Autodesk.Revit.DB.OverrideGraphicSettings
     """
 
-    # set up a new revit override settings instance
+    # set up a new revit api override settings instance
     revit_override_setting = OverrideGraphicSettings()
 
-    # set values based on data override
+    # set values based on data override if one is present
+    if data_override.are_overrides_present:
+        # Sets the override color of the background pattern of cut faces.
+        revit_override_setting.SetCutBackgroundPatternColor = get_colour(
+            data_override.override_cut.pattern_background.colour
+        )
+        # Sets the ElementId of the cut face background pattern override. The fill pattern must be a drafting pattern.
+        # A value of InvalidElementId means no override is set.
+        revit_override_setting.SetCutBackgroundPatternId = get_fill_pattern_id(
+            doc, data_override.override_cut.pattern_background.fill_pattern_setting
+        )
+        # Sets the visibility of the cut face background fill pattern.
+        revit_override_setting.SetCutBackgroundPatternVisible = (
+            data_override.override_cut.pattern_background.is_visible
+        )
+        # Sets the override color of the foreground pattern of cut faces.
+        revit_override_setting.SetCutForegroundPatternColor = get_colour(
+            data_override.override_cut.pattern_foreground.colour
+        )
+        # Sets the ElementId of the cut face foreground pattern override. The fill pattern must be a drafting pattern.
+        # A value of InvalidElementId means no override is set.
+        revit_override_setting.SetCutForegroundPatternId = get_fill_pattern_id(
+            doc, data_override.override_cut.pattern_foreground.fill_pattern_setting
+        )
+        # Sets the visibility of the cut face foreground fill pattern.
+        revit_override_setting.SetCutForegroundPatternVisible = (
+            data_override.override_cut.pattern_foreground.is_visible
+        )
+        # Sets the cut surface line color.
+        revit_override_setting.SetCutLineColor = get_colour(
+            data_override.override_cut.line_cut.colour
+        )
+        # Sets the ElementId of the cut surface line pattern.
+        revit_override_setting.SetCutLinePatternId = get_line_pattern_id(
+            data_override.override_cut.line_cut.line_pattern_settings
+        )
+        # Sets the cut surface line weight.
+        revit_override_setting.SetCutLineWeight = (
+            data_override.override_cut.line_cut.weight
+        )
 
-    # Sets the override color of the background pattern of cut faces.
-    revit_override_setting.SetCutBackgroundPatternColor = get_colour(
-        data_override.override_cut.pattern_background.colour
-    )
-    # Sets the ElementId of the cut face background pattern override. The fill pattern must be a drafting pattern.
-    # A value of InvalidElementId means no override is set.
-    revit_override_setting.SetCutBackgroundPatternId = get_fill_pattern_id(
-        doc, data_override.override_cut.pattern_background.fill_pattern_setting
-    )
-    # Sets the visibility of the cut face background fill pattern.
-    revit_override_setting.SetCutBackgroundPatternVisible = (
-        data_override.override_cut.pattern_background.is_visible
-    )
-    # Sets the override color of the foreground pattern of cut faces.
-    revit_override_setting.SetCutForegroundPatternColor = get_colour(
-        data_override.override_cut.pattern_foreground.colour
-    )
-    # Sets the ElementId of the cut face foreground pattern override. The fill pattern must be a drafting pattern.
-    # A value of InvalidElementId means no override is set.
-    revit_override_setting.SetCutForegroundPatternId = get_fill_pattern_id(
-        doc, data_override.override_cut.pattern_foreground.fill_pattern_setting
-    )
-    # Sets the visibility of the cut face foreground fill pattern.
-    revit_override_setting.SetCutForegroundPatternVisible = (
-        data_override.override_cut.pattern_foreground.is_visible
-    )
-    # Sets the cut surface line color.
-    revit_override_setting.SetCutLineColor = get_colour(
-        data_override.override_cut.line_cut.colour
-    )
-    # Sets the ElementId of the cut surface line pattern.
-    revit_override_setting.SetCutLinePatternId = get_line_pattern_id(
-        data_override.override_cut.line_cut.line_pattern_settings
-    )
-    # Sets the cut surface line weight.
-    revit_override_setting.SetCutLineWeight = data_override.override_cut.line_cut.weight
-    
-    # detail override only exists on category overrides
-    if(is_filter_override == False):
-        # Sets the detail level.
-        revit_override_setting.SetDetailLevel = VIEW_DETAIL_LEVEL_NAME_MAPPING_REVERSE[
-            data_override.detail_level
-        ]
+        # detail override only exists on category overrides
+        if is_filter_override == False:
+            # Sets the detail level.
+            revit_override_setting.SetDetailLevel = (
+                VIEW_DETAIL_LEVEL_NAME_MAPPING_REVERSE[data_override.detail_level]
+            )
 
-    # Sets the halftone value.
-    revit_override_setting.SetHalftone = data_override.halftone
-    # Sets the projection surface line color.
-    revit_override_setting.SetProjectionLineColor = get_colour(
-        data_override.override_projection.line_projection.colour
-    )
-    # Sets the ElementId of the projection surface line pattern.
-    revit_override_setting.SetProjectionLinePatternId = get_line_pattern_id(
-        data_override.override_projection.line_projection.line_pattern_settings
-    )
-    # Sets the projection surface line weight.
-    revit_override_setting.SetProjectionLineWeight = (
-        data_override.override_projection.line_projection.weight
-    )
-    # Sets the override color of the surface background pattern.
-    revit_override_setting.SetSurfaceBackgroundPatternColor = get_colour(
-        data_override.override_projection.pattern_background.colour
-    )
-    # Sets the ElementId of the surface background pattern override. The fill pattern must be a drafting pattern.
-    # A value of InvalidElementId means no override is set.
-    revit_override_setting.SetSurfaceBackgroundPatternId = get_fill_pattern_id(
-        doc, data_override.override_projection.pattern_background.fill_pattern_setting
-    )
-    # Sets the visibility of the surface background fill pattern.
-    revit_override_setting.SetSurfaceBackgroundPatternVisible = (
-        data_override.override_projection.pattern_background.is_visible
-    )
-    # Sets the override color of the surface foreground pattern.
-    revit_override_setting.SetSurfaceForegroundPatternColor = get_colour(
-        data_override.override_projection.pattern_foreground.colour
-    )
-    # Sets the ElementId of the surface foreground pattern override. The fill pattern must be a drafting pattern.
-    # A value of InvalidElementId means no override is set.
-    revit_override_setting.SetSurfaceForegroundPatternId = get_fill_pattern_id(
-        doc, data_override.override_projection.pattern_foreground.fill_pattern_setting
-    )
-    # Sets the visibility of the surface foreground fill pattern.
-    revit_override_setting.SetSurfaceForegroundPatternVisible = (
-        data_override.override_projection.pattern_foreground.is_visible
-    )
-    # Sets the projection surface transparency
-    revit_override_setting.SetSurfaceTransparency = data_override.transparency
+        # Sets the halftone value.
+        revit_override_setting.SetHalftone = data_override.halftone
+        # Sets the projection surface line color.
+        revit_override_setting.SetProjectionLineColor = get_colour(
+            data_override.override_projection.line_projection.colour
+        )
+        # Sets the ElementId of the projection surface line pattern.
+        revit_override_setting.SetProjectionLinePatternId = get_line_pattern_id(
+            data_override.override_projection.line_projection.line_pattern_settings
+        )
+        # Sets the projection surface line weight.
+        revit_override_setting.SetProjectionLineWeight = (
+            data_override.override_projection.line_projection.weight
+        )
+        # Sets the override color of the surface background pattern.
+        revit_override_setting.SetSurfaceBackgroundPatternColor = get_colour(
+            data_override.override_projection.pattern_background.colour
+        )
+        # Sets the ElementId of the surface background pattern override. The fill pattern must be a drafting pattern.
+        # A value of InvalidElementId means no override is set.
+        revit_override_setting.SetSurfaceBackgroundPatternId = get_fill_pattern_id(
+            doc,
+            data_override.override_projection.pattern_background.fill_pattern_setting,
+        )
+        # Sets the visibility of the surface background fill pattern.
+        revit_override_setting.SetSurfaceBackgroundPatternVisible = (
+            data_override.override_projection.pattern_background.is_visible
+        )
+        # Sets the override color of the surface foreground pattern.
+        revit_override_setting.SetSurfaceForegroundPatternColor = get_colour(
+            data_override.override_projection.pattern_foreground.colour
+        )
+        # Sets the ElementId of the surface foreground pattern override. The fill pattern must be a drafting pattern.
+        # A value of InvalidElementId means no override is set.
+        revit_override_setting.SetSurfaceForegroundPatternId = get_fill_pattern_id(
+            doc,
+            data_override.override_projection.pattern_foreground.fill_pattern_setting,
+        )
+        # Sets the visibility of the surface foreground fill pattern.
+        revit_override_setting.SetSurfaceForegroundPatternVisible = (
+            data_override.override_projection.pattern_foreground.is_visible
+        )
+        # Sets the projection surface transparency
+        revit_override_setting.SetSurfaceTransparency = data_override.transparency
 
     return revit_override_setting

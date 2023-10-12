a9336e802edbab84a1cd2949dbcf62388583930e
Jan <jan.r.christel@gmail.com>
2023-10-12 20:13:34 +1100

    Update timer.py
    docstrings added

--- src/duHast/Utilities/Objects/timer.py
+++ src/duHast/Utilities/Objects/timer.py
@@ -19,8 +19,8 @@ A timer class to measure code performance.
 # - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 # - Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 #
-# This software is provided by the copyright holder "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed. 
-# In no event shall the copyright holder be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; 
+# This software is provided by the copyright holder "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed.
+# In no event shall the copyright holder be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits;
 # or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
 #
 #
@@ -44,6 +44,20 @@ class TimerError(Exception):
 class Timer(base.Base):
     """
     A class that provides functionality to measure the elapsed time between starting and stopping the timer.
+
+    Example Usage:
+    ```python
+    timer = Timer()  # Create a new instance of the Timer class
+    timer.start()  # Start the timer
+    # Perform some operations
+    timer.stop()  # Stop the timer and get the elapsed time
+    ```
+
+    Methods:
+    - __init__(): Initializes a new instance of the Timer class.
+    - start(): Starts a new timer.
+    - stop(): Stops the timer and returns the elapsed time.
+    - is_running(): Checks whether the timer is running.
     """
 
     def __init__(self):

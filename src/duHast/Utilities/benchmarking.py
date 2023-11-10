487fb60b5fe0fbc38e01c646f4eb2221b69a7a5a
punderscoresmithuk <51383696+petersmithfromengland@users.noreply.github.com>
2023-11-10 12:04:20 +0900

    Updates measure_time decorator to allow override

--- src/duHast/Utilities/benchmarking.py
+++ src/duHast/Utilities/benchmarking.py
@@ -23,34 +23,52 @@ Helper functions for benchmarking:.
 # - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 # - Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 #
-# This software is provided by the copyright holder "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed. 
-# In no event shall the copyright holder be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; 
+# This software is provided by the copyright holder "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed.
+# In no event shall the copyright holder be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits;
 # or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
 #
 #
 #
 
-import time 
+import time
 
-def measure_time(func):
-    '''
+
+def measure_time(should_print=True):
+    """
     Decorator function to measure the time a function takes to execute
     and then print it to the console.
-    '''
-    # Get the function name for the print statement
 
-    func_name = func.__name__
-    def wrapper(*args, **kwargs):
-        # Get the start time
-        start = time.time()
-        # Execute the function
-        result = func(*args, **kwargs)
-        # Get the end time
-        end = time.time()
-        # Calculate the elapsed time
-        elapsed = round(float(end - start), 5)
-        # Print the elapsed time
-        print('Elapsed time for {} is : {}'.format(func_name, elapsed))
-        # Return the result of the function
-        return result
+    Use like:
+    @measure_time(should_print = show_benchmarking_output_bool)
+    def function_to_measure():
+        pass
+
+    :param should_print: Whether the elapsed time should be printed to the console
+    :type should_print: bool
+    """
+
+    # Get the function name for the print statement
+    def wrapper(func):
+        func_name = func.__name__
+
+        def wrapper_func(*args, **kwargs):
+            # Get the start time
+            start = time.time()
+            # Execute the function
+            result = func(*args, **kwargs)
+            # Get the end time
+            end = time.time()
+            # Calculate the elapsed time
+            elapsed = round(float(end - start), 5)
+            # Print the elapsed time
+
+            print("Elapsed time for {} is : {}".format(func_name, elapsed))
+            # Return the result of the function
+            return result
+
+        if should_print:
+            return wrapper_func
+        else:
+            return func
+
     return wrapper

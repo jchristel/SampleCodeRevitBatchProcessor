bde02f3ab0dbca4b0423952f5caf60a774998e43
Jan Christel <jan.r.christel@gmail.com>
2024-01-15 08:28:27 +1100

    Update levels.py
    added get_levels_list_descending

--- src/duHast/Revit/Levels/levels.py
+++ src/duHast/Revit/Levels/levels.py
@@ -19,8 +19,8 @@ Revit levels helper functions.
 # - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 # - Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 #
-# This software is provided by the copyright holder "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed. 
-# In no event shall the copyright holder be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; 
+# This software is provided by the copyright holder "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed.
+# In no event shall the copyright holder be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits;
 # or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
 #
 #
@@ -81,13 +81,31 @@ def get_levels_list_ascending(doc):
     return collector
 
 
+def get_levels_list_descending(doc):
+    """
+    Gets a filtered element collector of all levels in the model descending by project elevation.
+
+    Filters by category.
+
+    :param doc: Current Revit model document.
+    :type doc: Autodesk.Revit.DB.Document
+    :return: A filtered element collector of levels
+    :rtype: Autodesk.Revit.DB.FilteredElementCollector
+    """
+
+    levels_ascending = get_levels_list_ascending(doc)
+    levels_descending = levels_ascending.reverse()
+    return levels_descending
+
+
 def get_level_elevation_by_name(doc, level_name):
     level_elevation = None
     levels = get_levels_in_model(doc=doc)
     for level in levels:
-        if(level.Name == level_name):
+        if level.Name == level_name:
             return level.Elevation
     return level_elevation
+
 
 # ------------------------------------------------- filters --------------------------------------------------------------------
 

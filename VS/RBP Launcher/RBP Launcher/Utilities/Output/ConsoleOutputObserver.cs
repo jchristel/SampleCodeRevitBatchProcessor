752f1fd8c86f3faa80038cf30acb5d4f523c1a84
jchristel <jan.r.christel@gmail.com>
2024-02-02 18:55:51 +1100

    starting output routing

---
+++ VS/RBP Launcher/RBP Launcher/Utilities/Output/ConsoleOutputObserver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RBP_Launcher.Utilities.Output
+{
+    // Console observer implementation
+    public class ConsoleOutputObserver : RBP_Launcher.Interfaces.IOutputObserver
+    {
+        public void Update(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
---
+++ VS/RBP Launcher/RBP Launcher/Utilities/Output/WpfFormOutputObserver.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RBP_Launcher.Utilities.Output
+{
+    public class WpfFormOutputObserver : RBP_Launcher.Interfaces.IOutputObserver
+    { 
+        // private readonly  form; // Replace Form1 with your actual WPF form class
+
+        //public WpfFormOutputObserver(Form1 form)
+        //{
+        //    this.form = form;
+        //}
+
+        public void Update(string message)
+        {
+            // Update WPF form with the message
+            // Example:
+            // form.UpdateOutput(message);
+        }
+    }
+}

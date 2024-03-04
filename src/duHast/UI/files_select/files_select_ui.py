3d6037d1d636c65704b9e437b9a03ffbfd4f062b
jchristel <jan.r.christel@gmail.com>
2024-03-04 14:13:30 +1100

    clean up files_select_ui
    removed no longer required code

--- src/duHast/UI/files_select/files_select_ui.py
+++ src/duHast/UI/files_select/files_select_ui.py
@@ -17,38 +17,20 @@ ROOT_REPO_DIRECTORY = os.path.join(
 sys.path.append(ROOT_REPO_DIRECTORY)
 
 
-# import file item class
-# from duHast.UI import file_item as fi
-# import file list methods
+# import UI helper
 from duHast.UI import file_list as fl
-
-# import UI class
-from duHast.UI import ui_file_select as UIFs
-
-# import settings class
 from duHast.UI.Objects import file_select_settings as set
-
-# import workloader utils
+from duHast.UI.file_list import get_revit_files_for_processing
+from duHast.UI.Objects.XamlLoader import XamlLoader
 from duHast.UI import workloader as wl
 
-from duHast.Utilities.files_csv import read_csv_file, get_first_row_in_csv_file
-from duHast.Utilities.files_tab import get_first_row_in_file_no_strip
 from duHast.Utilities.files_io import (
     file_exist,
-    get_file_size,
-    FILE_SIZE_IN_KB,
-    is_back_up_file,
 )
 from duHast.Utilities.console_out import output_with_time_stamp
-from duHast.UI.Objects.file_item import MyFileItem
-
-# import bim360 utils from Library
-from duHast.Revit.BIM360 import util_bim_360 as ub360
 
 # xaml helper classes
 from ViewModel import ViewModel
-from duHast.UI.Objects.XamlLoader import XamlLoader
-
 
 # Define the path to your XAML file
 CURRENT_SCRIPT_DIRECTORY = os.path.dirname(__file__)
@@ -68,18 +50,23 @@ def main(argv):
     got_args, settings = process_args(argv)
     if got_args:
         # retrieve revit file data
-        revit_files = get_file_data(settings)
+        revit_files = get_revit_files_for_processing(
+            settings.input_directory,
+            settings.incl_sub_dirs,
+            settings.revit_file_extension,
+        )
         # check whether this is a BIM360 project or file system and assign
         # data retriever method accordingly
         if is_bim_360_file(revit_files):
             get_data = fl.bucket_to_task_list_bim_360
         else:
             get_data = fl.bucket_to_task_list_file_system
+        
         # check if anything came back
         if len(revit_files) > 0:
             # load xaml
             xaml = XamlLoader(XAML_FULL_FILE_NAME)
-            # form window
+            # forms window
             window = xaml.Root
             # view model initialise
             view_model = ViewModel(
@@ -89,23 +76,28 @@ def main(argv):
                 settings.revit_file_extension,
                 settings.output_file_num,
                 settings.incl_sub_dirs,
-                "2022",
+                ["2022"],
                 True,
-                revit_files,
             )
             # assign view model to xaml
             xaml.Root.DataContext = view_model
+            
             # testing
             # xaml.grid1.Background = Brushes.DarkSalmon
             # show application
             Application().Run(xaml.Root)
+            
             # debugging
             print("Destination Path {}".format(xaml.Root.DataContext.destination_path))
             print("Source Path {}".format(xaml.Root.DataContext.source_path))
             print(
-                "Source File Extension {}".format(xaml.Root.DataContext.source_file_extension)
+                "Source File Extension {}".format(
+                    xaml.Root.DataContext.source_file_extension
+                )
             )
-            print("Include sub dirs {}\n".format(xaml.Root.DataContext.include_sub_dirs))
+            print(
+                "Include sub dirs {}\n".format(xaml.Root.DataContext.include_sub_dirs)
+            )
             print("selected files {}\n".format(len(view_model.selected_files)))
             print("filter {}\n".format(view_model.filter_text))
             for rf in view_model.revit_files:
@@ -113,11 +105,16 @@ def main(argv):
             for frf in view_model.filtered_revit_files:
                 print("filtered files: {}".format(frf.name))
             print("debug {}".format("\n".join(view_model.debug)))
+            
+            
+            # get the dialog result (ok vs cancel buttons)
             ui_result = xaml.Root.DataContext.DialogResult
             if ui_result:
                 # build bucket list
                 buckets = wl.distribute_workload(
-                    settings.output_file_num, view_model.selected_files, fl.get_file_size
+                    settings.output_file_num,
+                    view_model.selected_files,
+                    fl.get_file_size,
                 )
                 # write out file lists
                 counter = 0
@@ -136,16 +133,18 @@ def main(argv):
                 sys.exit(2)
         else:
             # show message box
-            output_with_time_stamp("No files found matching extension: {}!".format(settings.revit_file_extension))
+            output_with_time_stamp(
+                "No files found matching extension: {}!".format(
+                    settings.revit_file_extension
+                )
+            )
             sys.exit(2)
     else:
         # invalid or no args provided... get out
         sys.exit(1)
-    
-    
+
     # Set the SizeToContent property of the window
     # xaml.Root.SizeToContent = SizeToContent.WidthAndHeight
-
 
 
 def process_args(argv):
@@ -236,150 +235,6 @@ def process_args(argv):
         output_file_number,
         revit_file_extension,
     )
-
-
-def get_directory_from_file_path(file_path):
-    """
-    Returns the directory from a fully qualified file path.
-
-    :param file_path: A fully qualified file path.
-    :type file_path: str
-
-    :return: A fully qualified directory path.
-        On exception an empty string.
-    :rtype: str
-    """
-
-    try:
-        value = os.path.dirname(file_path)
-    except Exception:
-        value = ""
-    return value
-
-
-def get_file_data(settings):
-    """
-    Retrieves Revit file data from either:
-
-        - directory on a file server
-
-        - a text file containing BIM 360 project data
-            - text file needs to be a .csv
-            - format:
-                - 0 Revit Version:YYYY,Project GUID, File GUID, file size, BIM 360 file path
-
-    :param settings: A file select settings object instance.
-    :type settings: :class:`.FileSelectionSettings`
-
-    :return: List of file items
-    :rtype: [:class:`.FileItem`]
-    """
-
-    revit_files = []
-    # check whether input is a directory path or a text file (csv) containing BIM 360 data
-    # since we tested for a valid path initially it will need to be either one...
-    try:
-        if os.path.isfile(settings.input_directory):
-            # got a text file...extract BIM 360 data
-            revit_files = get_file_data_from_text_file(settings.input_directory)
-        elif os.path.isdir(settings.input_directory):
-            # check a to search for files is to include sub dirs
-            revit_files_unfiltered = []
-            if settings.incl_sub_dirs:
-                # get revit files in input dir and subdirs
-                revit_files_unfiltered = fl.get_revit_files_incl_sub_dirs(
-                    settings.input_directory, settings.revit_file_extension
-                )
-            else:
-                # get revit files in input dir
-                revit_files_unfiltered = fl.get_revit_files(
-                    settings.input_directory, settings.revit_file_extension
-                )
-            # check for max path violations!
-            # The specified path, file name, or both are too long. The fully qualified file name must be less than 260 characters, and the directory name must be less than 248 characters.
-            for revit_file in revit_files_unfiltered:
-                # remove any back up files from selection
-                if is_back_up_file(os.path.basename(revit_file.name)) == False:
-                    if (
-                        len(os.path.dirname(os.path.abspath(revit_file.name))) < 248
-                        and len(revit_file.name) < 260
-                    ):
-                        revit_files.append(revit_file)
-                    else:
-                        output_with_time_stamp(
-                            "Max path length violation: {}".format(revit_file.name)
-                        )
-                        output_with_time_stamp("File has been removed from selection!")
-    except Exception as e:
-        output_with_time_stamp(
-            "An exception occurred during BIM360 file read! {}".format(e)
-        )
-        # return an empty list which will cause this script to abort
-        revit_files = []
-    return revit_files
-
-
-def get_files_from_list_file(file_path_csv):
-    """
-    Reads server based file data, the fully qualified file path, from a task file list file in csv format.
-
-    :param filePathCSV: The fully qualified file path to the task list file.
-    :type filePathCSV: str
-
-    :return: A list of MyFileItem objects. If an exception occurred an empty list will be returned.
-    :rtype: :class:`.MyFileItem`
-    """
-
-    revit_files = []
-    try:
-        # read the CSV into rows
-        rows = read_csv_file(file_path_csv)
-        # check whether anything came back
-        if len(rows) > 0:
-            # process rows
-            for rowData in rows:
-                if len(rowData) > 0:
-                    file_size = get_file_size(rowData[0], FILE_SIZE_IN_KB)
-                    dummy = MyFileItem(rowData[0], file_size)
-                    revit_files.append(dummy)
-    except Exception as e:
-        output_with_time_stamp(
-            "An exception occurred during row processing! {}".format(e)
-        )
-        # return an empty list which will cause this script to abort
-        revit_files = []
-    return revit_files
-
-
-def get_file_data_from_text_file(file_path):
-    """
-    Reads a file server based task list file. This file can either be a BIM360 task list file or \
-        a task list file containing file server based file path in a single column.
-
-    :param filePath: The fully qualified file path to the task list file.
-    :type filePath: str
-    :return: A list of MyFileItem objects.
-    :rtype: :class:`.MyFileItem`
-    """
-
-    files = []
-    # if file is empty an empty list will be returned
-    # also need to check whether this is a csv file...
-    if file_path.lower().endswith(".csv"):
-        # list of entries in first row
-        row = get_first_row_in_csv_file(file_path)
-    else:
-        row = get_first_row_in_file_no_strip(file_path)
-        # make sure we get a list of entries
-        if row is not None:
-            row = row.split("\t")
-    if row is not None:
-        # bim 360 or autodesk construction cloud files have at least 3 entries
-        if len(row) > 2:
-            files = ub360.get_bim_360_file_data(file_path)
-        else:
-            files = get_files_from_list_file(file_path)
-    return files
 
 
 def is_bim_360_file(revit_files):

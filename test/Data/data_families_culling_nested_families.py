07aebbb80fea4dd5316ad94fcaf0cc3f8424704a
Jan Christel <jan.r.christel@gmail.com>
2024-03-13 14:16:28 +1100

    updated tests

--- test/Data/data_families_culling_nested_families.py
+++ test/Data/data_families_culling_nested_families.py
@@ -30,37 +30,16 @@ This module contains data read from file tests .
 import os
 import sys
 
-_debug = True
-if(_debug):
-    # get the script location
-    SCRIPT_DIRECTORY = os.path.dirname(__file__)
-    # build flow directory name
-    FLOW_DIRECTORY = os.path.dirname(SCRIPT_DIRECTORY)
-    print("flow dir: {}".format(FLOW_DIRECTORY))
-    TEST_DIRECTORY = os.path.dirname(FLOW_DIRECTORY)
-    print("test dir: {}".format(TEST_DIRECTORY))
-    # build duHast and duHast test directories
-    DU_HAST_TEST_DIRECTORY = os.path.dirname(os.path.dirname(FLOW_DIRECTORY))
-    print("DU_HAST_TEST_DIRECTORY: {}".format(DU_HAST_TEST_DIRECTORY))
+from test.utils import test
 
-    DU_HAST_DIRECTORY = os.path.join(DU_HAST_TEST_DIRECTORY, r"duHast\src")
-    print("DU_HAST_DIRECTORY: {}".format( DU_HAST_DIRECTORY))
-
-    sys.path.insert(0, DU_HAST_DIRECTORY)
-
-    # add the directories to path
-    sys.path += [
-        TEST_DIRECTORY,
-    ]
-
-from utils import test
-
-from duHast.Revit.Family.Data.family_base_data_utils import nested_family, _cull_data_block
+from duHast.Revit.Family.Data.family_base_data_utils import nested_family, _check_data_blocks_for_overlap, _cull_data_block
 
 class DataCullingNestedFamilies(test.Test):
+    
     def __init__(self):
         # store document in base class
         super(DataCullingNestedFamilies, self).__init__(test_name="data_culling_nested_families")
+
 
     def test(self):
         """
@@ -98,7 +77,46 @@ class DataCullingNestedFamilies(test.Test):
                     categoryPath ="a_category::a_category::a_category::a_category",
                     hostFamily="test 2"
                 )
+            test_fam_4 = nested_family(
+                    name="test 4",
+                    category = "a category",
+                    filePath=r"\a test path\test 3",
+                    rootPath="a host family::test 1::test 2::test 4",
+                    categoryPath ="a_category::a_category::a_category::a_category",
+                    hostFamily="test 2"
+                )
+            test_fam_5 = nested_family(
+                    name="test 5",
+                    category = "a category",
+                    filePath=r"\a test path\test 3",
+                    rootPath="a host family::test 1::test 5",
+                    categoryPath ="a_category::a_category::a_category::a_category",
+                    hostFamily="test 1"
+                )
             
+            # setup test data block
+            data = [
+                test_fam_1,
+            ]
+
+            # set up expected result
+            expected_result = [test_fam_1]
+
+            cull = None
+            try:
+                # cull the data block
+                cull = _cull_data_block(family_base_nested_data_block=data)
+            except Exception as e:
+                message += "\nexception in culling data block\n {} \n".format(e)
+                
+
+            message += "\nresult from data: {} \nvs \nexpected: {}".format(
+                        sorted(cull), sorted(expected_result)
+            )
+            
+            # it should only return the longest unique root path which is family 3 in the test data
+            assert sorted(cull) == sorted(expected_result)
+
             # setup test data block
             data = [
                 test_fam_1,
@@ -111,27 +129,46 @@ class DataCullingNestedFamilies(test.Test):
 
             cull = None
             try:
-                print("here 0")
                 # cull the data block
-                cull = _cull_data_block(data)
-                print
+                cull = _cull_data_block(family_base_nested_data_block=data)
             except Exception as e:
-                print(e)
+                message += "\nexception in culling data block\n {} \n".format(e)
+                
 
-            message = "result from data: {} \nvs \nexpected: {}".format(
+            message += "\nresult from data: {} \nvs \nexpected: {}".format(
                         sorted(cull), sorted(expected_result)
             )
             
-            #it should only return the longest unique root path which is family 3 in the test data
+            # it should only return the longest unique root path which is family 3 in the test data
             assert sorted(cull) == sorted(expected_result)
             
+            # test nesting with multiple branches
+            data = [
+                test_fam_1,
+                test_fam_2,
+                test_fam_3,
+                test_fam_4,
+                test_fam_5
+            ]
 
-            #flag_one, message_one = self.call_with_temp_directory(action_one)
-            #flag_two, message_two = self.call_with_temp_directory(action_two)
+            # set up expected result
+            expected_result = [test_fam_3, test_fam_4, test_fam_5]
 
-            #flag = flag_one & flag_two
-            #message = "{}\n{}".format(message_one, message_two)
+            cull = None
+            try:
+                # cull the data block
+                cull = _cull_data_block(family_base_nested_data_block=data)
+            except Exception as e:
+                message += "\nexception in culling data block\n {} \n".format(e)
+            
+            message += "\nresult from data: {} \nvs \nexpected: {}".format(
+                        sorted(cull), sorted(expected_result)
+            )
 
+            # it should only return the longest unique root path which are 3 families in the test data
+            assert sorted(cull) == sorted(expected_result)
+
+            flag = False
         except Exception as e:
             flag = False
             message = (
@@ -141,9 +178,3 @@ class DataCullingNestedFamilies(test.Test):
             )
         return flag, message
     
-
-if __name__ == "__main__":
-    
-    test = DataCullingNestedFamilies()
-    flag, message = test.test()
-    print(flag, message)

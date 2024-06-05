e31cb5832f4d6ace6c68c4dcc5a376e0e5a21584
Jan Christel <jan.r.christel@gmail.com>
2024-06-05 14:03:43 +1000

    Update family_data_container.py
    completed adding storage data to container

--- src/duHast/Revit/Family/Data/Objects/family_data_container.py
+++ src/duHast/Revit/Family/Data/Objects/family_data_container.py
@@ -41,23 +41,60 @@ from duHast.Utilities.Objects import base
 
 import System
 import json
-from duHast.Revit.Family.Data.Objects.family_base_data_storage import FamilyBaseDataStorage
-from duHast.Revit.Categories.Data.Objects.category_data_storage import FamilyCategoryDataStorage
-from duHast.Revit.LinePattern.Data.Objects.line_pattern_data_storage import FamilyLinePatternDataStorage
-from duHast.Revit.SharedParameters.Data.Objects.shared_parameter_data_storage import FamilySharedParameterDataStorage
-from duHast.Revit.Warnings.Data.Objects.warnings_data_storage import FamilyWarningsDataStorage
+from duHast.Revit.Family.Data.Objects.family_base_data_storage import (
+    FamilyBaseDataStorage,
+)
+from duHast.Revit.Categories.Data.Objects.category_data_storage import (
+    FamilyCategoryDataStorage,
+)
+from duHast.Revit.LinePattern.Data.Objects.line_pattern_data_storage import (
+    FamilyLinePatternDataStorage,
+)
+from duHast.Revit.SharedParameters.Data.Objects.shared_parameter_data_storage import (
+    FamilySharedParameterDataStorage,
+)
+from duHast.Revit.Warnings.Data.Objects.warnings_data_storage import (
+    FamilyWarningsDataStorage,
+)
 
 
 class FamilyDataContainer(base.Base):
     def __init__(
         self,
-        family_base_data_storage = None,
-        category_data_storage = None,
-        line_pattern_data_storage = None,
-        shared_parameter_data_storage = None,
-        warnings_data_storage = None,
+        family_base_data_storage=None,
+        category_data_storage=None,
+        line_pattern_data_storage=None,
+        shared_parameter_data_storage=None,
+        warnings_data_storage=None,
         **kwargs
     ):
+        """
+        Family data container class.
+
+        Contains all instances of the following storage classes from reports read from files:
+            
+                - FamilyBaseDataStorage
+                - CategoryDataStorage
+                - WarningsDataStorage
+                - LinePatternDataStorage
+                - SharedParameterDataStorage
+        
+        Note: There is the possibility that only one of the storage classes is populated, this is due to the fact that the data is read from separate files and not all data is always present in each file.
+        
+        :param family_base_data_storage: a list of FamilyBaseDataStorage instances
+        :type family_base_data_storage: list
+        :param category_data_storage: a list of FamilyCategoryDataStorage instances
+        :type category_data_storage: list
+        :param line_pattern_data_storage: a list of FamilyLinePatternDataStorage instances
+        :type line_pattern_data_storage: list
+        :param shared_parameter_data_storage: a list of FamilySharedParameterDataStorage instances
+        :type shared_parameter_data_storage: list
+        :param warnings_data_storage: a list of FamilyWarningsDataStorage instances
+        :type warnings_data_storage: list
+        :param kwargs: _description_
+        :type kwargs: _type_
+        :raises ValueError: type mismatch if any of the past in lists are not of the correct type
+        """
 
         # forwards all unused arguments
         # ini super class to allow multi inheritance in children!
@@ -73,86 +110,84 @@ class FamilyDataContainer(base.Base):
 
         # populate class from past in data
         # start with family base data storage
+        # if a valid family base data storage instance is passed in this will also set the following properties:
+        # family_name, family_nesting_path, family_category, family_category_nesting_path, is_root_family, family_file_path
+
+        # family base data default is an empty list
         self.family_base_data_storage = []
         if family_base_data_storage is None:
-            # no change required 
+            # no change required
             pass
         else:
             if isinstance(family_base_data_storage, list):
-                # check if every member of the list is an instance of FamilyBaseDataStorage
-                if all(isinstance(x, FamilyBaseDataStorage) for x in family_base_data_storage):
-                    # should only ever be one entry!
-                    if(len(family_base_data_storage)>1):
-                        raise ValueError ("family_base_data_storage can only ever have one entry")
-                    else:
-                        self.add_family_base_data_storage(family_base_data_storage)
-                else:
-                    raise ValueError("family_base_data_storage must be a list of FamilyBaseDataStorage")
+                # should only ever be one entry!
+                if len(family_base_data_storage) > 1:
+                    raise ValueError(
+                        "family_base_data_storage can only ever have one entry"
+                    )
+                # add to class property
+                for storage_instance in family_base_data_storage:
+                    self.add_family_base_data_storage(storage_instance)
             elif isinstance(family_base_data_storage, FamilyBaseDataStorage):
+                # add to class property
                 self.add_family_base_data_storage(family_base_data_storage)
             else:
-                raise ValueError("family_base_data_storage must be a list or FamilyBaseDataStorage")
-        
-        # category data storage
+                raise ValueError(
+                    "family_base_data_storage must be a list or FamilyBaseDataStorage"
+                )
+
+        # category data storage default is an empty list
+        self.category_data_storage = []
         if category_data_storage is None:
-            self.category_data_storage = []
+            # no change required
+            pass
         else:
             if isinstance(category_data_storage, list):
-                # check if every member of the list is an instance of FamilyCategoryDataStorage
-                if all(isinstance(x, FamilyCategoryDataStorage) for x in category_data_storage):
-                    self.category_data_storage = category_data_storage
-                else:
-                    raise ValueError("category_data_storage must be a list of FamilyCategoryDataStorage")
-            elif isinstance(category_data_storage, FamilyCategoryDataStorage):
-                self.category_data_storage = [category_data_storage]
+                # add to class property
+                for storage_instance in category_data_storage:
+                    self.add_category_data_storage(storage_instance)
             else:
-                raise ValueError("category_data_storage must be a list or FamilyCategoryDataStorage")
-        
-        # line pattern data storage
+                self.add_category_data_storage(category_data_storage)
+
+        # line pattern data storage default is an empty list
+        self.line_pattern_data_storage = []
         if line_pattern_data_storage is None:
-            self.line_pattern_data_storage = []
+            # no change required
+            pass
         elif isinstance(line_pattern_data_storage, list):
-            # check if every member of the list is an instance of FamilyLinePatternDataStorage
-            if all(isinstance(x, FamilyLinePatternDataStorage) for x in line_pattern_data_storage):
-                self.line_pattern_data_storage = line_pattern_data_storage
-            else:
-                raise ValueError("line_pattern_data_storage must be a list of FamilyLinePatternDataStorage")
-        elif isinstance(line_pattern_data_storage, FamilyLinePatternDataStorage):
-            self.line_pattern_data_storage = [line_pattern_data_storage]
+            # add to class property
+            for storage_instance in line_pattern_data_storage:
+                self.add_line_pattern_data_storage(storage_instance)
         else:
-            raise ValueError("line_pattern_data_storage must be a list or FamilyLinePatternDataStorage")
-        
-        # shared parameter data storage
+            self.add_line_pattern_data_storage(line_pattern_data_storage)
+
+        # shared parameter data storage default is an empty list
+        self.shared_parameter_data_storage = []
         if shared_parameter_data_storage is None:
-            self.shared_parameter_data_storage = []
+            # no change required
+            pass
         elif isinstance(shared_parameter_data_storage, list):
-            # check if every member of the list is an instance of FamilySharedParameterDataStorage
-            if all(isinstance(x, FamilySharedParameterDataStorage) for x in shared_parameter_data_storage):
-                self.shared_parameter_data_storage = shared_parameter_data_storage
-            else:
-                raise ValueError("shared_parameter_data_storage must be a list of FamilySharedParameterDataStorage")
-        elif isinstance(shared_parameter_data_storage, FamilySharedParameterDataStorage):
-            self.shared_parameter_data_storage = [shared_parameter_data_storage]
+            # add to class property
+            for storage_instance in shared_parameter_data_storage:
+                self.add_shared_parameter_data_storage(storage_instance)
         else:
-            raise ValueError("shared_parameter_data_storage must be a list or FamilySharedParameterDataStorage")
-        
-        # warnings data storage
+            self.add_shared_parameter_data_storage(shared_parameter_data_storage)
+
+        # warnings data storage default is an empty list
+        self.warnings_data_storage = []
         if warnings_data_storage is None:
-            self.warnings_data_storage = []
+            # no change required
+            pass
         elif isinstance(warnings_data_storage, list):
-            # check if every member of the list is an instance of FamilyWarningsDataStorage
-            if all(isinstance(x, FamilyWarningsDataStorage) for x in warnings_data_storage):
-                self.warnings_data_storage = warnings_data_storage
-            else:
-                raise ValueError("warnings_data_storage must be a list of FamilyWarningsDataStorage")
-        elif isinstance(warnings_data_storage, FamilyWarningsDataStorage):
-            self.warnings_data_storage = [warnings_data_storage]
+            # add to class property
+            for storage_instance in warnings_data_storage:
+                self.add_warnings_data_storage(storage_instance)
         else:
-            raise ValueError("warnings_data_storage must be a list or FamilyWarningsDataStorage")
-    
+            self.add_warnings_data_storage(warnings_data_storage)
+
     def add_family_base_data_storage(self, other):
         """
-        Will add a family base data storage instance and infere some other class properties from it.
+        Will add a family base data storage instance and infer some other class properties from it.
 
         Note: if storage data properties root_name_path and root_category_path are different to the current values
         all other storage properties will be wiped to avoid mismatches!
@@ -162,25 +197,153 @@ class FamilyDataContainer(base.Base):
         :raises ValueError: _description_
         """
         # check is correct object type
-        if isinstance(other, FamilyBaseDataStorage)==False:
+        if isinstance(other, FamilyBaseDataStorage) == False:
             raise ValueError("other must be a list of FamilyBaseDataStorage")
-        
-        # there is always only going to be one entry in list!
-        if (len(self.family_base_data_storage)>0):
+
+        # there is always only going to be one entry in list, pop the existing one if there is one
+        if len(self.family_base_data_storage) > 0:
             self.family_base_data_storage.pop()
-        
-        # add to class property
+
+        # add new one to class property
         self.family_base_data_storage.append(other)
 
-        # set other class properties based on storage:
-        # check if nesting path and category nesting path are different to the current values
+        # check if nesting path and category nesting path are different to the current values but not None!
         # if so wipe the other storage properties to avoid mismatches!
-        
+        if (
+            self.family_nesting_path != other.root_name_path
+            and self.family_nesting_path != None
+            or self.family_category_nesting_path != other.root_category_path
+            and self.family_category_nesting_path != None
+        ):
+            self.category_data_storage = []
+            self.line_pattern_data_storage = []
+            self.shared_parameter_data_storage = []
+            self.warnings_data_storage = []
 
+        # set other class properties based on storage
         self.family_name = other.family_name
         self.family_file_path = other.family_file_path
         self.family_nesting_path = other.root_name_path
         self.family_category_nesting_path = other.root_category_path
-        
-        
-        
+
+        # check if this is a root family
+        if "::" in other.root_name_path:
+            self.is_root_family = False
+        else:
+            self.is_root_family = True
+
+    def add_category_data_storage(self, other):
+        """
+        Will add a category data storage instance.
+
+        Note: an exception will be raised if family_nesting_path and family_category_nesting_path do not match the current values!
+
+        :param other: a data storage instance
+        :type other: FamilyCategoryDataStorage
+        :raises ValueError: Type mismatch
+        """
+        # check is correct object type
+        if isinstance(other, FamilyCategoryDataStorage) == False:
+            raise ValueError("other must be a list of FamilyCategoryDataStorage")
+
+        # check if nesting path and category nesting path are different to the current values but not None!
+        # if so wipe throw error!
+        if (
+            self.family_nesting_path != other.root_name_path
+            and self.family_nesting_path != None
+            or self.family_category_nesting_path != other.root_category_path
+            and self.family_category_nesting_path != None
+        ):
+            raise ValueError(
+                "other root_name_path and root_category_path must match current values"
+            )
+
+        # add to class property
+        self.category_data_storage.append(other)
+
+    def add_line_pattern_data_storage(self, other):
+        """
+        Will add a line pattern data storage instance.
+
+        Note: an exception will be raised if family_nesting_path and family_category_nesting_path do not match the current values!
+
+        :param other: a data storage instance
+        :type other: FamilyLinePatternDataStorage
+        :raises ValueError: Type mismatch
+        """
+        # check is correct object type
+        if isinstance(other, FamilyLinePatternDataStorage) == False:
+            raise ValueError("other must be a list of FamilyLinePatternDataStorage")
+
+        # check if nesting path and category nesting path are different to the current values but not None!
+        # if so wipe throw error!
+        if (
+            self.family_nesting_path != other.root_name_path
+            and self.family_nesting_path != None
+            or self.family_category_nesting_path != other.root_category_path
+            and self.family_category_nesting_path != None
+        ):
+            raise ValueError(
+                "other root_name_path and root_category_path must match current values"
+            )
+
+        # add to class property
+        self.line_pattern_data_storage.append(other)
+
+    def add_shared_parameter_data_storage(self, other):
+        """
+        Will add a shared parameter data storage instance.
+
+        Note: an exception will be raised if family_nesting_path and family_category_nesting_path do not match the current values!
+
+        :param other: a data storage instance
+        :type other: FamilySharedParameterDataStorage
+        :raises ValueError: Type mismatch
+        """
+        # check is correct object type
+        if isinstance(other, FamilySharedParameterDataStorage) == False:
+            raise ValueError("other must be a list of FamilySharedParameterDataStorage")
+
+        # check if nesting path and category nesting path are different to the current values but not None!
+        # if so wipe throw error!
+        if (
+            self.family_nesting_path != other.root_name_path
+            and self.family_nesting_path != None
+            or self.family_category_nesting_path != other.root_category_path
+            and self.family_category_nesting_path != None
+        ):
+            raise ValueError(
+                "other root_name_path and root_category_path must match current values"
+            )
+
+        # add to class property
+        self.shared_parameter_data_storage.append(other)
+
+    def add_warnings_data_storage(self, other):
+        """
+        Will add a warnings data storage instance.
+
+        Note: an exception will be raised if family_nesting_path and family_category_nesting_path do not match the current values!
+
+        :param other: a data storage instance
+        :type other: FamilyWarningsDataStorage
+        :raises ValueError: Type mismatch
+        """
+        # check is correct object type
+        if isinstance(other, FamilyWarningsDataStorage) == False:
+            raise ValueError("other must be a list of FamilyWarningsDataStorage")
+
+        # check if nesting path and category nesting path are different to the current values but not None!
+        # if so wipe throw error!
+        if (
+            self.family_nesting_path != other.root_name_path
+            and self.family_nesting_path != None
+            or self.family_category_nesting_path != other.root_category_path
+            and self.family_category_nesting_path != None
+        ):
+            raise ValueError(
+                "other root_name_path and root_category_path must match current values"
+            )
+
+        # add to class property
+        self.warnings_data_storage.append(other)

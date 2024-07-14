f83ab2bdb526a19d391d0d485697e9e0b9729d6b
Jan <jan.r.christel@gmail.com>
2024-07-14 14:26:14 +1000

    Update family_report_reader.py
    added license text and updated notes

--- src/duHast/Revit/Family/Data/family_report_reader.py
+++ src/duHast/Revit/Family/Data/family_report_reader.py
@@ -4,6 +4,28 @@ Family report data utility module containing functions to read the data from fil
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 
+#
+# License:
+#
+#
+# Revit Batch Processor Sample Code
+#
+# BSD License
+# Copyright 2024, Jan Christel
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+# - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# - Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+#
+# This software is provided by the copyright holder "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed.
+# In no event shall the copyright holder be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits;
+# or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
+#
+#
+
 import os
 from duHast.Utilities.files_csv import read_csv_file
 from duHast.Revit.Family.Data.Objects.ifamily_data_storage import IFamilyDataStorage
@@ -433,7 +455,7 @@ def read_data_into_families(path_to_data):
     """
     Read the data from the csv files in the directory and return a list of FamilyDataFamily objects.
 
-    :param path_to_data: The path to the directory containing the csv files.
+    :param path_to_data: The path to the directory containing the csv files or fully qualified file path to single report csv file.
     :type path_to_data: str
     :return: A Result object containing the list of FamilyDataFamily objects if successful.
     :rtype: Result
@@ -467,10 +489,13 @@ def read_data_into_families(path_to_data):
             # check if the family is already in the list
             compare_family_name = None
             compare_family_category = None
+            
             if container.is_root_family:
+                # if family is a root family use directly the containers family name and family category
                 compare_family_name = container.family_nesting_path
                 compare_family_category = container.family_category_nesting_path
             else:
+                # if a nested family get, need to get to the root family at the top of the nesting tree
                 root_name_path_chunks = container.family_nesting_path.split(
                     NESTING_SEPARATOR
                 )

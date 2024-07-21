e57492a9b4fbf9cf78eaece03e3d14a1fe06ae83
Github Action <action@github.com>
2024-07-21 01:29:48 +0000

    Committed changes made by GitHub Action

--- docsource/conf.py
+++ docsource/conf.py
@@ -29,7 +29,7 @@ author = "Jan Christel"
 
 # The full version, including alpha/beta/rc tags
 # need to be in double high commas for bumpver to recognize this a the version number
-release = "1.0.0"
+release = "1.1.0"
 
 
 # -- General configuration ---------------------------------------------------

698a7721c6b0729d39aceb41624ccaa19002364f
Jan Christel <jan.r.christel@gmail.com>
2024-08-02 16:47:45 +1000

    Update loadable_family_categories.py
    amended  Autodesk import statement

--- src/duHast/Revit/Family/Utility/loadable_family_categories.py
+++ src/duHast/Revit/Family/Utility/loadable_family_categories.py
@@ -26,177 +26,177 @@ This module contains a number of lists containing loadable family categories.
 #
 #
 
-import Autodesk.Revit.DB as rdb
+from Autodesk.Revit.DB import BuiltInCategory
 from System.Collections.Generic import List
 
 #: This list contains 3D element categories and is used in obsolete revit family purge function
 #: any revit category commented out with note 'purged else where' can be found in list 'catsLoadableThreeDOther'
-CATEGORIES_LOADABLE_3D = List[rdb.BuiltInCategory](
+CATEGORIES_LOADABLE_3D = List[BuiltInCategory](
     [
-        # rdb.BuiltInCategory.OST_CableTrayFitting,  purged else where
-        rdb.BuiltInCategory.OST_Casework,
-        rdb.BuiltInCategory.OST_Columns,
-        rdb.BuiltInCategory.OST_CommunicationDevices,
-        # rdb.BuiltInCategory.OST_ConduitFitting,  purged else where
-        # rdb.BuiltInCategory.OST_CurtainWallPanels, purged else where
-        rdb.BuiltInCategory.OST_DataDevices,
-        # rdb.BuiltInCategory.OST_DetailComponents, purged else where
-        rdb.BuiltInCategory.OST_Doors,
-        # rdb.BuiltInCategory.OST_DuctAccessory,  purged else where
-        # rdb.BuiltInCategory.OST_DuctTerminal, purged else where
-        # rdb.BuiltInCategory.OST_DuctFitting,  purged else where
-        rdb.BuiltInCategory.OST_ElectricalEquipment,
-        rdb.BuiltInCategory.OST_ElectricalFixtures,
-        rdb.BuiltInCategory.OST_Entourage,
-        rdb.BuiltInCategory.OST_FireAlarmDevices,
-        rdb.BuiltInCategory.OST_Furniture,
-        rdb.BuiltInCategory.OST_FurnitureSystems,
-        rdb.BuiltInCategory.OST_GenericModel,
-        rdb.BuiltInCategory.OST_LightingFixtures,
-        rdb.BuiltInCategory.OST_LightingDevices,
-        rdb.BuiltInCategory.OST_MechanicalEquipment,
-        rdb.BuiltInCategory.OST_NurseCallDevices,
-        rdb.BuiltInCategory.OST_Parking,
-        # rdb.BuiltInCategory.OST_PipeAccessory,  purged else where
-        # rdb.BuiltInCategory.OST_PipeFitting,  purged else where
-        rdb.BuiltInCategory.OST_Planting,
-        rdb.BuiltInCategory.OST_PlumbingFixtures,
-        # rdb.BuiltInCategory.OST_ProfileFamilies, #purged elsewhere
-        rdb.BuiltInCategory.OST_SecurityDevices,
-        rdb.BuiltInCategory.OST_Site,
-        rdb.BuiltInCategory.OST_SpecialityEquipment,
-        rdb.BuiltInCategory.OST_Sprinklers,
-        # rdb.BuiltInCategory.OST_StairsRailingBaluster, #purged else where
-        rdb.BuiltInCategory.OST_StructuralColumns,
-        rdb.BuiltInCategory.OST_StructuralFoundation,
-        rdb.BuiltInCategory.OST_StructuralFraming,
-        rdb.BuiltInCategory.OST_TitleBlocks,
-        rdb.BuiltInCategory.OST_TelephoneDevices,
-        rdb.BuiltInCategory.OST_Windows,
+        # BuiltInCategory.OST_CableTrayFitting,  purged else where
+        BuiltInCategory.OST_Casework,
+        BuiltInCategory.OST_Columns,
+        BuiltInCategory.OST_CommunicationDevices,
+        # BuiltInCategory.OST_ConduitFitting,  purged else where
+        # BuiltInCategory.OST_CurtainWallPanels, purged else where
+        BuiltInCategory.OST_DataDevices,
+        # BuiltInCategory.OST_DetailComponents, purged else where
+        BuiltInCategory.OST_Doors,
+        # BuiltInCategory.OST_DuctAccessory,  purged else where
+        # BuiltInCategory.OST_DuctTerminal, purged else where
+        # BuiltInCategory.OST_DuctFitting,  purged else where
+        BuiltInCategory.OST_ElectricalEquipment,
+        BuiltInCategory.OST_ElectricalFixtures,
+        BuiltInCategory.OST_Entourage,
+        BuiltInCategory.OST_FireAlarmDevices,
+        BuiltInCategory.OST_Furniture,
+        BuiltInCategory.OST_FurnitureSystems,
+        BuiltInCategory.OST_GenericModel,
+        BuiltInCategory.OST_LightingFixtures,
+        BuiltInCategory.OST_LightingDevices,
+        BuiltInCategory.OST_MechanicalEquipment,
+        BuiltInCategory.OST_NurseCallDevices,
+        BuiltInCategory.OST_Parking,
+        # BuiltInCategory.OST_PipeAccessory,  purged else where
+        # BuiltInCategory.OST_PipeFitting,  purged else where
+        BuiltInCategory.OST_Planting,
+        BuiltInCategory.OST_PlumbingFixtures,
+        # BuiltInCategory.OST_ProfileFamilies, #purged elsewhere
+        BuiltInCategory.OST_SecurityDevices,
+        BuiltInCategory.OST_Site,
+        BuiltInCategory.OST_SpecialityEquipment,
+        BuiltInCategory.OST_Sprinklers,
+        # BuiltInCategory.OST_StairsRailingBaluster, #purged else where
+        BuiltInCategory.OST_StructuralColumns,
+        BuiltInCategory.OST_StructuralFoundation,
+        BuiltInCategory.OST_StructuralFraming,
+        BuiltInCategory.OST_TitleBlocks,
+        BuiltInCategory.OST_TelephoneDevices,
+        BuiltInCategory.OST_Windows,
     ]
 )
 
 #: Contains 3D family categories which needed specific purge code, rather then checking for unplaced family instances.
 #: i.e. built in revit type settings
-CATEGORIES_LOADABLE_3D_OTHER = List[rdb.BuiltInCategory](
+CATEGORIES_LOADABLE_3D_OTHER = List[BuiltInCategory](
     [
-        rdb.BuiltInCategory.OST_CableTrayFitting,
-        rdb.BuiltInCategory.OST_ConduitFitting,
-        rdb.BuiltInCategory.OST_CurtainWallPanels,
-        rdb.BuiltInCategory.OST_DetailComponents,
-        rdb.BuiltInCategory.OST_DuctAccessory,
-        rdb.BuiltInCategory.OST_DuctTerminal,
-        rdb.BuiltInCategory.OST_DuctFitting,
-        rdb.BuiltInCategory.OST_PipeAccessory,
-        rdb.BuiltInCategory.OST_PipeFitting,
-        rdb.BuiltInCategory.OST_ProfileFamilies,
-        rdb.BuiltInCategory.OST_StairsRailingBaluster,
+        BuiltInCategory.OST_CableTrayFitting,
+        BuiltInCategory.OST_ConduitFitting,
+        BuiltInCategory.OST_CurtainWallPanels,
+        BuiltInCategory.OST_DetailComponents,
+        BuiltInCategory.OST_DuctAccessory,
+        BuiltInCategory.OST_DuctTerminal,
+        BuiltInCategory.OST_DuctFitting,
+        BuiltInCategory.OST_PipeAccessory,
+        BuiltInCategory.OST_PipeFitting,
+        BuiltInCategory.OST_ProfileFamilies,
+        BuiltInCategory.OST_StairsRailingBaluster,
     ]
 )
 
 #: This list contains 2D element categories and is used in obsolete revit family purge function.\
 #: any revit category commented out with note 'purged else where' can be found in list 'catsLoadableTagsOther'
-CATEGORIES_LOADABLE_TAGS = List[rdb.BuiltInCategory](
+CATEGORIES_LOADABLE_TAGS = List[BuiltInCategory](
     [
-        rdb.BuiltInCategory.OST_CurtainWallPanelTags,
-        rdb.BuiltInCategory.OST_AreaTags,
-        rdb.BuiltInCategory.OST_CaseworkTags,
-        # rdb.BuiltInCategory.OST_CalloutHeads, #purged separately
-        rdb.BuiltInCategory.OST_CeilingTags,
-        rdb.BuiltInCategory.OST_DataDeviceTags,
-        rdb.BuiltInCategory.OST_DetailComponentTags,
-        rdb.BuiltInCategory.OST_DoorTags,
-        rdb.BuiltInCategory.OST_DuctAccessoryTags,
-        rdb.BuiltInCategory.OST_DuctFittingTags,
-        rdb.BuiltInCategory.OST_DuctInsulationsTags,
-        rdb.BuiltInCategory.OST_DuctLiningsTags,
-        rdb.BuiltInCategory.OST_DuctTags,
-        rdb.BuiltInCategory.OST_DuctTerminalTags,
-        rdb.BuiltInCategory.OST_ElectricalCircuitTags,
-        rdb.BuiltInCategory.OST_ElectricalEquipmentTags,
-        rdb.BuiltInCategory.OST_ElectricalFixtureTags,
-        # rdb.BuiltInCategory.OST_ElevationMarks, #purged separately
-        rdb.BuiltInCategory.OST_FabricAreaTags,
-        rdb.BuiltInCategory.OST_FabricReinforcementTags,
-        rdb.BuiltInCategory.OST_FireAlarmDeviceTags,
-        rdb.BuiltInCategory.OST_FlexDuctTags,
-        rdb.BuiltInCategory.OST_FlexPipeTags,
-        rdb.BuiltInCategory.OST_FloorTags,
-        rdb.BuiltInCategory.OST_FoundationSlabAnalyticalTags,
-        rdb.BuiltInCategory.OST_FurnitureSystemTags,
-        rdb.BuiltInCategory.OST_GenericModelTags,
-        # rdb.BuiltInCategory.OST_GenericAnnotation, # purged separately tricky one...some of these might be used in dimensions for instance...
-        # rdb.BuiltInCategory.OST_GridHeads, # purged separately
-        rdb.BuiltInCategory.OST_InternalAreaLoadTags,
-        rdb.BuiltInCategory.OST_InternalLineLoadTags,
-        rdb.BuiltInCategory.OST_InternalPointLoadTags,
-        rdb.BuiltInCategory.OST_IsolatedFoundationAnalyticalTags,
-        rdb.BuiltInCategory.OST_KeynoteTags,
+        BuiltInCategory.OST_CurtainWallPanelTags,
+        BuiltInCategory.OST_AreaTags,
+        BuiltInCategory.OST_CaseworkTags,
+        # BuiltInCategory.OST_CalloutHeads, #purged separately
+        BuiltInCategory.OST_CeilingTags,
+        BuiltInCategory.OST_DataDeviceTags,
+        BuiltInCategory.OST_DetailComponentTags,
+        BuiltInCategory.OST_DoorTags,
+        BuiltInCategory.OST_DuctAccessoryTags,
+        BuiltInCategory.OST_DuctFittingTags,
+        BuiltInCategory.OST_DuctInsulationsTags,
+        BuiltInCategory.OST_DuctLiningsTags,
+        BuiltInCategory.OST_DuctTags,
+        BuiltInCategory.OST_DuctTerminalTags,
+        BuiltInCategory.OST_ElectricalCircuitTags,
+        BuiltInCategory.OST_ElectricalEquipmentTags,
+        BuiltInCategory.OST_ElectricalFixtureTags,
+        # BuiltInCategory.OST_ElevationMarks, #purged separately
+        BuiltInCategory.OST_FabricAreaTags,
+        BuiltInCategory.OST_FabricReinforcementTags,
+        BuiltInCategory.OST_FireAlarmDeviceTags,
+        BuiltInCategory.OST_FlexDuctTags,
+        BuiltInCategory.OST_FlexPipeTags,
+        BuiltInCategory.OST_FloorTags,
+        BuiltInCategory.OST_FoundationSlabAnalyticalTags,
+        BuiltInCategory.OST_FurnitureSystemTags,
+        BuiltInCategory.OST_GenericModelTags,
+        # BuiltInCategory.OST_GenericAnnotation, # purged separately tricky one...some of these might be used in dimensions for instance...
+        # BuiltInCategory.OST_GridHeads, # purged separately
+        BuiltInCategory.OST_InternalAreaLoadTags,
+        BuiltInCategory.OST_InternalLineLoadTags,
+        BuiltInCategory.OST_InternalPointLoadTags,
+        BuiltInCategory.OST_IsolatedFoundationAnalyticalTags,
+        BuiltInCategory.OST_KeynoteTags,
         # BuiltInCategory.OST_LevelHeads, #purged separately
-        rdb.BuiltInCategory.OST_LightingDeviceTags,
-        rdb.BuiltInCategory.OST_LightingFixtureTags,
-        rdb.BuiltInCategory.OST_LineLoadTags,
-        rdb.BuiltInCategory.OST_LinkAnalyticalTags,
-        rdb.BuiltInCategory.OST_MassTags,
-        rdb.BuiltInCategory.OST_MaterialTags,
-        rdb.BuiltInCategory.OST_MechanicalEquipmentTags,
-        rdb.BuiltInCategory.OST_MEPSpaceTags,
-        rdb.BuiltInCategory.OST_MultiCategoryTags,
-        rdb.BuiltInCategory.OST_NodeAnalyticalTags,
-        rdb.BuiltInCategory.OST_NurseCallDeviceTags,
-        rdb.BuiltInCategory.OST_ParkingTags,
-        rdb.BuiltInCategory.OST_PartTags,
-        rdb.BuiltInCategory.OST_PathReinTags,
-        rdb.BuiltInCategory.OST_PipeAccessoryTags,
-        rdb.BuiltInCategory.OST_PipeFittingTags,
-        rdb.BuiltInCategory.OST_PipeInsulationsTags,
-        rdb.BuiltInCategory.OST_PipeTags,
-        rdb.BuiltInCategory.OST_PlantingTags,
-        rdb.BuiltInCategory.OST_PlumbingFixtureTags,
-        rdb.BuiltInCategory.OST_RailingSystemTags,
-        rdb.BuiltInCategory.OST_RebarTags,
-        # rdb.BuiltInCategory.OST_ReferenceViewerSymbol, #purged separately
-        rdb.BuiltInCategory.OST_RevisionCloudTags,
-        rdb.BuiltInCategory.OST_RoofTags,
-        rdb.BuiltInCategory.OST_RoomTags,
-        # rdb.BuiltInCategory.OST_SectionHeads, #purged separately
-        rdb.BuiltInCategory.OST_SecurityDeviceTags,
-        rdb.BuiltInCategory.OST_SitePropertyLineSegmentTags,
-        rdb.BuiltInCategory.OST_SitePropertyTags,
-        rdb.BuiltInCategory.OST_SpecialityEquipmentTags,
-        # rdb.BuiltInCategory.OST_SpotElevSymbols, #purged elsewhere
-        rdb.BuiltInCategory.OST_SprinklerTags,
-        rdb.BuiltInCategory.OST_StairsLandingTags,
-        rdb.BuiltInCategory.OST_StairsRailingTags,
-        rdb.BuiltInCategory.OST_StairsRunTags,
-        rdb.BuiltInCategory.OST_StairsSupportTags,
-        rdb.BuiltInCategory.OST_StairsTags,
-        rdb.BuiltInCategory.OST_StairsTriserTags,
-        rdb.BuiltInCategory.OST_StructConnectionTags,
-        rdb.BuiltInCategory.OST_StructuralColumnTags,
-        rdb.BuiltInCategory.OST_StructuralFoundationTags,
-        rdb.BuiltInCategory.OST_StructuralFramingTags,
-        rdb.BuiltInCategory.OST_StructuralStiffenerTags,
-        rdb.BuiltInCategory.OST_TelephoneDeviceTags,
-        rdb.BuiltInCategory.OST_TrussTags,
-        # rdb.BuiltInCategory.OST_ViewportLabel, #purged elsewhere
-        rdb.BuiltInCategory.OST_WallTags,
-        rdb.BuiltInCategory.OST_WindowTags,
+        BuiltInCategory.OST_LightingDeviceTags,
+        BuiltInCategory.OST_LightingFixtureTags,
+        BuiltInCategory.OST_LineLoadTags,
+        BuiltInCategory.OST_LinkAnalyticalTags,
+        BuiltInCategory.OST_MassTags,
+        BuiltInCategory.OST_MaterialTags,
+        BuiltInCategory.OST_MechanicalEquipmentTags,
+        BuiltInCategory.OST_MEPSpaceTags,
+        BuiltInCategory.OST_MultiCategoryTags,
+        BuiltInCategory.OST_NodeAnalyticalTags,
+        BuiltInCategory.OST_NurseCallDeviceTags,
+        BuiltInCategory.OST_ParkingTags,
+        BuiltInCategory.OST_PartTags,
+        BuiltInCategory.OST_PathReinTags,
+        BuiltInCategory.OST_PipeAccessoryTags,
+        BuiltInCategory.OST_PipeFittingTags,
+        BuiltInCategory.OST_PipeInsulationsTags,
+        BuiltInCategory.OST_PipeTags,
+        BuiltInCategory.OST_PlantingTags,
+        BuiltInCategory.OST_PlumbingFixtureTags,
+        BuiltInCategory.OST_RailingSystemTags,
+        BuiltInCategory.OST_RebarTags,
+        # BuiltInCategory.OST_ReferenceViewerSymbol, #purged separately
+        BuiltInCategory.OST_RevisionCloudTags,
+        BuiltInCategory.OST_RoofTags,
+        BuiltInCategory.OST_RoomTags,
+        # BuiltInCategory.OST_SectionHeads, #purged separately
+        BuiltInCategory.OST_SecurityDeviceTags,
+        BuiltInCategory.OST_SitePropertyLineSegmentTags,
+        BuiltInCategory.OST_SitePropertyTags,
+        BuiltInCategory.OST_SpecialityEquipmentTags,
+        # BuiltInCategory.OST_SpotElevSymbols, #purged elsewhere
+        BuiltInCategory.OST_SprinklerTags,
+        BuiltInCategory.OST_StairsLandingTags,
+        BuiltInCategory.OST_StairsRailingTags,
+        BuiltInCategory.OST_StairsRunTags,
+        BuiltInCategory.OST_StairsSupportTags,
+        BuiltInCategory.OST_StairsTags,
+        BuiltInCategory.OST_StairsTriserTags,
+        BuiltInCategory.OST_StructConnectionTags,
+        BuiltInCategory.OST_StructuralColumnTags,
+        BuiltInCategory.OST_StructuralFoundationTags,
+        BuiltInCategory.OST_StructuralFramingTags,
+        BuiltInCategory.OST_StructuralStiffenerTags,
+        BuiltInCategory.OST_TelephoneDeviceTags,
+        BuiltInCategory.OST_TrussTags,
+        # BuiltInCategory.OST_ViewportLabel, #purged elsewhere
+        BuiltInCategory.OST_WallTags,
+        BuiltInCategory.OST_WindowTags,
     ]
 )
 
 #: Contains 2D family categories which needed specific purge code, rather then checking for unplaced family instances
 #: i.e. built in revit type settings
-CATEGORIES_LOADABLE_TAGS_OTHER = List[rdb.BuiltInCategory](
+CATEGORIES_LOADABLE_TAGS_OTHER = List[BuiltInCategory](
     [
-        rdb.BuiltInCategory.OST_CalloutHeads,
-        rdb.BuiltInCategory.OST_ElevationMarks,
-        rdb.BuiltInCategory.OST_GenericAnnotation,
-        rdb.BuiltInCategory.OST_GridHeads,
-        rdb.BuiltInCategory.OST_LevelHeads,
-        rdb.BuiltInCategory.OST_ReferenceViewerSymbol,
-        rdb.BuiltInCategory.OST_SectionHeads,
-        rdb.BuiltInCategory.OST_SpotElevSymbols,
-        rdb.BuiltInCategory.OST_ViewportLabel,
+        BuiltInCategory.OST_CalloutHeads,
+        BuiltInCategory.OST_ElevationMarks,
+        BuiltInCategory.OST_GenericAnnotation,
+        BuiltInCategory.OST_GridHeads,
+        BuiltInCategory.OST_LevelHeads,
+        BuiltInCategory.OST_ReferenceViewerSymbol,
+        BuiltInCategory.OST_SectionHeads,
+        BuiltInCategory.OST_SpotElevSymbols,
+        BuiltInCategory.OST_ViewportLabel,
     ]
 )

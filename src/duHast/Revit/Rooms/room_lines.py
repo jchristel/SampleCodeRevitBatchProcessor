271750881eee5d1856096333e7973ab5a3e06bd3
Jan Christel <jan.r.christel@gmail.com>
2024-08-16 14:39:18 +1000

    Update room_lines.py
    added room separation line filters:
    filter_room_separation_lines_by_level_id
    filter_room_separation_lines_by_phase_created_older_than_active_view_phase

--- src/duHast/Revit/Rooms/room_lines.py
+++ src/duHast/Revit/Rooms/room_lines.py
@@ -27,7 +27,9 @@ Revit API utility functions for Room separation lines.
 #
 #
 
-from Autodesk.Revit.DB import FilteredElementCollector, BuiltInCategory
+from Autodesk.Revit.DB import BuiltInCategory, ElementId, FilteredElementCollector
+
+from duHast.Revit.Common.phases import get_all_phases_in_order
 
 
 def get_room_separation_lines(doc):
@@ -103,6 +105,28 @@ def get_room_separation_lines_by_level_names(doc):
     return room_separation_lines
 
 
+def sort_room_separation_line_by_phase_created(room_separation_lines):
+    """
+    Sorts the room separation lines by phase created id
+
+    :param room_separation_lines: list of room separation lines
+    :type room_separation_lines:list
+
+    :return: A dictionary of room separation lines, where key is the phase created id, and values are the associated room separation lines
+    :rtype: {ElementId:[ModelLine]}
+
+    """
+    sorted_lines = {}
+
+    for line in room_separation_lines:
+        if line.CreatedPhaseId in sorted_lines:
+            sorted_lines[line.CreatedPhaseId].append(line)
+        else:
+            sorted_lines[line.CreatedPhaseId] = [line]
+
+    return sorted_lines
+
+
 def filter_room_separation_lines_by_phase_created(
     doc, room_separation_lines, phase_created_name
 ):
@@ -152,7 +176,7 @@ def filter_room_separation_lines_by_phase_demolished(
 
     # loop over lines and filter them by phase created
     for room_line in room_separation_lines:
-        
+
         # get the demo phase of the room separation line
         phase_Line_demoed = doc.GetElement(room_line.DemolishedPhaseId)
 
@@ -173,3 +197,73 @@ def filter_room_separation_lines_by_phase_demolished(
 
     # return any matches
     return room_separation_lines_filtered
+
+
+def filter_room_separation_lines_by_phase_created_older_than_active_view_phase(
+    doc, room_separation_lines, upper_bound_phase_id
+):
+    """
+    Filter room separation lines by phase created and an upper bound phase id.
+    If upper bound phase Id is and ElementId.InvalidElementId the list will be returned unchanged.
+
+    :param doc: The Revit document.
+    :type doc: Document
+    :param room_separation_lines: A list of room separation lines
+    :type room_separation_lines: []
+    :param upper_bound_phase_id: the id of the phase to be used as upper bound
+    :type upper_bound_phase_id: Autodesk.Revit.ElementId
+
+    :return: A list of room separation lines filtered by phase created.
+    :rtype: list
+    """
+
+    if(isinstance(upper_bound_phase_id, ElementId)== False):
+        raise TypeError ("upper_bound_phase_id needs to be of type ElementId. But is: {}".format(upper_bound_phase_id))
+    
+    if upper_bound_phase_id == ElementId.InvalidElementId:
+        return room_separation_lines
+
+    # filtered lines
+    lines_filtered = []
+
+    # phases in model
+    phases_ordered = get_all_phases_in_order(doc)
+
+    # flip the phases_ordered list to get the index of the active view phase
+    phase_id_index_by_phase_id = {}
+    counter = 0
+    for phase in phases_ordered:
+        phase_id_index_by_phase_id[phase[0]] = counter
+        counter += 1
+
+    # index of upper bound
+    upper_bound_phase_id_index = phase_id_index_by_phase_id[upper_bound_phase_id]
+
+    # loop over lines and filter away any room separation lines newer then the upper bound phase id
+    for line in room_separation_lines:
+        line_phase_index = phase_id_index_by_phase_id[line.CreatedPhaseId]
+        if line_phase_index <= upper_bound_phase_id_index:
+            lines_filtered.append(line)
+
+    return lines_filtered
+
+
+def filter_room_separation_lines_by_level_id(room_separation_lines, level_id):
+    """
+    Filter room separation lines by level id its placed on.
+
+    :param room_separation_lines: A list of room separation lines
+    :type room_separation_lines: []
+    :param level_id: The id of he level to be filtered by.
+    :type level_id: Autodesk.Revit.ElementId
+
+    :return: A list of room separation lines filtered by level placed on.
+    :rtype: list
+    """
+
+    lines_filtered = []
+    for room_line in room_separation_lines:
+        if room_line.LevelId == level_id:
+            lines_filtered.append(room_line)
+
+    return lines_filtered

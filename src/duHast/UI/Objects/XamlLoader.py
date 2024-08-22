3ffc033b1892e339a6b2e93bd284c7664943eeba
Jan Christel <jan.r.christel@gmail.com>
2024-08-22 11:25:35 +1000

    Update XamlLoader.py
    updated xaml loader class to work with wpf in Revit context...might need testing with current WPF  UI classes?

--- src/duHast/UI/Objects/XamlLoader.py
+++ src/duHast/UI/Objects/XamlLoader.py
@@ -25,30 +25,50 @@ https://markheath.net/post/wpf-and-mvvm-in-ironpython
 # - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
 # - Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 #
-# This software is provided by the copyright holder "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed. 
-# In no event shall the copyright holder be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits; 
+# This software is provided by the copyright holder "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed.
+# In no event shall the copyright holder be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits;
 # or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
 #
 #
 #
 
 
-
-
-import clr
-clr.AddReference("PresentationFramework")
-clr.AddReference("PresentationCore")
-
-from System.IO import File
 from System.Windows.Markup import XamlReader
 
 class XamlLoader(object):
-    def __init__(self, xamlPath):
-        stream = File.OpenRead(xamlPath)
-        self.Root = XamlReader.Load(stream)
-        
-    def __getattr__(self, item):
-        """Maps values to attributes.
-        Only called if there *isn't* an attribute with this name
+    def __init__(self, xaml_path):
         """
-        return self.Root.FindName(item)
+        Loads a XAML file and provides access to its objects.
+
+        :param xaml_path: The path to the XAML file to load
+        :type xaml_path: str
+        """
+        self.Root = self.load_xaml(xaml_path)
+    
+    def load_xaml(self, xaml_path):
+        """
+        Load the XAML file and parse it.
+
+        :param xaml_path: The path to the XAML file to load
+        :type xaml_path: str
+        :return: The root element of the loaded XAML
+        :rtype: System.Windows.UIElement
+        """
+        # Read the XAML file
+        with open(xaml_path, 'r') as file:
+            xaml_content = file.read()
+        
+        # Parse the XAML content
+        return XamlReader.Parse(xaml_content)
+
+    def __getattr__(self, item):
+        """
+        Maps values to attributes.
+        Only called if there *isn't* an attribute with this name.
+
+        :param item: The name of the attribute
+        :return: The value of the attribute, if found
+        """
+        if hasattr(self.Root, 'FindName'):
+            return self.Root.FindName(item)
+        raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{item}'")

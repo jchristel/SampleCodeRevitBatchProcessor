9acf394641ebccd4207937299d726502a9a4d12a
jchristel <jan.r.christel@gmail.com>
2024-08-25 18:13:39 +1000

    Update RoomViewModel.py
    formatting updates

--- Samples/WPF/RoomsInModel/ViewModels/RoomViewModel.py
+++ Samples/WPF/RoomsInModel/ViewModels/RoomViewModel.py
@@ -1,4 +1,3 @@
-
 from Models.Room import Room
 from duHast.UI.Objects.ViewModelBase import ViewModelBase
 
@@ -8,22 +7,18 @@ class RoomViewModel(ViewModelBase):
     A view model for the rooms.
     """
 
-
     def __init__(self, room):
         super(RoomViewModel, self).__init__()
 
         if not isinstance(room, Room):
             raise TypeError("room must be of type Room")
-        
+
         # store the room
         self.room = room
-        
+
         # store the room properties
         self.room_id = str(room.room_id.room_id_integer)
         self.room_name = room.room_name
         self.room_number = room.room_number
         self.phase = room.phase
         self.level = room.level
-    
-    
-    

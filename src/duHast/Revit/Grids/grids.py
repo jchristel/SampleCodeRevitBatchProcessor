7fcf3930a02addfa126e1bfccfc379059af165e3
Jan Christel <jan.r.christel@gmail.com>
2024-09-06 11:28:30 +1000

    Update grids.py
    bug fix in get_linear_grids_in_model_by_view

--- src/duHast/Revit/Grids/grids.py
+++ src/duHast/Revit/Grids/grids.py
@@ -51,6 +51,7 @@ from Autodesk.Revit.DB import (
     Grid,
     Line,
     FilteredElementCollector,
+    View,
 )
 
 
@@ -151,10 +152,14 @@ def get_linear_grids_in_model_by_view(doc, view):
     :rtype: [Autodesk.Revit.DB.Grid]
     """
 
+    # do some type checking
+    if(isinstance(view, View)==False):
+        raise TypeError("view needs to be of type Autodesk.Revit.DB.View. Got {} instead.".format(type(view)))
+    
     # get all grids in view
-    grids = get_grids_in_view(doc, view.Id).ToElements()
+    grids = get_grids_in_view(doc, view).ToElements()
     # get all multi segment grids in view
-    grids_multi = get_multi_segmented_grids_in_view(doc, view.Id).ToElements()
+    grids_multi = get_multi_segmented_grids_in_view(doc, view).ToElements()
     # get the individual grid id's making up the multi segment grid
     segment_grid_ids = get_all_segment_grid_ids(grids_multi)
     # set up placeholder to be returned

ae5f69a19642f0d21da24a24ee317c12fd54022e
Jan Christel <jan.r.christel@gmail.com>
2024-10-08 13:59:45 +1100

    Update data_base.py
    fixed super init statement

--- src/duHast/Data/Utils/data_base.py
+++ src/duHast/Data/Utils/data_base.py
@@ -41,7 +41,7 @@ class DataBase(base.Base):
 
         # forwards all unused arguments
         # ini super class to allow multi inheritance in children!
-        super(DataBase, self).__init__(**kwargs)
+        super(DataBase, self).__init__()
         self.data_type = data_type
 
     @property

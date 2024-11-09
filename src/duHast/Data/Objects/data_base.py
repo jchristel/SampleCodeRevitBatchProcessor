f82e8ede702246186b5c2c367188e20de688a6c2
jchristel <jan.r.christel@gmail.com>
2024-11-09 17:31:32 +1100

    Update data_base.py
    bug fix in inheritance

--- src/duHast/Data/Objects/data_base.py
+++ src/duHast/Data/Objects/data_base.py
@@ -41,7 +41,7 @@ class DataBase(Base):
 
         # forwards all unused arguments
         # ini super class to allow multi inheritance in children!
-        super(DataBase, self).__init__()
+        super(DataBase, self).__init__(**kwargs)
         self.data_type = data_type
         if isinstance(self.data_type, str) == False:
             raise TypeError(

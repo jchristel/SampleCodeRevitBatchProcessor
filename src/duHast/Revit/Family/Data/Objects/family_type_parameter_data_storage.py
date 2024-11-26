0175af286aca7aee340b07808f638aa05db133bc
Jan <jan.r.christel@gmail.com>
2024-11-27 08:03:54 +1100

    working on type data reporting

---
+++ src/duHast/Revit/Family/Data/Objects/family_type_parameter_data_storage.py
@@ -0,0 +1,47 @@
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Class for family parameter data storage class.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Used to store values of family parameters by family type.
+
+"""
+
+#
+# License:
+#
+#
+# Revit Batch Processor Sample Code
+#
+# BSD License
+# Copyright 2024, Jan Christel
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+# - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# - Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+#
+# This software is provided by the copyright holder "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed.
+# In no event shall the copyright holder be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits;
+# or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
+#
+#
+#
+
+from duHast.Revit.Family.Data.Objects import ifamily_data_storage as IFamDataStorage
+from duHast.Utilities.Objects.base import Base
+
+class FamilyTypeParameterDataStorage(Base):
+
+    # data type for this class ( used in reports as first entry per row )
+    data_type = "FamilyTypeParameter"
+
+    # number of properties in this class ( used in report reader function )
+    number_of_properties = 5
+
+    def __init__(
+        self
+    ):
+        super(FamilyTypeParameterDataStorage, self).__init__()

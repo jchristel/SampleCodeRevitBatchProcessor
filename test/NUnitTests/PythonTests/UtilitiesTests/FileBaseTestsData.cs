e6701ea14f65c21e57785bc7fde202d57947dabb
Jan <jan.r.christel@gmail.com>
2024-12-27 17:41:42 +1100

    Update FileBaseTestsData.cs
    re-formatting code...needs testing

--- test/NUnitTests/PythonTests/UtilitiesTests/FileBaseTestsData.cs
+++ test/NUnitTests/PythonTests/UtilitiesTests/FileBaseTestsData.cs
@@ -43,28 +43,6 @@
             return (fileName, header, data);
         }
 
-        public static (string fileName, List<string> header, List<List<string>> data) DelimitedTestData_DataOnly(string tempDirectory,char delimiter)
-        {
-            // Arrange
-            string fileName = Path.Combine(tempDirectory, "report.csv");
-            List<string> header = new List<string> (); // No header
-            List<List<string>> data = new List<List<string>>
-            {
-                new List<string> { $"Value1{delimiter}Part1", "Value2" },
-                new List<string> { "Value3", $"Value4{delimiter}Part4" }
-            };
-            return (fileName, header, data);
-        }
-
-        public static (string fileName, List<string> header, List<List<string>> data) DelimitedTestData_HeaderOnly(string tempDirectory, char delimiter)
-        {
-            // Arrange
-            string fileName = Path.Combine(tempDirectory, "report.csv");
-            List<string> header = new List<string> { $"Header1{delimiter}Part1", $"Header2{delimiter}Part2" };// Header containing delimiter
-            List<List<string>> data = new List<List<string>>(); // No data rows
-            return (fileName, header, data);
-        }
-
         public static (string fileName, List<string> header, List<List<string>> data) DelimitedTestData_HeaderAndData(string tempDirectory, char delimiter)
         {
             // Arrange
@@ -78,16 +56,46 @@
             return (fileName, header, data);
         }
 
-
-        public static (string fileName, List<string> header, List<List<string>> data) NonASCIITestData(string tempDirectory, char delimiter)
+        public static (string fileName, List<string> header, List<List<string>> data) DelimitedTestData_HeaderOnly(string tempDirectory, char delimiter)
         {
+            // Arrange
             string fileName = Path.Combine(tempDirectory, "report.csv");
-            List<string> header = new List<string> { "Header1", "Header2" };
+            List<string> header = new List<string> { $"Header1{delimiter}Part1", $"Header2{delimiter}Part2" };// Header containing delimiter
+            List<List<string>> data = new List<List<string>>(); // No data rows
+            return (fileName, header, data);
+        }
+
+        public static (string fileName, List<string> header, List<List<string>> data) DelimitedTestData_DataOnly(string tempDirectory,char delimiter)
+        {
+            // Arrange
+            string fileName = Path.Combine(tempDirectory, "report.csv");
+            List<string> header = new List<string> (); // No header
             List<List<string>> data = new List<List<string>>
             {
-                new List<string> { "Välue1", "Välue2" },
-                new List<string> { "Välue3", "Välue4" }
+                new List<string> { $"Value1{delimiter}Part1", "Value2" },
+                new List<string> { "Value3", $"Value4{delimiter}Part4" }
             };
+            return (fileName, header, data);
+        }
+
+        public static (string fileName, List<string> header, List<List<string>> data) NonUTF8TestData_HeaderAndData(string tempDirectory, char delimiter)
+        {
+            string fileName = Path.Combine(tempDirectory, "report.csv");
+            List<string> header = new List<string> { "Hëader1", "Hëader2" }; // Non-UTF-8 characters in header
+            List<List<string>> data = new List<List<string>>
+            {
+                new List<string> { "Value1", "Value2" },
+                new List<string> { "Value3", "Välue4" } // Non-UTF-8 character in data
+            };
+            return (fileName, header, data);
+        }
+
+        public static (string fileName, List<string> header, List<List<string>> data) NonUTF8TestData_HeaderOnly(string tempDirectory, char delimiter)
+        {
+            // Arrange
+            string fileName = Path.Combine(tempDirectory, "report.csv");
+            List<string> header = new List<string> { "Hëader1", "Hëader2" }; // Non-UTF-8 characters in header
+            List<List<string>> data = new List<List<string>>(); // No data rows
             return (fileName, header, data);
         }
 
@@ -103,24 +111,26 @@
             return (fileName, header, data);
         }
 
-        public static (string fileName, List<string> header, List<List<string>> data) NonUTF8TestData_HeaderOnly(string tempDirectory, char delimiter)
+       public static (string fileName, List<string> header, List<List<string>> data) NonUTF8AndDelimitedTestData_HeaderAndData(string tempDirectory, char delimiter)
         {
             // Arrange
             string fileName = Path.Combine(tempDirectory, "report.csv");
-            List<string> header = new List<string> { "Hëader1", "Hëader2" }; // Non-UTF-8 characters in header
-            List<List<string>> data = new List<List<string>>(); // No data rows
-            return (fileName, header, data);
-        }
-
-        public static (string fileName, List<string> header, List<List<string>> data) NonUTF8TestData_HeaderAndData(string tempDirectory, char delimiter)
-        {
-            string fileName = Path.Combine(tempDirectory, "report.csv");
-            List<string> header = new List<string> { "Hëader1", "Hëader2" }; // Non-UTF-8 characters in header
+            List<string> header = new List<string>{ $"Hëader1{delimiter}Part1", $"Hëader2{delimiter}Part2" }; // Non-UTF-8 characters and delimiter in header
             List<List<string>> data = new List<List<string>>
             {
                 new List<string> { "Value1", "Value2" },
-                new List<string> { "Value3", "Välue4" } // Non-UTF-8 character in data
+                new List<string> { "Value3", $"Välue4{delimiter}Part" } // Non-UTF-8 character and delimiter in data
             };
+            return (fileName, header, data);
+        }
+
+       public static (string fileName, List<string> header, List<List<string>> data) NonUTF8AndDelimitedTestData_HeaderOnly(string tempDirectory, char delimiter)
+        {
+            // Arrange
+            string fileName = Path.Combine(tempDirectory, "report.csv");
+            List<string> header = new List<string>{ $"Hëader1{delimiter}Part1", $"Hëader2{delimiter}Part2" }; // Non-UTF-8 characters and delimiter in header
+
+            List<List<string>> data = new List<List<string>>(); // No data rows
             return (fileName, header, data);
         }
 
@@ -137,17 +147,6 @@
             return (fileName, header, data);
         }
 
-        public static (string fileName, List<string> header, List<List<string>> data) NonUTF8AndDelimitedTestData_HeaderOnly(string tempDirectory, char delimiter)
-        {
-            // Arrange
-            string fileName = Path.Combine(tempDirectory, "report.csv");
-            List<string> header = new List<string>{ $"Hëader1{delimiter}Part1", $"Hëader2{delimiter}Part2" }; // Non-UTF-8 characters and delimiter in header
-
-            List<List<string>> data = new List<List<string>>(); // No data rows
-            return (fileName, header, data);
-        }
-
-
         public static (string fileName, List<string> header, List<List<string>> data) ArrangeMaxFieldSizeTestData(string tempDirectory, char delimiter)
         {
             // Arrange
@@ -157,6 +156,18 @@
             {
                 new List<string> { new string('A', 1000000), "Value2" }, // Large field size
                 new List<string> { "Value3", "Value4" }
+            };
+            return (fileName, header, data);
+        }
+
+        public static (string fileName, List<string> header, List<List<string>> data) NonASCIITestData(string tempDirectory, char delimiter)
+        {
+            string fileName = Path.Combine(tempDirectory, "report.csv");
+            List<string> header = new List<string> { "Header1", "Header2" };
+            List<List<string>> data = new List<List<string>>
+            {
+                new List<string> { "Välue1", "Välue2" },
+                new List<string> { "Välue3", "Välue4" }
             };
             return (fileName, header, data);
         }

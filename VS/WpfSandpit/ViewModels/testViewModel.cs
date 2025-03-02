23c6637719561ca2f9cb57092f3321e318646bb1
jchristel <jan.r.christel@gmail.com>
2025-03-03 07:23:36 +1100

    WPF Sandpit
    setting up completed

---
+++ VS/WpfSandpit/ViewModels/testViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using duHast.Utils.WPF.ViewModels;
+
+namespace WpfSandpit.ViewModels
+{
+    public class testViewModel :ViewModelBase
+    {
+    }
+}

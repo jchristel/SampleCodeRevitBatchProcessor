e5cbd3c4cd64edc00ea6b3370de384eed1b38b12
Jan Christel <jan.r.christel@gmail.com>
2025-03-05 10:09:14 +1100

    completed cloud data

--- Samples/pyRevit/Extensions/duHast.extension/duHast.tab/lib/cloud/cloud_guids.py
+++ Samples/pyRevit/Extensions/duHast.extension/duHast.tab/lib/cloud/cloud_guids.py
@@ -22,12 +22,13 @@
 
 
 from duHast.Utilities.Objects.result import Result
-
-
 from duHast.Revit.BIM360.bim_360 import get_model_bim_360_ids, get_model_file_size_from_GUID, get_model_file_size
 from duHast.Revit.Links.links import get_all_revit_link_instances
 from duHast.pyRevit.console_output import print_header
 from duHast.Revit.Common.revit_version import get_revit_version_number
+from duHast.Utilities.files_csv import write_report_data_as_csv
+
+from pyrevit.framework import Forms
 
 def get_cloud_guids_entry(doc, output, forms):
     """
@@ -48,15 +49,20 @@ def get_cloud_guids_entry(doc, output, forms):
 
     try:
 
+        data = []
         print_header("Getting cloud GUIDs")
 
         revit_version = get_revit_version_number(doc=doc)
         file_size = get_model_file_size(doc)
         # main document
         main_doc = get_model_bim_360_ids(doc)
-        print("version, project guid, file guid, file name")
-        print ("{revit_version},{main_doc[0]},{main_doc[1]},{file_size},{main_doc[2]}".format(revit_version, main_doc[0],main_doc[1],file_size, main_doc[2]))
-        all_docs = []
+
+        # build string for root model
+        data_root = ["{}".format(revit_version), "{}".format(main_doc[0]),"{}".format(main_doc[1]),"{}".format(file_size), main_doc[2]]
+        data.append(data_root)
+
+        print("version, project guid, file guid, file size, file name")
+        print (str.join(",", data_root))
         
         # get all links
         links = get_all_revit_link_instances(doc)
@@ -66,10 +72,37 @@ def get_cloud_guids_entry(doc, output, forms):
             try:
                 cloud_data =  get_model_bim_360_ids(link_doc)
                 file_size_link = get_model_file_size_from_GUID(revit_version=revit_version, file_guid=cloud_data[1])
-                print("{revit_version},{cloud_data[0]},{cloud_data[1]},{file_size_link},{cloud_data[2]}".format(revit_version,cloud_data[0],cloud_data[1],file_size_link,cloud_data[2]))
-            except Exception:
-                pass
+                data_cloud_link = ["{}".format(revit_version),"{}".format(cloud_data[0]),"{}".format(cloud_data[1]),"{}".format(file_size_link),cloud_data[2]]
+                data.append(data_cloud_link)
+                print (str.join(",", data_cloud_link))
+            except Exception as e:
+                print ("An exception occured when retrieving cloud data for links: {}".format(e))
     
+        # offer to safe data
+        file_name = None
+        sf_dlg = Forms.SaveFileDialog()  # (file_ext="json", title="Save template data")
+        sf_dlg.Filter = "Text files (*.csv)|*.csv|All files (*.*)|*.*"
+        if sf_dlg.ShowDialog() == Forms.DialogResult.OK:
+            file_name = sf_dlg.FileName
+
+        if file_name is None:
+            print("No file name for data file selected. Exiting.")
+            return_value.update_sep(False, "No file name for data file selected. Exiting.")
+            return return_value
+        
+        try:
+            # write data to file
+            write_result = write_report_data_as_csv(
+                file_name=file_name, header="", data=data
+            )
+            if write_result.status == False:
+                raise ValueError(write_result.message)
+            
+            return_value.append_message("Wrote file to: {}".format(file_name))
+        except Exception as e:
+            return_value.update_sep(
+                False, "Failed to write data file with exception: {}".format(e)
+            )
     except Exception as e:
         return_value.update_sep(
             False, "Failed to retrieve cloud guids with exception: {}".format(e)

1fcd8205412d6e111e374329cff1b00bbc5990f2
jchristel <jan.r.christel@gmail.com>
2025-04-01 20:45:03 +1100

    push it error messages out
    revised number of errors shown

--- VS/PushIt/RevitActions/PushSingleRoomDataToRevit.cs
+++ VS/PushIt/RevitActions/PushSingleRoomDataToRevit.cs
@@ -94,11 +94,22 @@ namespace duHast.PushIt.RevitActions
                 AddMessage($"Error updating single room in Revit: {ex.Message}", Utils.WPF.Stores.MessageTypes.Error);
             }
 
+            // get the ovearll error count
+            int erroCount = GetErrorMessages().Count;
+
             // check if any error messages were added
-            if (GetErrorMessages().Count > 0)
+            if (erroCount > 0)
             {
-                // return the message
-                return (string.Join("\n", GetErrorMessages()), Utils.WPF.Stores.MessageTypes.Error);
+                if (erroCount > 3)
+                {
+                    //return abridged message
+                    return ($"{erroCount} Errors occured. Refer to log file for details.", Utils.WPF.Stores.MessageTypes.Error);
+                }
+                else
+                {
+                    // return the message
+                    return (string.Join("\n", GetErrorMessages()), Utils.WPF.Stores.MessageTypes.Error);
+                }
             }
             else
             {

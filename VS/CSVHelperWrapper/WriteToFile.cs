af589171edb00451de9e45f3706a9bfb7ea6ea5a
jchristel <jan.r.christel@gmail.com>
2025-04-07 21:26:11 +1000

    VS - CSV wrapper
    started work on CSV wrapper function in .net to get around encoding errors in python

---
+++ VS/CSVHelperWrapper/WriteToFile.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using CsvHelper;
+using CsvHelper.Configuration;
+using System.Globalization;
+
+namespace CSVHelperWrapper
+{
+    public class WriteToFile
+    {
+        public static bool WriteToCsv(string filePath, List<string> header, List<List<string>> data)
+        {
+            try
+            {
+                using (var writer = new StreamWriter(filePath))
+                using (var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)))
+                {
+                    // Write header if provided
+                    if (header != null && header.Count > 0)
+                    {
+                        foreach (var field in header)
+                        {
+                            csv.WriteField(field);
+                        }
+                        csv.NextRecord();
+                    }
+
+                    // Write data rows
+                    foreach (var row in data)
+                    {
+                        foreach (var field in row)
+                        {
+                            csv.WriteField(field);
+                        }
+                        csv.NextRecord();
+                    }
+                }
+                return true; // Success
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error writing to CSV: {ex.Message}");
+                return false; // Failure
+            }
+        }
+    }
+}

c39dd389697a9be0e23d76286fcd88baf9ef74b5
jchristel <jan.r.christel@gmail.com>
2025-04-18 16:44:45 +1000

    Working on push it
    Updates to imports and area calcs

--- Samples/pyRevit/Extensions/duHast.extension/duHast.tab/lib/pushIt_associated/get_a_room.py
+++ Samples/pyRevit/Extensions/duHast.extension/duHast.tab/lib/pushIt_associated/get_a_room.py
@@ -1,6 +1,25 @@
+# License:
+#
+#
+# Revit Batch Processor Sample Code
+#
+# BSD License
+# Copyright 2025, Jan Christel
+# All rights reserved.
 
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
-import clr
+# - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# - Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+#
+# This software is provided by the copyright holder "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed.
+# In no event shall the copyright holder be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits;
+# or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
+#
+#
+#
+
 import sys
 import os
 
@@ -15,7 +34,8 @@ from duHast.Utilities.unit_conversion import convert_imperial_feet_to_metric_mm
 
 
 from duHast.Revit.UI.custom_selection_user import get_user_selection
-from duHast.Revit.DetailItems.filled_regions import  get_filled_region_curve_loops, create_filled_region_by_view
+from duHast.Revit.DetailItems.filled_regions import  get_filled_region_curve_loops
+from duHast.Revit.DetailItems.filled_regions_create import create_filled_region_by_view
 from duHast.Revit.DetailItems.curve_create import draw_2D_lines_on_bounding_box
 
 from duHast.Revit.Family.family_rename_loaded_families import  _rename_loaded_families
@@ -27,29 +47,22 @@ from duHast.Revit.Family.family_utils import get_family_instances_of_built_in_ca
 
 from duHast.Revit.Common.Geometry.transforms import move_xyz_to_zero
 from duHast.Revit.Common.Geometry.geometry import get_bounding_box_centre
-from duHast.Revit.Common.Geometry.curve import create_curve_loops_through_transform
+from duHast.Revit.Common.Geometry.curve_loops import create_curve_loops_through_transform, get_area_from_closed_curve_loop
 from duHast.Revit.Common.file_io import  save_as_family
 
 from duHast.Revit.SharedParameters.shared_parameters import get_all_shared_parameters
 
-
 from duHast.pyRevit.console_output import print_header, print_error
 
-
-#from geometry import draw_2D_lines_on_bounding_box
 
 from Autodesk.Revit.DB import BuiltInCategory, CurveLoop, Element, FilteredElementCollector, Family, ViewType, SaveAsOptions, Transform, UnitUtils, XYZ
 
 
 # add path to ifc dll
 SCRIPT_DIRECTORY = get_directory_path_from_file_path(__file__)
-sys.path.append(os.path.join(SCRIPT_DIRECTORY , "RevitAPIIFC.dll"))
-clr.AddReference("RevitAPIIFC")
-
-# used to calculate the area of the filled region
-from Autodesk.Revit.DB.IFC import ExporterIFCUtils
 
 
+# location of family templates
 FAMILY_TEMPLATE_DIRECTORY = os.path.join(get_directory_path_from_file_path(__file__),"templates")
 
 
@@ -215,27 +228,80 @@ def hook_up_shared_parameters(doc):
     return return_value
 
 
-def get_filled_region_area(filled_region):
+def get_filled_region_area(doc, filled_region):
+    """
+    Get the area of a filled region
+    
+    :param doc: The Revit document
+    :type doc: Autodesk.Revit.DB.Document
+    :param filled_region: The filled region to get the area from
+    :type filled_region: Autodesk.Revit.DB.FilledRegion
+    
+    :return: A result object with the area of the filled region
+    :rtype: Result
+    """
+    
     # set up a status tracker
     return_value = Result()
     try:
         # set the default value
-        area = -1
+        area = -1.0
         # get the curve loops of the filled region
         filled_region_curve_loops = get_filled_region_curve_loops(filled_region)
+        
+        # get the outer area of the filled region
+        area_outer_result = get_area_from_closed_curve_loop(
+            doc= doc, 
+            view = doc.ActiveView, 
+            curve_loop = filled_region_curve_loops[0],
+            filled_region_type_id = filled_region.GetTypeId(),
+        )
+        # check if the area was found
+        if area_outer_result.status == False:
+            message = "Failed to get outer area: {}".format(area_outer_result.message)
+            return_value.update_sep(False, message)
+            # return -1 as area
+            return_value.result.append(area)
+            return return_value
+        
+        # get the area of the outer loop
+        area_outer = area_outer_result.result[0]
+        
         # assume the second loop is the inner loop
         if len(filled_region_curve_loops) == 2:
             # get the area of the filled region
-            area = ExporterIFCUtils.ComputeAreaOfCurveLoops(List[CurveLoop](filled_region_curve_loops[1]))
+            area_inner_result = get_area_from_closed_curve_loop(
+                doc= doc, 
+                view = doc.ActiveView, 
+                curve_loop = filled_region_curve_loops[1],
+                filled_region_type_id = filled_region.GetTypeId(),
+            )
+            
+            # check if the area was found
+            if area_inner_result.status == False:
+                message = "Failed to get inner area: {}".format(area_inner_result.message)
+                return_value.update_sep(False, message)
+                # return -1 as area
+                return_value.result.append(area)
+                return return_value
+            
+            # get the area of the inner and outer loops
+            area_inner = area_inner_result.result[0]
+            
+            # calculate the area of the filled region
+            area_wall_half = (area_outer - area_inner)/2
+            area = area + area_wall_half
+            
         else:
-            area = ExporterIFCUtils.ComputeAreaOfCurveLoops(List[CurveLoop](filled_region_curve_loops[0]))
-        return_value.result.append(area)
+            # single loop filled region
+            return_value.append_message("Filled region has only one loop.")
+            return_value.result.append(area_outer)
+        
     except Exception as e:
         message = "Failed to get area: {}".format(e)
         return_value.result.append(area)
         return_value.update_sep(False, message)
     return return_value
-
 
 
 def update_overall_dimension_parameter_values(doc, bounding_box, filled_region):
@@ -251,7 +317,7 @@ def update_overall_dimension_parameter_values(doc, bounding_box, filled_region):
         depth_bbox =  convert_imperial_feet_to_metric_mm(bounding_box.Max.Y -bounding_box.Min.Y)
 
         # get the area of the filled region depending on the number of curve loops
-        area_of_interest_result =  get_filled_region_area(filled_region)
+        area_of_interest_result =  get_filled_region_area(doc=doc, filled_region=filled_region)
         print(area_of_interest_result)
         # check if the area of interest was found
         area_of_interest = area_of_interest_result.result[0]

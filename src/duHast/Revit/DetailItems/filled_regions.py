165a577de3a0be69ab773bec68beeb2bcbc613a7
jchristel <jan.r.christel@gmail.com>
2025-04-18 16:43:43 +1000

    Update filled_regions.py
    updating module functions. added:
    get_filled_region_loop_area
    removed create

--- src/duHast/Revit/DetailItems/filled_regions.py
+++ src/duHast/Revit/DetailItems/filled_regions.py
@@ -31,15 +31,13 @@ from duHast.Revit.DetailItems.Utility import (
     detail_items_type_sorting as rDetailItemTypeSort,
 )
 from duHast.Revit.DetailItems.detail_items import get_all_detail_types_by_category, FILLED_REGION_TYPE
-from duHast.Revit.Common.transaction import in_transaction
 from duHast.Utilities.Objects.result import Result
-
+from duHast.Revit.Common.Geometry.curve_loops import get_area_from_closed_curve_loop
 
 # import Autodesk
 from Autodesk.Revit.DB import (
     FilledRegion,
     FilteredElementCollector,
-    Transaction,
 )
 
 
@@ -98,44 +96,41 @@ def get_filled_region_curve_loops(filled_region):
     return curve_loops
 
 
-def create_filled_region_by_view(doc, view, curve_loops, filled_region_type):
+def get_filled_region_loop_area(doc, view, filled_region, loop_index=0):
     """
-    Creates a filled region in the specified view.
+    Gets loop area of a filled region.
+    This will create a temporary filled region using just the loop and get the area.
 
-    :param doc: Current Revit model document.
-    :type doc: Autodesk.Revit.DB.Document
-    :param view: The view to create the filled region in.
-    :type view: Autodesk.Revit.DB.View
-    :param curve_loops: The curve loops to use for the filled region.
-    :type curve_loops: list Autodesk.Revit.DB.CurveLoop
-    :param filled_region_type: The type of filled region to create.
-    :type filled_region_type: Autodesk.Revit.DB.ElementId
-
-    :return: The created filled region instance.
-    :rtype: Autodesk.Revit.DB.FilledRegion
+    :param filled_region: A filled region instance.
+    :type filled_region: Autodesk.Revit.DB.FilledRegion
+    :param loop_index: The index of the loop to get the area for.
+    :type loop_index: int
+    
+    :return: The area of the loop.
+    :rtype: float
     """
-
-
+    
     # set up a status tracker
     return_value = Result()
+    
+    try:
+        # get the loops of a filled region's geometry
+        curve_loops = get_filled_region_curve_loops(filled_region)
+        
+        if loop_index >= len(curve_loops):
+            return_value.update_sep(False, "Loop index out of range.")
+            return return_value
 
-    # set up an action creating a filled region
-    def action():
-         # set up a status tracker
-        action_return_value = Result()
-        try:
-            # create the filled region
-            filled_region = FilledRegion.Create(doc, filled_region_type, view.Id, curve_loops)
-            # store the filled region in the return value
-            action_return_value.result.append(filled_region)
-            action_return_value.append_message("Filled region created successfully.")
-        except Exception as e:
-            action_return_value.update_sep(False, "Failed to create filled region with error: {}".format(e))
-        return action_return_value
+        # get the area of the filled region using the loop
+        area_result = get_area_from_closed_curve_loop(
+            doc=doc,
+            view=view,
+            curve_loop=curve_loops[loop_index],
+        )
 
-
-    transaction = Transaction(doc, "Drawing filled region")
-    return_value = in_transaction(transaction, action)
-
-
+        # update the return value with the area
+        return_value.update(area_result)
+    except Exception as e:
+        return_value.update_sep(False, "Failed to get filled region loop area with error: {}".format(e))
+    
     return return_value

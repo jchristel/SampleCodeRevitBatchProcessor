61a99a2c3ae7cb55dcd6f3b7d4cf37abcd777b3f
jchristel <jan.r.christel@gmail.com>
2025-04-20 19:39:00 +1000

    Update nested_families.py
    working on exztrusion

--- Samples/pyRevit/Extensions/duHast.extension/duHast.tab/lib/pushIt_associated/get_a_room/nested_families.py
+++ Samples/pyRevit/Extensions/duHast.extension/duHast.tab/lib/pushIt_associated/get_a_room/nested_families.py
@@ -23,73 +23,99 @@
 from duHast.Revit.Family.family_element_utils import get_all_curve_based_elements_in_family,get_all_generic_forms_in_family
 from duHast.Utilities.Objects.result import Result
 from duHast.Revit.Common.delete import delete_by_element_ids
+from duHast.Revit.Common.transaction import in_transaction
 
-from Autodesk.Revit.DB import Extrusion, ModelLine ,CurveLoop, Line, XYZ, ReferencePlane, CurveElement, ModelText, ElementId
+from duHast.Revit.Levels.levels import get_levels_in_model
+
+from Autodesk.Revit.DB import CurveArrArray, CurveArray, CurveLoop, Curve, Extrusion, ModelLine ,Transaction, SketchPlane
+
+
+def convert_loop_to_curve_array(curve_loop):
+    curve_array = CurveArray()
+    if (isinstance(curve_loop, CurveLoop)):
+        for curve in curve_loop:
+            if (isinstance(curve, Curve)):
+                print("Curve: {}".format(curve))
+                curve_array.Append(curve)
+                
+    return curve_array
+
+def convert_curve_loops_to_curve_arr_array(curve_loops):
+    """
+    Convert a list of curve loops to a CurveArrArray object.
+    This is used to create a new extrusion in the family document.
+    
+    :param curve_loops: list of curve loops
+    :return: CurveArrArray object
+    """
+    
+    # create a new curve array array
+    curve_arr_array = CurveArrArray()
+
+    curve_array = CurveArray()
+    for loop in curve_loops:
+        
+        if (isinstance(loop, CurveLoop)):
+            # a loop of curves, convert to curve array
+            c_ar = convert_loop_to_curve_array(loop)
+            curve_arr_array.Append(c_ar)
+        elif (isinstance(loop, Curve)):
+            # just a curve, not a loop
+            curve_array.Append(loop)
+            
+    # only append the curve array if it is not empty
+    if (curve_array.Size > 0):
+        curve_arr_array.Append(curve_array)
+
+    return curve_arr_array
+
 
 def update_extrusion_outline(family_doc, curve_loops):
     # set up a status tracker
     return_value = Result()
     try:
+        # rather than updating the extrusion outline, we will delete the old extrusion and create a new one
+        # need to get the category of the existing extrusion
+        # cant delete an extrusion when it comes from a family template...need to create a new family from
+        # an existing family not a from a template
         
-        print("Updating extrusion outline in family")
-        # get all (should be 1) extrusions in the family
-        elements_col = get_all_generic_forms_in_family(doc=family_doc)
+        # create a new extrusion
+        # get the level in the family document   
+        level_col = get_levels_in_model(family_doc)
         
-        # loop over elements and update outlines
-        for el in elements_col:
-            if isinstance(el, Extrusion):
-                # store all existing curves in the extrusion
-                extrusion_model_curve_ids = []
+        level_plane = None
+        for level in level_col:
+            level_plane =level
+            
+        # convert filled region curve loops to curve array array
+        new_profile = convert_curve_loops_to_curve_arr_array(curve_loops)
+        
+        # create a new extrusion in the family document
+        def action():
+       
+            action_return_value = Result()
+            try:
+                level_reference = level_plane.GetPlaneReference()
+                sketch_plane = SketchPlane.Create(family_doc, level_reference)
                 
-                # get the sketch
-                sketch_extrusion = el.Sketch
+                # Create new extrusion
+                new_extrusion = family_doc.FamilyCreate.NewExtrusion(True, new_profile, sketch_plane, 10.00)
                 
-                # get the sketch plane of the extrusion
-                sketch_plane = sketch_extrusion.SketchPlane
+                action_return_value.append_message("Created new extrusion in family")
+                action_return_value.result.append(new_extrusion)
                 
-                # get the element ids of elements in the sketch
-                sketch_element_ids = sketch_extrusion.GetAllElements()
-               
-                # get the curves of the sketch elements
-                for sketch_element_id in sketch_element_ids:
-                    sketch_element = family_doc.GetElement(sketch_element_id)
-                    if isinstance(sketch_element, ModelLine):
-                        print("Found model line: {}".format(sketch_element.Id))
-                        # get the curve of the sketch element
-                        extrusion_model_curve_ids.append(sketch_element_id)
-                
-                # delete old curves and add new ones
-                # delete old sketch elements
-                result_delete = delete_by_element_ids(
-                    doc=family_doc, 
-                    ids=extrusion_model_curve_ids, 
-                    transaction_name="Delete old sketch elements",
-                    element_name= "model lines"
-                )
-                
-                print("Deleted old sketch elements: {}".format(result_delete))
-                
-                # add new curves to the sketch
-                for curve_loop in curve_loops:
-                    # loop over the curves in the loop
-                    for curve in curve_loop:
-                        # create a new model line in the sketch plane
-                        new_model_line = family_doc.NewModelCurve(curve, sketch_plane)
-                        # set the model line to be part of the extrusion sketch
-                        sketch_extrusion.AddElement(new_model_line)
-                
-            else:
-                # ignore this element
-                pass
-                
-            # get out of loop
-            break
-                
+            except Exception as e:
+                action_return_value.update_sep(False, "Failed to create new extrusion in family: {}".format(e))
+            return action_return_value
+
+        transaction = Transaction(family_doc, "Creating extrusion")
+        return_value = in_transaction(transaction,action )
                 
     except Exception as e:
         message = "Failed to update extrusion in family: {}".format(e)
         return_value.update_sep(False, message)
     return return_value
+
 
 def add_2D_outline(family_doc, curve_loop):
     # set up a status tracker

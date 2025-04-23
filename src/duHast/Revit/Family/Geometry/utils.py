f2baf71acd94f9ff634d897c4b5b26062ec94533
Jan Christel <jan.r.christel@gmail.com>
2025-04-23 15:48:05 +1000

    Update utils.py
    added:
     set_curve_sub_category

--- src/duHast/Revit/Family/Geometry/utils.py
+++ src/duHast/Revit/Family/Geometry/utils.py
@@ -99,3 +99,63 @@ def set_element_sub_category(doc, element, source_graphic_style, source_graphic_
         message = "Failed to set sub category of element in family: {}".format(e)
         return_value.update_sep(False, message)
     return return_value
+
+
+def set_curve_sub_category(doc, element, source_graphic_style, transaction_manager=in_transaction):
+    """
+    Set the subcategory of a curve in a family document. ( its really the graphic style...)
+    The subcategory is set to the subcategory of the source projection graphic style.
+
+    :param doc: The family document.
+    :type doc: Autodesk.Revit.DB.Document
+    :param element: The element.
+    :type element: Autodesk.Revit.DB.Element
+    :param source_graphic_style: The graphic style of the source curve.
+    :type source_graphic_style: Autodesk.Revit.DB.GraphicStyle
+    :param transaction_manager: The transaction manager.
+    :type transaction_manager: function
+
+    :return: Result class instance.
+        - `result.status` (bool): True if the elements subcategory was set successfully, otherwise False.
+        - `result.message` (str): Confirmation of successful setting of the elements subcategory.
+        - `result.result` (list): Empty.
+    On exception:
+
+        - `result.status` (bool): False.
+        - `result.message` (str): Generic exception message.
+        - `result.result` (list): Empty.
+    :rtype: :class:`.Result`
+    """
+
+    return_value = Result()
+    try:
+        # assign the graphic style
+        paras = element.GetOrderedParameters()
+        
+        # flag to indicate if the value has been attempted to be set
+        value_has_been_set = False
+        
+        # find the parameter driving the subcategory
+        for p in paras:
+            if p.Definition.BuiltInParameter in ELEMENTS_PARAS_SUB:
+                # get the subcategory style id
+                target_id = source_graphic_style.Id
+                # set the subcategory id
+                updated_para = set_parameter_value(
+                    p, 
+                    str(target_id), 
+                    doc,
+                    transaction_manager
+                )
+                return_value.update(updated_para)
+                
+                # set flag to true to indicate that the value has been set, or at least attempted to be set
+                value_has_been_set = True
+                
+                break
+        if not value_has_been_set:
+            return_value.update_sep(False, "Failed to set sub category of element in family. No parameter match found.")
+    except Exception as e:
+        message = "Failed to set sub category of element in family: {}".format(e)
+        return_value.update_sep(False, message)
+    return return_value

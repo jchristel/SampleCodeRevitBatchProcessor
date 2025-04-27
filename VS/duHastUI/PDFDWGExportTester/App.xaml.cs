01379a97533314c9ef56577774ab91b3b097d1b6
jchristel <jan.r.christel@gmail.com>
2025-04-27 16:17:33 +1000

    added a ui test application for dwg pdf exporter
    to speed things up a little

---
+++ VS/duHastUI/PDFDWGExportTester/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PDFDWGExportTester
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            // Initialize the application
+            var main = new duHastNet.UI.PDFDWGExporterUI.Main(
+                currentPDFExportString: "PDFExportString",
+                currentDWGExportString: "DWGExportString",
+                parameterNames: new List<string> { "Parameter1", "Parameter2" }
+            );
+
+            // Execute the main function to get the settings
+            var settings = main.Execute();
+        }
+    }
+}

343f49ba37f46c1027d0a7bdf378039853a4cb9b
Jan Christel <jan.r.christel@gmail.com>
2025-05-02 08:44:14 +1000

    added milliseconds option

--- src/duHast/Utilities/date_stamps.py
+++ src/duHast/Utilities/date_stamps.py
@@ -54,6 +54,9 @@ FILE_DATE_STAMP_YYYY_MM_DD = "%Y_%m_%d"
 #: file stamp date time format using underscores as delimiter: 2021_03_01_18_59_59
 FILE_DATE_STAMP_YYYY_MM_DD_HH_MM_SEC = "%Y_%m_%d_%H_%M_%S"
 
+#: file stamp date time format using underscores as delimiter: 2021_03_01_18_59_59_123
+FILE_DATE_STAMP_YYYY_MM_DD_HH_MM_SEC_MSEC = "%Y_%m_%d_%H_%M_%S_%f"
+
 
 #: time stamp using colons: 18:59:59
 TIME_STAMP_HHMMSEC_COLON = "%H:%M:%S"
@@ -86,32 +89,64 @@ FOLDER_DATE_STAMP_YYYYMMDD = "%Y%m%d"
 #: folder date format: no delimiter 2021
 FOLDER_DATE_STAMP_YYYY = "%Y"
 
-
-def get_folder_date_stamp(format=FOLDER_DATE_STAMP_YYYYMMDD):
+def date_time_stamp_builder(format):
     """
-    Returns a date stamp formatted suitable for a folder name.
-    :param format: The date stamp format, defaults to FOLDER_DATE_STAMP_YYYYMMDD
-    :type format: str, optional
-    :return: datetime.now() string formatted using supplied format string
-    :rtype: str
-    """
+    Returns a formatted date stamp.
 
-    d = datetime.datetime.now()
-    return d.strftime(format)
+    Note: milliseconds, if required, are capped to 3 digits.
 
-
-# get the date stamp in provided format
-def get_date_stamp(format):
-    """
-    Returns a date stamp formatted using past in format string.
     :param format: The date stamp format
     :type format: str
     :return: datetime.now() string formatted using supplied format string
     :rtype: str
     """
 
-    d = datetime.datetime.now()
-    return d.strftime(format)
+    if isinstance(format, str):
+        # check if the format is a valid date format
+        try:
+            d = datetime.datetime.now()
+            formatted_time = d.strftime(format)
+            
+            # Check if the format includes microseconds and trim to milliseconds if needed
+            if "%f" in format:
+                formatted_time = formatted_time[:-3]
+
+            return formatted_time
+        except ValueError:
+            raise ValueError("Invalid date format string provided.")
+    else:
+        raise TypeError("Format must be a string, got {}".format(type(format))) 
+    
+
+def get_folder_date_stamp(format=FOLDER_DATE_STAMP_YYYYMMDD):
+    """
+    Returns a date stamp formatted suitable for a folder name.
+
+    Note: milliseconds, if required, are capped to 3 digits.
+
+    :param format: The date stamp format, defaults to FOLDER_DATE_STAMP_YYYYMMDD
+    :type format: str, optional
+    :return: datetime.now() string formatted using supplied format string
+    :rtype: str
+    """
+
+    return  date_time_stamp_builder(format)
+
+
+
+def get_date_stamp(format):
+    """
+    Returns a date stamp formatted using past in format string.
+
+    Note: milliseconds, if required, are capped to 3 digits.
+
+    :param format: The date stamp format
+    :type format: str
+    :return: datetime.now() string formatted using supplied format string
+    :rtype: str
+    """
+
+    return date_time_stamp_builder(format)
 
 
 def get_date_stamped_file_name(file_path, file_extension=".txt", file_suffix=""):

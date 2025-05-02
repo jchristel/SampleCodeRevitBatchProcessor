225312245e4dfd81fa5df32dfbb8bba3bba038cc
jchristel <jan.r.christel@gmail.com>
2025-05-03 09:09:44 +1000

    push it added count converter

---
+++ VS/PushIt/Converters/CountConverter.cs
@@ -0,0 +1,65 @@
+﻿//
+//License:
+//
+//
+// Revit Batch Processor Sample Code
+//
+// BSD License
+// Copyright 2025, Jan Christel
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+// - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+// - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+// - Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//
+// This software is provided by the copyright holder "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed.
+// In no event shall the copyright holder be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits;
+// or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
+//
+//
+//
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace duHastNet.PushIt.Converters
+{
+    /// <summary>
+    /// Used to convert the count column value to an integer so it can be used for row colouring.
+    /// The count column can have this format: "1 (2)" or "1". In both cases we want to extract the first number.
+    /// </summary>
+    public class CountConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string strValue)
+            {
+                // Regex to match number before parentheses
+                Match match = Regex.Match(strValue, @"^\d+");
+                if (match.Success)
+                {
+                    return match.Value; // Return the number before brackets
+                }
+            }
+            else if (value is int count)
+            {
+                return count; // If it's a pure number, return it directly
+            }
+
+            return value; // Default return if no match found
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

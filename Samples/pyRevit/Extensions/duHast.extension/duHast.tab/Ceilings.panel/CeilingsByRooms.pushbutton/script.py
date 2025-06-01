6e5b0e87db9e0439374790764ea62ae1bc89a9f0
Jan Christel <jan.r.christel@gmail.com>
2025-06-02 08:59:30 +1000

    pyRevit Samples updates
    Ceilings by Room
    Associate Walls to Rooms

---
+++ Samples/pyRevit/Extensions/duHast.extension/duHast.tab/Ceilings.panel/CeilingsByRooms.pushbutton/script.py
@@ -0,0 +1,14 @@
+# pyrevit stuff
+from pyrevit import revit, script, forms
+logger = script.get_logger()
+output = script.get_output()
+
+# get the revit document
+doc = revit.doc
+
+
+# import grids and bubbles from library
+from rooms.rooms_and_ceilings import ceilings_to_rooms_entry
+
+# switches all bubbles in the active view off
+ceilings_to_rooms_entry(doc=doc, output=output, forms=forms)

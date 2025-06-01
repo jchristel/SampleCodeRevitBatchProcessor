a6a7b9b4d4fb257d297e3c0cc02a0704782e29ea
Jan Christel <jan.r.christel@gmail.com>
2025-06-02 08:58:52 +1000

    Update geometry.py
    added convert_boundary_segments_to_curve_loop

--- src/duHast/Revit/Rooms/Geometry/geometry.py
+++ src/duHast/Revit/Rooms/Geometry/geometry.py
@@ -27,16 +27,26 @@ This module contains a Revit rooms geometry extraction functions.
 #
 #
 
-import Autodesk.Revit.DB as rdb
 
+from duHast.Utilities.Objects.result import Result
 from duHast.Revit.Rooms.rooms import get_all_rooms
 from duHast.Data.Objects.Collectors.Properties.Geometry import geometry_polygon_2 as dGeometryPoly
+from duHast.Revit.Common.Geometry.curve import splice_adjacent_lines
+
+from Autodesk.Revit.DB import (
+    CurveLoop,
+    SpatialElementBoundaryOptions,
+    SpatialElementBoundaryLocation,
+    ElementId,
+    Line,
+    Arc,
+)
 
 
 def get_room_boundary_loops(
     revit_room,
-    spatial_boundary_option=rdb.SpatialElementBoundaryOptions(),
-    boundary_location=rdb.SpatialElementBoundaryLocation.Center,
+    spatial_boundary_option=SpatialElementBoundaryOptions(),
+    boundary_location=SpatialElementBoundaryLocation.Center,
 ):
     """
     Returns all boundary loops for a rooms. Default value set to the center
@@ -131,3 +141,113 @@ def get_2d_points_from_all_revit_rooms(doc):
         if len(room_points) > 0:
             all_room_point_groups.append(room_points)
     return all_room_point_groups
+
+
+def convert_boundary_segments_to_curve_loops(boundary_loop):
+    """
+    Takes a boundary loop and checks whether consecutive segments host have the same id ( same wall  or room separation line. )
+    If that is the case it will attempt to splice these segments and return them as one curve within the CurveLoop.
+
+    :param boundary_loop: List of boundary segments defining the room.
+    :type boundary_loop: List of Autodesk.Revit.DB.BoundarySegment
+
+    :return:
+        Result class instance.
+
+        - conversion status (bool) returned in result.status. False if an exception occurred, otherwise True.
+        - Result.message contain logs of segment conversion step by step.
+        - Result.result will contain the created CurveLoop instance.
+
+        On exception:
+
+        - .status (bool) will be False.
+        - .message will contain the exception message.
+
+    :rtype: :class:`.Result`
+    
+    """
+    return_value = Result()
+
+    # set up curve loops for ceiling creation
+    curve_loop = CurveLoop()
+
+    # boundary loops are nested and contain the outlines of the room
+   
+    current_host_id = ElementId.InvalidElementId
+    current_curve = None
+    
+    counter = 0
+    # change boundary loop to curve loop
+    for boundary_seg in boundary_loop:
+
+        # get the host id
+        host_id = boundary_seg.ElementId
+        # get the curve from the boundary segment
+        curve = boundary_seg.GetCurve()
+
+        # logging
+        return_value.append_message("current host id: {} and host id: {}".format(current_host_id, host_id))
+
+        # only add the previous curve ... in case curves need to be combined!!
+        # if curve host as changed, append the current curve to the curve loop
+        if host_id != current_host_id:
+            return_value.append_message("host id changed: {} to {}".format(current_host_id, host_id))
+            return_value.append_message("current curve: {} {} ".format(curve.GetEndPoint(0), curve.GetEndPoint(1)))
+            # set the current host id
+            current_host_id = host_id
+            # if there is a current curve, append it to the curve loop
+            if current_curve is not None:
+                curve_loop.Append(current_curve)
+            # set the new curve to be the current curve
+            current_curve = curve
+        else:
+
+            # check if line or arc...if neither do not combine
+            if not isinstance(curve, Line) and not isinstance(curve, Arc):
+                return_value.append_message("current curve is not a line or arc: {}".format(curve))
+                # if the curve is not a line or arc, do not combine
+                curve_loop.Append(curve)
+                current_curve = curve
+
+            # check if curve is a line
+            elif isinstance(curve, Line):
+                return_value.append_message ("current curve: {} {} and curve: {} {}".format(
+                    current_curve.GetEndPoint(0),
+                    current_curve.GetEndPoint(1),
+                    curve.GetEndPoint(0),
+                    curve.GetEndPoint(1))
+                )
+                return_value.append_message("current curve is a line: {}".format(current_curve))
+                combined_curve = splice_adjacent_lines(current_curve, curve)
+
+                if combined_curve is None:
+                    return_value.append_message("failed to combine")
+                    # if no match is found, do not combine
+                    current_curve = curve
+                    continue
+                
+                return_value.append_message("combined curve: {} {}".format(combined_curve.GetEndPoint(0),combined_curve.GetEndPoint(1)))
+                # dont append the combined curve in case its more then 2 segments 
+                current_curve = combined_curve
+            # check if curve is an arc
+            elif isinstance(curve, Arc):
+                return_value.append_message("current curve is an arc: {}".format(curve))
+                # dont combine just yet
+                current_curve = curve
+            # curve is neither a line nor an arc
+            else:
+                # if the curve is not a line or arc, do not combine
+                return_value.append_message("current curve is not a line or arc: {}".format(curve))
+                current_curve = curve
+                
+        counter = counter + 1
+    
+    # append the last curve to the curve loop
+    if current_curve is not None:
+        return_value.append_message("appending last curve: {} {}".format(current_curve.GetEndPoint(0), current_curve.GetEndPoint(1)))
+        curve_loop.Append(current_curve)
+
+
+    return_value.append_message("counter : {}".format(counter))
+    return_value.result.append(curve_loop)
+    return return_value

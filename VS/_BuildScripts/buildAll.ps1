594f63732b8a7a517b798723941ae0c7769f0644
jchristel <jan.r.christel@gmail.com>
2025-07-19 09:04:56 +1000

    updating build script to work with
    net 48 folder name

--- VS/_BuildScripts/buildAll.ps1
+++ VS/_BuildScripts/buildAll.ps1
@@ -1,14 +1,14 @@
 # Define paths
-$solution1 = "C:\Users\janchristel\Documents\GitHub\SampleCodeRevitBatchProcessor\VS\duHastUtils\duHastUtils.sln"
-$solution2 = "C:\Users\janchristel\Documents\GitHub\SampleCodeRevitBatchProcessor\VS\duHastRevitUtils\duHastRevitUtils.sln"
-$solution3 = "C:\Users\janchristel\Documents\GitHub\SampleCodeRevitBatchProcessor\VS\duHastRevitApplications\duHastRevitApplications.sln"
-$solution4 = "C:\Users\janchristel\Documents\GitHub\SampleCodeRevitBatchProcessor\VS\duHastApplications\duHastApplications.sln"
-$solution5 = "C:\Users\janchristel\Documents\GitHub\SampleCodeRevitBatchProcessor\VS\duHastUI\duHastUI.sln"
-$solution6 = "C:\Users\janchristel\Documents\GitHub\SampleCodeRevitBatchProcessor\VS\duHastUICustomControls\duHastUICustomControls.sln"
+$solution1 = "C:\Users\janchristel\Documents\GitHub\SampleCodeRevitBatchProcessor-NET48\VS\duHastUtils\duHastUtils.sln"
+$solution2 = "C:\Users\janchristel\Documents\GitHub\SampleCodeRevitBatchProcessor-NET48\VS\duHastRevitUtils\duHastRevitUtils.sln"
+$solution3 = "C:\Users\janchristel\Documents\GitHub\SampleCodeRevitBatchProcessor-NET48\VS\duHastRevitApplications\duHastRevitApplications.sln"
+$solution4 = "C:\Users\janchristel\Documents\GitHub\SampleCodeRevitBatchProcessor-NET48\VS\duHastApplications\duHastApplications.sln"
+$solution5 = "C:\Users\janchristel\Documents\GitHub\SampleCodeRevitBatchProcessor-NET48\VS\duHastUI\duHastUI.sln"
+$solution6 = "C:\Users\janchristel\Documents\GitHub\SampleCodeRevitBatchProcessor-NET48\VS\duHastUICustomControls\duHastUICustomControls.sln"
 
-$commonDir = "C:\Users\janchristel\Documents\GitHub\SampleCodeRevitBatchProcessor\VS\_References\duHast"
+$commonDir = "C:\Users\janchristel\Documents\GitHub\SampleCodeRevitBatchProcessor-NET48\VS\_References\duHast"
 
-$targetDirSolution5 = "C:\Users\janchristel\Documents\GitHub\SampleCodeRevitBatchProcessor\src\duHast\lib"
+$targetDirSolution5 = "C:\Users\janchristel\Documents\GitHub\SampleCodeRevitBatchProcessor-NET48\src\duHast\lib"
 
 # Ask user for build configuration (Release or Debug)
 $buildConfig = Read-Host "Enter build configuration (Release/Debug)"

6d6ee9fead29b5f81c6ee9aa14a517df55fc2bf9
jchristel <jan.r.christel@gmail.com>
2025-07-20 16:31:21 +1000

    Create FamiliesDataGridViewModel.cs

---
+++ VS/duHastRevitApplications/AtTheLibrary/ViewModels/FamiliesDataGridViewModel.cs
@@ -0,0 +1,475 @@
+﻿//
+//License:
+//
+//
+// Revit Batch Processor Sample Code
+//
+// BSD License
+// Copyright 2025, Jan Christel
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+// - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+// - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+// - Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//
+// This software is provided by the copyright holder "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed.
+// In no event shall the copyright holder be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits;
+// or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
+//
+//
+//
+
+using duHastNet.UI.CustomControls.CustomDataGrid;
+using duHastNet.Utils.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace duHastNet.AtTheLibrary.ViewModels
+{
+    public class FamiliesDataGridViewModel: BaseDynamicGridViewModel<DynamicRowData>
+    {
+        private Models.RevitFamiliesDataModel RevitDataModel { get; set; }
+       
+
+        /// <summary>
+        /// Dictionary containing custom default values for specific columns
+        /// </summary>
+        private Dictionary<string, Func<FamiliesDataGridViewModel, object>> _columnDefaults;
+
+        /// <summary>
+        /// Dictionary containing custom default values for specific columns
+        /// </summary>
+        public Dictionary<string, Func<FamiliesDataGridViewModel, object>> ColumnDefaults
+        {
+            get => _columnDefaults;
+        }
+
+        /// <summary>
+        /// maps column ids to display names
+        /// </summary>
+        private Dictionary<string, string> _columnIdToParameterNameLookUp;
+        public Dictionary<string, string> ColumnIdToParameterNameLookUp
+        {
+            get => _columnIdToParameterNameLookUp;
+        }
+
+        #region user interaction
+
+        #region selected family
+
+        protected override void OnSelectionChanged()
+        {
+            base.OnSelectionChanged();
+
+            if (HasSelection)
+            {
+                //get the selected row item
+                var selectedRow = SelectedItem;
+                var familyId = selectedRow[Models.Constants.ColumnHeaderFamilyId.Replace(" ", "")].ToString();
+                SelectedFamily = RevitDataModel.GetAllFamilies().FirstOrDefault(r => r.Id.Value == familyId);
+
+                //check matching revit rooms
+                if (SelectedFamily != null)
+                {
+                    IsMatchingRevitFamiliesEmpty = SelectedFamily.MatchingRevitFamilies.Count == 0;
+                }
+                else
+                {
+                    IsMatchingRevitFamiliesEmpty = true;
+                }
+            }
+            else
+            {
+                SelectedFamily = null;
+                IsMatchingRevitFamiliesEmpty = true;
+            }
+        }
+
+        //field to store the selected room from the revit data model
+        private Models.FamilyDataModel _selectedFamily;
+
+        //property to get the selected room from the revit data model
+        public Models.FamilyDataModel SelectedFamily
+        {
+            set
+            {
+                _selectedFamily = value;
+                OnPropertyChanged(nameof(SelectedFamily));
+            }
+
+            get => _selectedFamily;
+        }
+
+
+        //field to store if the selected room has any matching Revit rooms
+        bool _isMatchingRevitFamiliesEmpty;
+
+        //property to check if the selected room has any matching Revit rooms
+        //used to determine if the button to push the selected room to Revit should be enabled
+        public bool IsMatchingRevitFamiliesEmpty
+        {
+            set
+            {
+                _isMatchingRevitFamiliesEmpty = value;
+                OnPropertyChanged(nameof(IsMatchingRevitFamiliesEmpty));
+            }
+            get => _isMatchingRevitFamiliesEmpty;
+
+        }
+
+        #endregion selected family
+
+        #endregion user interaction
+
+        #region data loading
+
+        /// <summary>
+        /// Define what columns are available for the revit categories grid
+        /// </summary>
+        protected override void InitializeAvailableColumns()
+        {
+
+            // Don't do anything if called from base constructor
+            if (this.RevitDataModel == null) return;
+
+            //initialise available columns with default room properties
+            AvailableColumns = new ObservableCollection<AvailableColumnDefinition> { 
+            
+                // Basic revit category Info
+                new AvailableColumnDefinition(Models.Constants.ColumnHeaderFamilyId.Replace(" ", "") , Models.Constants.ColumnHeaderFamilyId, typeof(string)),
+                new AvailableColumnDefinition(Models.Constants.ColumnHeaderFamilyCount.Replace(" ", "") , Models.Constants.ColumnHeaderFamilyCount, typeof(int)),
+                new AvailableColumnDefinition(Models.Constants.ColumnHeaderFamilyName.Replace(" ",""), Models.Constants.ColumnHeaderFamilyName, typeof(string)),
+                new AvailableColumnDefinition(Models.Constants.ColumnHeaderFamilyTypeName.Replace(" ",""), Models.Constants.ColumnHeaderFamilyTypeName, typeof(string)),
+                new AvailableColumnDefinition(Models.Constants.ColumnHeaderFamilyCategory.Replace(" ",""), Models.Constants.ColumnHeaderFamilyCategory, typeof(string)),
+            };
+
+            // add any other properties
+            foreach (var property in RevitDataModel.GetAllParameters())
+            {
+                // skip the unique id property
+                //if (property.IsUniqueId)
+                //{
+                //    continue;
+                //}
+
+                AvailableColumns.Add(
+                    new AvailableColumnDefinition(property.Replace(" ", ""), property, typeof(string))
+                    );
+            }
+
+            // Pre-compute the parameter name to column id lookup dictionary
+            _columnIdToParameterNameLookUp = RevitDataModel.GetAllParameters().ToDictionary(name => name.Replace(" ", ""), name => name);
+
+            // Set up initial columns that should be visible by default
+            SetupDefaultColumns();
+        }
+
+        /// <summary>
+        /// Create a new revit category row with default data
+        /// </summary>
+        protected override DynamicRowData CreateNewRow()
+        {
+            return new DynamicRowData
+            {
+                // pre-populate some default values here if needed
+                // The base class will fill in defaults for all columns
+            };
+        }
+
+        /// <summary>
+        /// Load data from the RevitDataModel into the grid
+        /// </summary>
+        private void LoadDataFromRevitDataModel()
+        {
+            Data.Clear();
+
+            if (RevitDataModel?.GetAllFamilies() == null || RevitDataModel?.GetAllFamilies().Count == 0) return;
+
+            foreach (var familyEntry in RevitDataModel.GetAllFamilies())
+            {
+                try
+                {
+                    var rowData = CreateRowFromFamily(familyEntry);
+                    Data.Add(rowData);
+                }
+                catch (Exception ex)
+                {
+                    // Log or handle individual category errors
+                    System.Diagnostics.Debug.WriteLine($"Error loading family data: {ex.Message}");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Convert a room data object to a dynamic row for the grid
+        /// </summary>
+        private DynamicRowData CreateRowFromFamily(Models.FamilyDataModel familyEntry)
+        {
+            var rowData = new DynamicRowData();
+
+            // Set the standard column values
+            rowData[Models.Constants.ColumnHeaderFamilyId.Replace(" ", "")] = familyEntry.Id.Value;
+            rowData[Models.Constants.ColumnHeaderFamilyCount.Replace(" ", "")] = familyEntry.MatchingRevitFamilies.Count;
+            rowData[Models.Constants.ColumnHeaderFamilyName.Replace(" ", "")] = familyEntry.FamilyName.Value;
+            rowData[Models.Constants.ColumnHeaderFamilyTypeName.Replace(" ", "")] = familyEntry.FamilyTypeName.Value;
+            rowData[Models.Constants.ColumnHeaderFamilyCategory.Replace(" ", "")] = familyEntry.FamilyCategory.Value;
+
+
+            foreach (var property in familyEntry.Properties)
+            {
+                // add all other property values
+                rowData[property.Name.Replace(" ", "")] = property.Value;
+            }
+
+            return rowData;
+        }
+
+        #endregion data loading
+
+        #region event handlers
+
+        /// <summary>
+        /// used to catch property changed events from the underlying model in order to update the ui
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Model_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            // check which property changed in the underlying model
+            switch (e.PropertyName)
+            {
+                case Utilities.PropertyChangedEventNames.DATA_MODEL_FAMILIES_UPDATED:
+                    //update rooms in the view model
+                    LoadDataFromRevitDataModel();
+                    break;
+
+                // Add more cases for other properties as needed
+
+                default:
+                    // Handle changes for properties not explicitly handled
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Custom closing logic for RoomsSelectionViewModel
+        /// Disposes all external events from the event manager
+        /// </summary>
+        public override void OnClosing()
+        {
+            //unbsubscribe from underlying model changes
+            RevitDataModel.PropertyChanged -= Model_PropertyChanged;
+
+            base.OnClosing();
+        }
+
+        #endregion event handlers
+
+        #region Private Helper Methods
+        private void InitializeColumnDefaults()
+        {
+            //initialise with default category properties and default values
+            _columnDefaults = new Dictionary<string, Func<FamiliesDataGridViewModel, object>>
+            {
+                {Models.Constants.ColumnHeaderFamilyId.Replace(" ", ""), vm => "NEW" },
+                {Models.Constants.ColumnHeaderFamilyCount.Replace(" ", ""), vm =>  0},
+                {Models.Constants.ColumnHeaderFamilyName.Replace(" ",""), vm => "-"},
+                {Models.Constants.ColumnHeaderFamilyTypeName.Replace(" ",""), vm => "-"},
+                {Models.Constants.ColumnHeaderFamilyCategory.Replace(" ",""), vm => "-"},
+            };
+
+            // add any other properties
+            foreach (var property in RevitDataModel.GetAllParameters())
+            {
+                //// skip the unique id property
+                //if (property.IsUniqueId)
+                //{
+                //    continue;
+                //}
+
+                // add empty string for now...
+                _columnDefaults[property.Replace(" ", "")] = vm => "";
+            }
+        }
+
+
+        /// <summary>
+        /// Override to restore actual room data instead of defaults when adding columns
+        /// </summary>
+        protected override void RestoreOrSetDefaultColumnData(string propertyName, AvailableColumnDefinition availableColumn)
+        {
+            if (_removedColumnData.ContainsKey(propertyName))
+            {
+                // Use the base implementation for restored data
+                base.RestoreOrSetDefaultColumnData(propertyName, availableColumn);
+            }
+            else
+            {
+                // For new columns, populate with actual room data instead of defaults
+                RestoreActualFamilyData(propertyName, availableColumn);
+            }
+        }
+
+
+        /// <summary>
+        /// Populate column with actual data from rooms instead of defaults
+        /// </summary>
+        private void RestoreActualFamilyData(string propertyName, AvailableColumnDefinition availableColumn)
+        {
+            var families = RevitDataModel.GetAllFamilies();
+            // For all rows, set the actual data from the corresponding room
+            for (int i = 0; i < Data.Count && i < families.Count; i++)
+            {
+                var family = families[i];
+                var rowData = Data[i];
+
+                // Set the value based on the property name
+                if (propertyName == Models.Constants.ColumnHeaderFamilyId.Replace(" ", ""))
+                {
+                    rowData[propertyName] = family.Id.Value;
+                }
+                else if (propertyName == Models.Constants.ColumnHeaderFamilyCount.Replace(" ", ""))
+                {
+                    rowData[propertyName] = family.MatchingRevitFamilies.Count;
+                }
+                else if (propertyName == Models.Constants.ColumnHeaderFamilyName.Replace(" ", ""))
+                {
+                    rowData[propertyName] = family.FamilyName.Value;
+                }
+                else if (propertyName == Models.Constants.ColumnHeaderFamilyTypeName.Replace(" ", ""))
+                {
+                    rowData[propertyName] = family.FamilyTypeName.Value;
+                }
+                else if (propertyName == Models.Constants.ColumnHeaderFamilyCategory.Replace(" ", ""))
+                {
+                    rowData[propertyName] = family.FamilyCategory.Value;
+                }
+                else
+                {
+                    // Handle custom properties
+                    var property = family.Properties.FirstOrDefault(p => p.Name.Replace(" ", "") == propertyName);
+                    if (property != null)
+                    {
+                        rowData[propertyName] = property.Value ?? "";
+                    }
+                    else
+                    {
+                        // Fallback to default value
+                        rowData[propertyName] = GetDefaultValueForColumn(availableColumn);
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Get default value for a specific column in the catgeories context
+        /// </summary>
+        protected override object GetDefaultValueForColumn(AvailableColumnDefinition columnDef)
+        {
+
+            // Check if we have a custom default for this column
+            if (ColumnDefaults.TryGetValue(columnDef.PropertyName, out Func<FamiliesDataGridViewModel, object> defaultValueFunc))
+            {
+                return defaultValueFunc(this);
+            }
+
+            // Fall back to base implementation for standard types
+            return GetDefaultValue(columnDef.DataType);
+        }
+
+
+        /// <summary>
+        /// Define which columns should be read-only by default ( all of them! )
+        /// </summary>
+        protected override bool GetDefaultReadOnlyForColumn(string propertyName)
+        {
+            return true; // Everything is locked
+        }
+
+        /// <summary>
+        /// Set up the default columns that should be visible when the grid loads
+        /// </summary>
+        private void SetupDefaultColumns()
+        {
+            var defaultColumns = new List<string>();
+
+            //check if any column id ( property name without space) is mentioned in the settings object
+            //if there are columns stored in settings retrieved from file
+            if (RevitDataModel.Settings.ColumnIds != null &&
+                RevitDataModel.Settings.ColumnIds.Count > 0)
+            {
+                foreach (string columnId in RevitDataModel.Settings.ColumnIds)
+                {
+                    var columNameByParameter = FindColumnNameById(columnId);
+                    //check if column id has a match in parameters available
+                    if (columNameByParameter != null)
+                    {
+                        // add the id not the display value!
+                        defaultColumns.Add(columnId);
+                    }
+                    else
+                    {
+                        var columnNameDefault = Models.Constants.ColumnFamiliesInfo.TryGetValue(columnId, out string displayName) ? displayName : null;
+                        if (columnNameDefault != null)
+                        {
+                            // add the id not the display value!
+                            defaultColumns.Add(columnId);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                //add reserved names first
+                foreach (KeyValuePair<string, string> entry in Models.Constants.ColumnFamiliesInfo)
+                {
+                    defaultColumns.Add(entry.Key);
+                }
+            }
+
+            foreach (var columnName in defaultColumns)
+            {
+                AddSelectedColumn(columnName);
+            }
+        }
+
+
+        public string FindColumnNameById(string id)
+        {
+            return _columnIdToParameterNameLookUp.TryGetValue(id, out string displayName) ? displayName : null;
+        }
+
+
+        #endregion event handlers
+
+
+
+        public FamiliesDataGridViewModel(Models.RevitFamiliesDataModel revitDataModel)
+        {
+            // Base constructor will call InitializeAvailableColumns()
+            // and set up all the commands
+
+            this.RevitDataModel = revitDataModel;
+
+            // manually call this since it was skipped during base constructor
+            InitializeAvailableColumns();
+
+            // Initialize column defaults first
+            InitializeColumnDefaults();
+
+            // Populate the grid with data from the sheets model
+            LoadDataFromRevitDataModel();
+
+            //subscribe to underlying model changes
+            RevitDataModel.PropertyChanged += Model_PropertyChanged;
+        }
+    }
+}

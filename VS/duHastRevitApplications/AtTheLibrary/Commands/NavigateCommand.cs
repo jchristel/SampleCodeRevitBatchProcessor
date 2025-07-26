97d69c40ae16794623d626cb428df5b620130524
jchristel <jan.r.christel@gmail.com>
2025-07-26 21:25:18 +1000

    Create NavigateCommand.cs

---
+++ VS/duHastRevitApplications/AtTheLibrary/Commands/NavigateCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace duHastNet.AtTheLibrary.Commands
+{
+    public class NavigateCommand : Utils.WPF.Commands.CommandBase
+    {
+        private readonly duHastNet.Utils.WPF.Stores.NavigationStore _navigationStore;
+        private readonly Func<Utils.WPF.ViewModels.ViewModelBase> _createViewModel;
+
+        public NavigateCommand(
+            duHastNet.Utils.WPF.Stores.NavigationStore navigationStore,
+            Func<Utils.WPF.ViewModels.ViewModelBase> createViewModel
+        )
+        {
+            _navigationStore = navigationStore;
+            _createViewModel = createViewModel;
+        }
+
+        public override void Execute(object parameter)
+        {
+            _navigationStore.CurrentViewModel = _createViewModel();
+        }
+    }
+}

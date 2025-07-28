3666195490c0c1e3ff859087731ce1b185e6c419
jchristel <jan.r.christel@gmail.com>
2025-07-28 20:39:37 +1000

    Reloader
    added classes

---
+++ VS/duHastUI/FamilyReloaderUI/Utils/MatchStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace duHastNet.UI.FamilyReloaderUI.Utils
+{
+    public enum MatchStatus
+    {
+        Success,
+        NoMatch
+    }
+}

a5fed4e33010d251074ccdd57dc0d60fc98ac187
jchristel <jan.r.christel@gmail.com>
2025-08-03 09:20:37 +1000

    Update deploy_dll.ps1
    updated bin folder location to be within the extension

--- VS/_BuildScripts/deploy_dll.ps1
+++ VS/_BuildScripts/deploy_dll.ps1
@@ -185,7 +185,7 @@ Write-Output "File copied successfully from $sourceFileFamilyReloaderUI to $dest
 
 # lib directory
 $sourceFolderLib="$basePath\VS\_References\duHast"
-$destinationFolderLib_one="$basePath\Samples\pyRevit\Extensions\bin"
+$destinationFolderLib_one="$basePath\Samples\pyRevit\Extensions\$pyRevitExtensionName\bin"
 $destinationFolderLib_two="$basePath\src\duHast\lib"
 
 # Function to clean and copy files

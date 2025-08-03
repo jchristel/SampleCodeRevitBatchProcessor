11854671c626c42e5cbb13f51bda3ffd87de9f2b
jchristel <jan.r.christel@gmail.com>
2025-08-03 11:15:34 +1000

    date time formatting added
    added license and date time formatting

--- VS/duHastUICustomControls/duHastUICustomControls/CustomDataGrid/DynamicDataGrid.cs
+++ VS/duHastUICustomControls/duHastUICustomControls/CustomDataGrid/DynamicDataGrid.cs
@@ -1,9 +1,33 @@
-﻿using System.Collections.ObjectModel;
+﻿//
+//License:
+//
+//
+// Revit Batch Processor Sample Code
+//
+// BSD License
+// Copyright 2025, Jan Christel
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+// - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+// - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+// - Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+//
+// This software is provided by the copyright holder "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed.
+// In no event shall the copyright holder be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits;
+// or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
+//
+//
+//
+
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System;
 
 namespace duHastNet.UI.CustomControls.CustomDataGrid
 {
@@ -155,6 +179,12 @@ namespace duHastNet.UI.CustomControls.CustomDataGrid
                 Mode = columnDef.IsReadOnly ? BindingMode.OneWay : BindingMode.TwoWay,
                 UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged
             };
+
+            // DATE FORMATTING
+            if (columnDef.DataType == typeof(DateTime) || columnDef.DataType == typeof(DateTime?))
+            {
+                binding.StringFormat = "dd/MM/yyyy HH:mm:ss";
+            }
 
             // Set binding based on column type
             if (column is DataGridTextColumn textColumn)

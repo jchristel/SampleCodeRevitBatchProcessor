b40a7b4b77e6331c079888128938b4e474fce19a
Jan Christel <jan.r.christel@gmail.com>
2025-08-13 10:20:37 +1000

    fixed dll import errors
    updated the lib folder name

--- src/duHast/Utilities/file_base_write_net.py
+++ src/duHast/Utilities/file_base_write_net.py
@@ -39,7 +39,7 @@ from duHast.Utilities.Objects.file_encoding_bom import BOMValue as bom_value
 # the dll is located in the libs folder of the extension, which is one level up from the current file's directory
 current_directory = os.path.dirname(__file__)
 parent_directory = os.path.dirname(current_directory)
-dll_path = os.path.join(parent_directory, "libs", "FileIOWrapper.dll")
+dll_path = os.path.join(parent_directory, "lib", "FileIOWrapper.dll")
 clr.AddReference(dll_path)
 
 # import the WriteToFile class from the CSVHelperWrapper namespace

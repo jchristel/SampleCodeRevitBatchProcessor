4809d8b9e54f67e9ea10f76c2caf03649d5b7ba2
Jan Christel <jan.r.christel@gmail.com>
2025-08-14 09:12:38 +1000

    Update ui_data_get.py
    started schedule integration

--- Samples/pyRevit/Extensions/duHast-2024.extension/duHast.tab/lib/export/ui_data_get.py
+++ Samples/pyRevit/Extensions/duHast-2024.extension/duHast.tab/lib/export/ui_data_get.py
@@ -26,12 +26,137 @@ from System.Collections.Generic import List
 
 from duHast.Utilities.Objects.result import Result
 from duHast.Revit.Views.sheets import get_all_sheets
+from duHast.Revit.Views.schedules import  get_all_sheet_schedules
+from duHast.Revit.Views.schedules_fields import get_field_names_to_parameters, get_field_values_from_schedule_by_parameter_id
+from duHast.Revit.Views.schedules_sheets_export import  export_all_sheet_schedules_and_read_data_back
 from duHast.pyRevit.console_output import  print_error
 
 
 from export.utility import get_sheet_parameter_data
 
-from Autodesk.Revit.DB import FilteredElementCollector, ViewSheet, ViewSheetSet
+from Autodesk.Revit.DB import ElementId, FilteredElementCollector, ViewSheet, ViewSheetSet
+
+def schedule_contains_sheet_number_field(schedule):
+    """
+    Checks if the given schedule contains the sheet number field.
+
+    :param schedule: The schedule to check.
+    :type schedule: Autodesk.Revit.DB.ViewSchedule
+    :return: True if the schedule contains the sheet number field, False otherwise.
+    :rtype: bool
+    """
+
+    # get the field names in the schedule
+    field_names = get_field_names_to_parameters(schedule)
+
+    # check if the sheet number field is in the field parameter ids
+    for field_names,parameter_id in field_names.items():
+        # SHEET_NUMBER	-1,007,401	"Sheet Number" ( revit api docs)
+        if parameter_id.IntegerValue == -1007401:
+            # sheet number field found, return True
+            return True
+    
+    return false
+
+
+def get_sheet_numbers_in_schedule(schedule):
+    """
+    Gets all sheet numbers listed in the given schedule.
+
+    :param schedule: The schedule to extract sheet numbers from.
+    :type schedule: Autodesk.Revit.DB.ViewSchedule
+    :return: A list of sheet numbers found in the schedule.
+    :rtype: list of str
+    """
+
+    sheet_numbers = []
+
+    sheet_number_element_id = ElementId(-1007401)  # SHEET_NUMBER parameter id
+    try:
+        sheet_numbers = get_field_values_from_schedule_by_parameter_id(schedule, sheet_number_element_id)
+        return sheet_numbers
+    except Exception as e:
+        print_error("Error getting sheet numbers from schedule '{}': {}".format(schedule.Name, e))
+        return sheet_numbers
+
+    
+
+def get_ui_schedule_data (doc, sheets):
+
+    """
+    Gets the print set data for the export process.
+
+    :param doc: Current Revit model document.
+    :type doc: Autodesk.Revit.DB.Document
+    :return: Tuple containing the print set data.
+    :rtype: tuple
+    """
+    
+    # import the Schedule class from the PDFDWGExporterUI namespace
+    from duHastNet.UI.PDFDWGExporterSelectionUI.Models import RevitSchedule
+    from duHastNet.UI.PDFDWGExporterSelectionUI.Models import RevitSheet
+
+    # create a .net list to hold the schedule objects
+    schedule_data = List[RevitSchedule]()
+
+
+    # export all sheet schedules and read the data back
+    schedule_export_result = export_all_sheet_schedules_and_read_data_back(doc)
+    print("Exported all sheet schedules with status: {}".format(schedule_export_result.status))
+    print("Message: {}".format(schedule_export_result.message))
+    print("Result: {}".format(schedule_export_result.result))
+
+
+    if schedule_export_result.status:
+        
+        # if the export was successful, get the data from the result
+        schedule_data = schedule_export_result.result
+        print("Found {} schedules in the model.".format(len(schedule_data)))
+    else:
+        # if the export failed, print the error message and return an empty list
+        print_error("Error exporting sheet schedules: {}".format(schedule_export_result.message))
+        return schedule_data
+
+
+    # get all sheet schedules in the model'
+    sheet_schedules = get_all_sheet_schedules(doc)
+
+    print( "Found {} sheet schedules in the model.".format(sheet_schedules.Count))
+    for schedule in sheet_schedules:
+        
+        # check if the schedule contains the sheet number field
+        if not schedule_contains_sheet_number_field(schedule):
+            continue
+
+        # get all sheet numbers listed in schedule
+        sheet_numbers_in_schedule = get_sheet_numbers_in_schedule(schedule)
+        if len(sheet_numbers_in_schedule) == 0:
+            # no sheet numbers found in the schedule, skip this schedule
+            continue
+
+        sheets_in_schedule = List[RevitSheet]() 
+
+        for number_found in sheet_numbers_in_schedule:
+            for sheet in sheets:
+                if sheet.SheetNumber == number_found:
+                    sheets_in_schedule.Add(sheet)
+
+        # populate a RevitSchedule object with the schedule data
+        revit_sheet_schedule =  RevitSchedule(
+            name=schedule.Name,
+        )
+
+        # iterate over the sheets in the set and add them to the RevitPrintSet object
+        for sheet in sheets_in_schedule:
+            revit_sheet_schedule.AddRevitSheet(sheet)
+
+        # add the RevitPrintSet object to the list of print sets
+        schedule_data.Add(revit_sheet_schedule)
+
+    # currently no schedule data is available, so return an empty list
+    return schedule_data
+
+
 
 def get_view_sets(doc):
 
@@ -92,8 +217,7 @@ def get_ui_print_set_data(doc, sheets):
 
         # create a RevitPrintSet object with the set data
         revit_print_set = RevitPrintSet(
-            name=set.Name,
-            requiresUpdate=False,
+            name=set.Name
         )
 
         # iterate over the sheets in the set and add them to the RevitPrintSet object
@@ -177,6 +301,10 @@ def get_ui_data(doc):
 
     # get print set data
     print_set_ui_data = get_ui_print_set_data(doc, sheet_ui_data)
+
+    # get schedule data
+    schedule_ui_data = get_ui_schedule_data(doc, sheet_ui_data)
+    print ("Schedule UI Data: {}".format(schedule_ui_data))
 
     # return data as a tuple
     ui_data = (sheet_ui_data,print_set_ui_data)

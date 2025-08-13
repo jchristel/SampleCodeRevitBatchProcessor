c59915c9f145ae01cd14a8ead333feefe0b07b9f
Jan Christel <jan.r.christel@gmail.com>
2025-08-14 09:12:56 +1000

    fixed copy mistakes

--- src/duHast/Revit/Views/schedules_export.py
+++ src/duHast/Revit/Views/schedules_export.py
@@ -1,7 +1,7 @@
 """
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-This module contains a number of helper functions relating to Revit view schedules of category sheet export to file. 
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+This module contains a number of helper functions relating to Revit view schedules export to file. 
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 #
 # License:
@@ -26,171 +26,82 @@ This module contains a number of helper functions relating to Revit view schedul
 #
 #
 
-import os
-
-from duHast.Revit.Views.schedules import  get_all_sheet_schedules
-from duHast.Revit.Views.schedules_fields import schedule_contains_sheet_number_field
-from duHast.Revit.Views.schedules_export import export_schedule_to_file
-
 from duHast.Utilities.Objects.result import Result
-from duHast.Utilities.directory_io import directory_exists, create_temp_directory, directory_delete
-from duHast.Utilities.files_get import get_files_single_directory
-from duHast.Utilities.files_io import get_file_name_without_ext
-from duHast.Utilities.file_base_read_net import read_from_delimited_text_file
+from duHast.Utilities.files_io import get_directory_path_from_file_path, get_file_name_without_ext, get_file_extension
+from duHast.Utilities.directory_io import directory_exists
 
 
-def filter_schedules_by_sheet_number_field(schedules):
+from Autodesk.Revit.DB import ExportColumnHeaders, ExportTextQualifier, ViewSchedule, ViewScheduleExportOptions
+
+def export_schedule_to_file(schedule, file_path, field_delimiter=",", export_title=False, export_column_headers = ExportColumnHeaders.None ,export_text_qualifier=ExportTextQualifier.DoubleQuote):
+
     """
-    Filters the given schedules to only those that contain a visible sheet number field.
-    
-    :param schedules: The list of schedules to filter.
-    :type schedules: list[Autodesk.Revit.DB.ViewSchedule]
+    Exports a Revit schedule to a file.
 
-    :return: A list of schedules that contain the sheet number field.
-    :rtype: list[Autodesk.Revit.DB.ViewSchedule]
-    """
-
-    filtered_schedules = []
-
-    for schedule in schedules:
-        # check if the schedule contains the sheet number field
-        if schedule_contains_sheet_number_field(schedule = schedule, ignore_hidden_field=True):
-            filtered_schedules.append(schedule)
-    
-    return filtered_schedules
-
-
-def export_all_sheet_schedules_to_file(doc, directory_path, export_if_number_is_hidden = False):
-    """
-    Exports all schedules that are placed on sheets to file.
-    
-    :param directory_path: The directory path to export the schedules to.
-    :type directory_path: str
-    :param export_if_number_is_hidden: Whether to export schedules even if the number is hidden.
-    :type export_if_number_is_hidden: bool, optional
+    :param schedule: The schedule to export.
+    :type schedule: Autodesk.Revit.DB.ViewSchedule
+    :param file_path: The file path to export the schedule to.
+    :type file_path: str
+    :param field_delimiter: The delimiter to use between fields in the exported file.
+    :type field_delimiter: str, optional
+    :param export_title: Whether to include the schedule title in the exported file.
+    :type export_title: bool, optional
+    :param export_column_headers: The type of column headers to include in the exported file.
+    :type export_column_headers: Autodesk.Revit.DB.ExportColumnHeaders, optional
+    :param export_text_qualifier: The text qualifier to use in the exported file.
+    :type export_text_qualifier: Autodesk.Revit.DB.ExportTextQualifier, optional
     
     :return: Result object containing success status and messages.
     :rtype: duHast.Utilities.Objects.result.Result
     """
-    
+
     return_value = Result()
 
     try:
+
+        # check if the schedule is a view schedule instance
+        if not isinstance(schedule, ViewSchedule):
+            return_value.update_sep(False, "Provided schedule is not a valid ViewSchedule instance. Got {} instead.".format(type(schedule).__name__))
+            return return_value
+
+        # check if a file path was provided
+        if not file_path:
+            return_value.update_sep(False, "No file path provided for schedule export.")
+            return return_value
+        
+        # check if file_path is a string
+        if not isinstance(file_path, str):
+            return_value.update_sep(False, "File path must be a string. Got {} instead.".format(type(file_path).__name__))
+            return return_value
+
+
+        # set up an schedule export options object with default values
+        export_options = ViewScheduleExportOptions()
+        export_options.FieldDelimiter = field_delimiter
+        export_options.Title = export_title
+        export_options.TextQualifier = export_text_qualifier
+        export_options.ColumnHeaders = export_column_headers
+
+        # get directory from file_path
+        directory_path = get_directory_path_from_file_path(file_path)
 
         # check if the directory exists
-        if directory_exists(directory_path) is False:
-            return_value.update_sep(False, "Directory does not exist: {directory_path}".format(directory_path=directory_path))
+        if not directory_exists(directory_path):
+            return_value.update_sep(False, "Directory does not exist: {directory}".format(directory=directory_path))
             return return_value
 
-        # get all sheet schedules in doc
-        all_schedules = get_all_sheet_schedules(doc)
+        # get the file name from the file_path
+        file_name = get_file_name_without_ext(file_path)
+        # add the extension back in
+        file_extension = get_file_extension(file_path)
+        file_name += file_extension
 
-        # check if there are any schedules to export
-        if len(all_schedules) == 0:
-            return_value.update_sep(False, "No sheet schedules in document.")
-            return return_value
-        
-        # check if schedules need to be filtered ( export only if sheet number field is not hidden)
-        if export_if_number_is_hidden is False:
-            # filter schedules to only those that have the sheet number field visible
-            all_schedules = filter_schedules_by_sheet_number_field(all_schedules)
+        # export the schedule to a file
+        schedule.Export(directory_path, file_name, export_options)
 
-        # iterate through all schedules and export them to file
-        for schedule in all_schedules:
-            # get the file name from the schedule name
-            file_name = schedule.Name.replace(" ", "_") + ".csv"
-            file_path = os.path.join(directory_path, file_name)
-            
-            # export the schedule to file
-            export_result = export_schedule_to_file(schedule, file_path)
-            
-            return_value.update(export_result)
-
+        return_value.append_message( "Schedule exported successfully to: {file}".format(file=file_path))
         return return_value
-    
+
     except Exception as e:
-        return_value.update_sep(False, "Failed to export schedules to file: {e}".format(e=e))
-        return return_value
-
-
-def export_all_sheet_schedules_and_read_data_back(doc, export_if_number_is_hidden=False):
-    """
-    Exports all sheet schedules to file and reads the data back into a dictionary.
-
-    :param export_if_number_is_hidden: Whether to export schedules even if the number is hidden.
-    :type export_if_number_is_hidden: bool, optional
-
-    :return: Result object containing success status, messages, and data.
-    :rtype: duHast.Utilities.Objects.result.Result
-    """
-
-    return_value = Result()
-
-    try:
-        # set up a temporary directory to export the schedules to
-        directory_path = create_temp_directory()
-        
-        # export all sheet schedules to file
-        export_result = export_all_sheet_schedules_to_file(
-            doc=doc, 
-            directory_path=directory_path, 
-            export_if_number_is_hidden=export_if_number_is_hidden
-        )
-        
-        return_value.update(export_result)
-
-        if not return_value.status:
-            return return_value
-
-        # get all files in temp directory
-        if directory_exists(directory_path) is False:
-            return_value.update_sep(False, "Temporary directory does not exist: {directory_path}".format(directory_path=directory_path))
-            return return_value
-        
-        # get all files in the directory with the specified filters
-        files_found = get_files_single_directory(directory_path, file_prefix="", file_suffix="", file_extension=".csv")
-
-        # check if any files were found
-        if len(files_found) == 0:
-            return_value.update_sep(False, "No files found in temporary directory: {directory_path}".format(directory_path=directory_path))
-            
-            # delete the temp directory
-            delete_flag = directory_delete(directory_path)
-            return_value.append_message("Deleted temporary directory: {directory_path} with status {flag}".format(directory_path=directory_path, flag=delete_flag))
-
-            return return_value
-        
-        # log the number of files found
-        return_value.append_message("Found {num_files} files in temporary directory: {directory_path}".format(num_files=len(files_found), directory_path=directory_path))
-        
-        # read the data back into a dictionary
-        data = {}
-
-        for file_path in files_found:
-            # attempt to read the file
-            file_data_result = read_from_delimited_text_file(file_path)
-
-            # check if the file was read successfully
-            if file_data_result.status is False:
-                return_value.update_sep(False, "Failed to read data from file: {file_path}".format(file_path=file_path))
-                continue
-
-            else:
-                # log the success message
-                return_value.append_message("Successfully read data from file: {file_path}".format(file_path=file_path))
-                # store the data in the dictionary with the file name as the key
-                file_name = get_file_name_without_ext(file_path)
-
-                data[file_name] = file_data_result.result
-
-        # delete the temporary directory
-        delete_flag = directory_delete(directory_path)
-        return_value.append_message("Deleted temporary directory: {directory_path} with status {flag}".format(directory_path=directory_path, flag=delete_flag))
-
-        # add data read to return value
-        return_value.result.append(data)
-        return return_value
-    
-    except Exception as e:
-        return_value.update_sep(False, "Failed to export and read data back: {e}".format(e=e))
+        return_value.update_sep(False, "Failed to export schedule to file: {e}".format(e=e))
         return return_value

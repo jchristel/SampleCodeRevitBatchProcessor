8b8928b89299f8d7b071ce3f9a17a4e278728633
jchristel <jan.r.christel@gmail.com>
2025-08-17 19:14:57 +1000

    Create MultiParameterConverter.cs

---
+++ VS/duHastUtils/Utils/WPF/Converters/MultiParameterConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace duHastNet.Utils.WPF.Converters
+{
+    // <summary>
+    /// Converter that combines multiple values into an array for command parameters
+    /// </summary>
+    public class MultiParameterConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Return all the values as an array
+            return values?.Clone();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

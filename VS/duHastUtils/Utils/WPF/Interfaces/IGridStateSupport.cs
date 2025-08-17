42e56057285e5863255a76d5b58099193184293d
jchristel <jan.r.christel@gmail.com>
2025-08-17 19:14:50 +1000

    State management added to Utils library

---
+++ VS/duHastUtils/Utils/WPF/Interfaces/IGridStateSupport.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace duHastNet.Utils.WPF.Interfaces
+{
+    // <summary>
+    /// Interface for ViewModels that support grid state management
+    /// </summary>
+    public interface IGridStateSupport
+    {
+        /// <summary>
+        /// Whether state management is enabled for this ViewModel
+        /// </summary>
+        bool StateManagementEnabled { get; }
+
+        /// <summary>
+        /// Creates a state object representing the current state of the ViewModel's grid
+        /// </summary>
+        /// <returns>Current grid state, or null if state cannot be captured</returns>
+        IGridState CreateStateFromViewModel();
+
+        /// <summary>
+        /// Applies a saved state to the ViewModel's grid
+        /// </summary>
+        /// <param name="state">The state to apply</param>
+        /// <returns>True if state was successfully applied</returns>
+        bool ApplyStateToViewModel(IGridState state);
+
+        /// <summary>
+        /// Gets a unique identifier for this ViewModel's grid state
+        /// This can be used to distinguish between different grid configurations
+        /// </summary>
+        /// <returns>Unique state identifier</returns>
+        string GetGridStateId();
+    }
+}
---
+++ VS/duHastUtils/Utils/WPF/States/GridState.cs
@@ -0,0 +1,74 @@
+﻿
+using duHastNet.Utils.WPF.Interfaces;
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace duHastNet.Utils.WPF.States
+{
+    public class GridState : IGridState
+    {
+        public string GridId { get; set; }
+        public string StateName { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastModified { get; set; }
+        public string Version { get; set; } = "1.0";
+        public Dictionary<string, object> Metadata { get; set; } = new Dictionary<string, object>();
+
+        public string GetSummary()
+        {
+            return $"Grid: {GridId}, State: {StateName}";
+        }
+
+        public IGridState Clone()
+        {
+            var serialized = Serialize();
+            var clone = new GridState();
+            clone.Deserialize(serialized);
+            return clone;
+        }
+
+        public string Serialize()
+        {
+            try
+            {
+                return JsonConvert.SerializeObject(this);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public bool Deserialize(string serializedState)
+        {
+            try
+            {
+                var deserialized = JsonConvert.DeserializeObject<GridState>(serializedState);
+                if (deserialized != null)
+                {
+                    GridId = deserialized.GridId;
+                    StateName = deserialized.StateName;
+                    CreatedDate = deserialized.CreatedDate;
+                    LastModified = deserialized.LastModified;
+                    Version = deserialized.Version;
+                    Metadata = deserialized.Metadata;
+                    return true;
+                }
+            }
+            catch
+            {
+                // Ignore errors
+            }
+            return false;
+        }
+
+        public List<string> Validate()
+        {
+            var errors = new List<string>();
+            if (string.IsNullOrEmpty(GridId))
+                errors.Add("GridId is required");
+            return errors;
+        }
+    }
+}

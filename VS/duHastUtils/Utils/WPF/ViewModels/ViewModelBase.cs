87f0684ba19b9a7e8802bedc9a89ed0deea00f33
jchristel <jan.r.christel@gmail.com>
2025-08-20 21:12:28 +1000

    state management improvement
    # Conflicts:
    #	VS/duHastUtils/Utils/WPF/ViewModels/ViewModelBase.cs

--- VS/duHastUtils/Utils/WPF/ViewModels/ViewModelBase.cs
+++ VS/duHastUtils/Utils/WPF/ViewModels/ViewModelBase.cs
@@ -1,73 +1,58 @@
-﻿//
-//License:
-//
-//
-// Revit Batch Processor Sample Code
-//
-// BSD License
-// Copyright 2025, Jan Christel
-// All rights reserved.
-
-// Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
-
-// - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
-// - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
-// - Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
-//
-// This software is provided by the copyright holder "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed.
-// In no event shall the copyright holder be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits;
-// or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
-//
-//
-//
-
-
-using duHastNet.Utils.WPF.Interfaces;
+﻿using duHastNet.Utils.WPF.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 
-
-namespace duHastNet.Utils.WPF.ViewModels
-{
-    public class ViewModelBase : INotifyPropertyChanged, duHastNet.Utils.WPF.Interfaces.ICloseable
+    namespace duHastNet.Utils.WPF.ViewModels
     {
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        public void RaisePropertyChanged(string name)
-        {
-            OnPropertyChanged(name);
-        }
-
-
         /// <summary>
-        /// list of nested view models
+        /// Base class for ViewModels - simple and focused on core ViewModel functionality
         /// </summary>
-        protected List<ICloseable> _childViewModels = [];
-
-        protected void RegisterChild(ICloseable child)
+        public class ViewModelBase : INotifyPropertyChanged, ICloseable
         {
-            _childViewModels.Add(child);
-        }
-        public virtual void OnClosing()
-        {
-            // Override this method in derived classes to perform clean-up operations
+            #region INotifyPropertyChanged
 
-            // Close all children first
-            foreach (var child in _childViewModels)
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            protected void OnPropertyChanged(string propertyName)
             {
-                child.OnClosing();
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
             }
-            _childViewModels.Clear();
-        }
 
-        public virtual void Dispose()
-        {
+            public void RaisePropertyChanged(string name)
+            {
+                OnPropertyChanged(name);
+            }
+
+            #endregion
+
+            #region ICloseable
+
+            /// <summary>
+            /// list of nested view models
+            /// </summary>
+            protected List<ICloseable> _childViewModels = new List<ICloseable>();
+
+            protected void RegisterChild(ICloseable child)
+            {
+                _childViewModels.Add(child);
+            }
+
+            public virtual void OnClosing()
+            {
+                // Close all children first
+                foreach (var child in _childViewModels)
+                {
+                    child.OnClosing();
+                }
+                _childViewModels.Clear();
+            }
+
+            public virtual void Dispose()
+            {
+            }
+
+            #endregion
         }
     }
-}

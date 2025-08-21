e82d6854b12c2312d8e39a31a6237399a50cdcec
jchristel <jan.r.christel@gmail.com>
2025-08-21 22:17:25 +1000

    duHast dll updates

--- Samples/pyRevit/Extensions/duHast-2024.extension/duHast.tab/lib/export/export_pdf_dwg_settings.py
+++ Samples/pyRevit/Extensions/duHast-2024.extension/duHast.tab/lib/export/export_pdf_dwg_settings.py
@@ -37,7 +37,7 @@ from duHast.pyRevit.console_output import print_header, print_error
 
 from export import settings
 
-DEBUG = False
+DEBUG = True
 
 
 from Autodesk.Revit.DB import  BaseExportOptions

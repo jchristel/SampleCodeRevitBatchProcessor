028ad3800b29d42cf10f29456b95e0a5d62d2814
jchristel <jan.r.christel@gmail.com>
2025-08-31 21:56:38 +1000

    Update ParametersDataGridViewModel.cs
    namespace fix

--- VS/duHastRevitApplications/AtTheLibrary/ViewModels/ParametersDataGridViewModel.cs
+++ VS/duHastRevitApplications/AtTheLibrary/ViewModels/ParametersDataGridViewModel.cs
@@ -31,7 +31,7 @@ using System.Linq;
 
 namespace duHastNet.AtTheLibrary.ViewModels
 {
-    public class ParametersDataGridViewModel : duHastNet.UI.CustomControls.ViewModels.BaseDynamicGridViewModel<DynamicRowData>
+    public class ParametersDataGridViewModel : duHastNet.Utils.WPF.ViewModels.BaseDynamicGridViewModel<DynamicRowData>
     {
         private Models.RevitFamiliesDataModel RevitDataModel { get; set; }
 

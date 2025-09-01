193e301774d046b5aaafccab4860dabea6fa8f00
jchristel <jan.r.christel@gmail.com>
2025-09-01 19:57:52 +1000

    trying to make version numbering more centralised
    # Conflicts:
    #	VS/duHastUICustomControls/duHastUICustomControls/Themes/Generic.xaml

---
+++ VS/duHastUICustomControls/duHastUICustomControls/ResourceUriHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace duHastNet.UI.CustomControls
+{
+    public static class ResourceUriHelper
+    {
+        // Assembly info - change this one place when versioning
+        private const string ASSEMBLY_NAME = "duHastUICustomControls.23.0.0.4";
+
+        // Base URI pattern
+        private static string GetPackUri(string resourcePath)
+        {
+            return $"pack://application:,,,/{ASSEMBLY_NAME};component/{resourcePath}";
+        }
+
+        // Specific resource URIs
+        public static Uri ThreeWaySwitchStyle => new Uri(GetPackUri("ThreeWaySwitch/ThreeWaySwitchStyle.xaml"), UriKind.Absolute);
+        public static Uri DynamicDataGridStyle => new Uri(GetPackUri("CustomDataGrid/DynamicDataGridStyle.xaml"), UriKind.Absolute);
+        public static Uri CellEditorStyle => new Uri(GetPackUri("CellEditor/CellEditorStyle.xaml"), UriKind.Absolute);
+    }
+}

7a2b4e43aac8966ec5bea173698e14d8f5b73281
Jan Christel <jan.r.christel@gmail.com>
2025-09-17 13:12:30 +1000

    Create convert_data_to_filter_logic_filter.py

---
+++ src/duHast/Revit/Views/Utility/convert_data_to_filter_logic_filter.py
@@ -0,0 +1,59 @@
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+storage to revit api value provider conversion helper functions.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+#
+# License:
+#
+#
+# Revit Batch Processor Sample Code
+#
+# BSD License
+# Copyright 2025, Jan Christel
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+# - Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+# - Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# - Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+#
+# This software is provided by the copyright holder "as is" and any express or implied warranties, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose are disclaimed.
+# In no event shall the copyright holder be liable for any direct, indirect, incidental, special, exemplary, or consequential damages (including, but not limited to, procurement of substitute goods or services; loss of use, data, or profits;
+# or business interruption) however caused and on any theory of liability, whether in contract, strict liability, or tort (including negligence or otherwise) arising in any way out of the use of this software, even if advised of the possibility of such damage.
+#
+#
+#
+
+from Autodesk.Revit.DB import  (
+    LogicalAndFilter,
+    LogicalOrFilter,
+)
+
+
+# dictionary containing logical_filter_class types
+
+class_mapping = {
+    "LogicalAndFilter": LogicalAndFilter,
+    "LogicalOrFilter" :LogicalOrFilter,
+}
+
+
+def get_logical_filter_class(class_name):
+    """
+    Returns a logical filter class based on the provided class name.
+    None ignored or not found.
+
+    :param class_name: Name of the logical filter class to retrieve.
+    :type class_name: str
+
+    :return: The logical filter class if found, otherwise None.
+    :rtype: class or None
+    """
+    
+    return_value = None
+    if class_name and class_name in class_mapping:
+        logical_filter_class = class_mapping.get(class_name)
+        return logical_filter_class
+    return return_value

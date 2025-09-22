08a4e045266f88eab14db0eec8525177ae77dc90
jchristel <jan.r.christel@gmail.com>
2025-09-22 21:01:14 +1000

    added  iron python test extension
    started with module loading test

---
+++ test/pyRevit/test-2024.extension/test.tab/About.panel/About.pulldown/Icon8.pushbutton/script.py
@@ -0,0 +1,3 @@
+import webbrowser
+
+webbrowser.open('https://icons8.com/')  # Go to icon8.com
---
+++ test/pyRevit/test-2024.extension/test.tab/About.panel/About.pulldown/License.pushbutton/script.py
@@ -0,0 +1,3 @@
+import webbrowser
+
+webbrowser.open('https://github.com/jchristel/SampleCodeRevitBatchProcessor?tab=License-1-ov-file#readme')  # Go to github repo

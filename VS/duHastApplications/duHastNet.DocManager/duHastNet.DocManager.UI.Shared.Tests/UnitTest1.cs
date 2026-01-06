911f81fb3d68902b9be0254cc810d61bf38c3751
jchristel <jan.r.christel@gmail.com>
2026-01-06 11:08:35 +1100

    added ui test project

---
+++ VS/duHastApplications/duHastNet.DocManager/duHastNet.DocManager.UI.Shared.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace duHastNet.DocManager.UI.Shared.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}

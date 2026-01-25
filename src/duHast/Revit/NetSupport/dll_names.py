5c1e2e5e5e3741c9f155146a344d12f7f17ef50e
jchristel <jan.r.christel@gmail.com>
2026-01-25 19:37:46 +1100

    dll version number updates

--- src/duHast/Revit/NetSupport/dll_names.py
+++ src/duHast/Revit/NetSupport/dll_names.py
@@ -29,7 +29,7 @@ This is required since the dll's used have a version number in their file name.
 #
 # 
 
-DLL_VERSION = ".23.0.0.5"
+DLL_VERSION = ".23.0.0.6"
 
 # family reloader ui
 FAMILY_RELOADER_UI = "FamilyReloaderUI{}.dll".format(DLL_VERSION)

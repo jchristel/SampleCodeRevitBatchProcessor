a9c51de56d3576e5f9c9db34056b95390a14fe7d
jchristel <jan.r.christel@gmail.com>
2026-01-27 22:46:18 +1100

    Update ResourceUriHelper.cs
    fixed bad assembly name replacement

--- VS/duHastUICustomControls/duHastUICustomControls/ResourceUriHelper.cs
+++ VS/duHastUICustomControls/duHastUICustomControls/ResourceUriHelper.cs
@@ -5,7 +5,7 @@ namespace duHastNet.UI.CustomControls
     public static class ResourceUriHelper
     {
         // Assembly info - change this one place when versioning
-        private const string ASSEMBLY_NAME = "$223.0.0.6";
+        private const string ASSEMBLY_NAME = "duHastUICustomControls.23.0.0.6";
 
         // Base URI pattern
         private static string GetPackUri(string resourcePath)

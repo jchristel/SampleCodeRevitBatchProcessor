50ad7d46491fab017eea9bacfa62030e31c6d698
jchristel <jan.r.christel@gmail.com>
2026-01-31 18:10:47 +1100

    Update revisions_from_sheet_WIP.py
    incorporated None revision sequence instances
    Improved Sequence classes ( stored index of revision on sheet )
    started sequence base class
    
    TODO: consider renaming these classes to RevisionBaseStorage, RevisionNumericStorage, RevisionAlphaNumericStorage

--- src/duHast/Revit/Revisions/revisions_from_sheet_WIP.py
+++ src/duHast/Revit/Revisions/revisions_from_sheet_WIP.py
@@ -10,9 +10,57 @@ def get_all_sheets(doc):
     collector_views = FilteredElementCollector(doc).OfClass(ViewSheet)
     return collector_views
 
+class SequenceBase():
+	"""
+	Revision sequence utility base class
+	"""
+	
+	def __init__(self, revit_id,revit_revision, revision_index_on_sheet):
+		
+		self._revit_id=revit_id
+		self._revit_revision = revit_revision
+		self._revision_index_on_sheet = revision_index_on_sheet
+		
+	@property
+	def revit_id(self):
+		return self._revit_id
+
+	@property
+	def revit_revision(self):
+		return self._revit_revision
+		
+	@property
+	def revision_index_on_sheet(self):
+		return self._revision_index_on_sheet
+	
+	def get_revision(self, revision_index_on_sheet):
+		return None
+
+class SequenceNone():
+	def __init__(self, revit_id,revit_revision, revision_index_on_sheet):
+		
+		self._revit_id=revit_id
+		self._revit_revision = revit_revision
+		self._revision_index_on_sheet = revision_index_on_sheet
+		
+	@property
+	def revit_id(self):
+		return self._revit_id
+
+	@property
+	def revit_revision(self):
+		return self._revit_revision
+		
+	@property
+	def revision_index_on_sheet(self):
+		return self._revision_index_on_sheet
+	
+	def get_revision(self, revision_index_on_sheet):
+		return None
+
 
 class SequenceNumeric():
-	def __init__(self, revit_id,revit_revision, prefix,suffix,start_number,min_digits):
+	def __init__(self, revit_id,revit_revision, prefix,suffix,start_number,min_digits,revision_index_on_sheet):
 		
 		self._revit_id=revit_id
 		self._revit_revision = revit_revision
@@ -20,6 +68,7 @@ class SequenceNumeric():
 		self._suffix=suffix
 		self._start_number=start_number
 		self._min_digits=min_digits
+		self._revision_index_on_sheet = revision_index_on_sheet
 
 	@property
 	def revit_id(self):
@@ -44,6 +93,10 @@ class SequenceNumeric():
 	@property
 	def min_digits(self):
 		return self._min_digits
+	
+	@property
+	def revision_index_on_sheet(self):
+		return self._revision_index_on_sheet
 		
 	def get_revision(self, revision_index_on_sheet):
 		
@@ -57,13 +110,14 @@ class SequenceNumeric():
 
 
 class SequenceAlphaNumeric():
-	def __init__(self, revit_id, revit_revision,prefix,suffix, sequence):
+	def __init__(self, revit_id, revit_revision,prefix,suffix, sequence,revision_index_on_sheet):
 		
 		self._revit_id=revit_id
 		self._revit_revision = revit_revision
 		self._prefix=prefix
 		self._suffix=suffix
 		self._sequence = sequence
+		self._revision_index_on_sheet = revision_index_on_sheet
 
 	@property
 	def revit_id(self):
@@ -84,6 +138,10 @@ class SequenceAlphaNumeric():
 	@property
 	def sequence(self):
 		return self._sequence
+	
+	@property
+	def revision_index_on_sheet(self):
+		return self._revision_index_on_sheet
 	
 	def index_to_revision(self, index):
 	    """
@@ -141,7 +199,7 @@ class SequenceAlphaNumeric():
 		return "{}{}{}".format(self._prefix,rev,self._suffix)	
 
 
-def get_sequence_numeric(revision, revision_sequence):
+def get_sequence_numeric(revision, revision_sequence,revision_index_on_sheet):
 	settings = revision_sequence.GetNumericRevisionSettings()
 	seq_number = SequenceNumeric(
 		revision_sequence.Id.Value, 
@@ -149,12 +207,13 @@ def get_sequence_numeric(revision, revision_sequence):
 		settings.Prefix,
 		settings.Suffix,
 		settings.StartNumber,
-		settings.MinimumDigits
+		settings.MinimumDigits,
+		revision_index_on_sheet
 	)
 	return seq_number
 	
 
-def get_sequence_alphanumeric(revision, revision_sequence):
+def get_sequence_alphanumeric(revision, revision_sequence,revision_index_on_sheet):
 	settings = revision_sequence.GetAlphanumericRevisionSettings()
 	sequence = settings.GetSequence()
 	seq_number=SequenceAlphaNumeric(
@@ -162,10 +221,15 @@ def get_sequence_alphanumeric(revision, revision_sequence):
 		revision,
 		settings.Prefix,
 		settings.Suffix,
-		sequence
+		sequence,
+		revision_index_on_sheet
 	)
 	return seq_number
-	
+
+def get_sequence_none(revision, revision_index_on_sheet):
+	seq_number=SequenceNone(ElementId.InvalidElementId, revision,revision_index_on_sheet)
+	return seq_number
+
 
 def build_revision_data(rev_dictionary, number_of_revs_on_sheet):
 	
@@ -175,23 +239,29 @@ def build_revision_data(rev_dictionary, number_of_revs_on_sheet):
 	# start getting data in order of revisions on sheet
 	index_counter = 0
 	
+	# loop over all revisions on a sheet by index
 	for i in range(0, number_of_revs_on_sheet):
 		print ("...checking revision on sheet of index: {}".format(i))
 		
+		# set default flag to false
 		found_match = False
 		
 		# loop over items and build a revision data
 		for sequence_id, revision_data_list in rev_dictionary.items():
 			
-			
-			seq_by_revision_type = 0
+			# setup counter by type
+			seq_by_revision_type_counter = 0
 			for rev_data_entry in revision_data_list:
-				print ("rev index on sheet: {} overal index: {} type: {} ".format(rev_data_entry[1], i, type(rev_data_entry[1])))
-				if rev_data_entry[1] == i:
+				
+				# check if this revision index, stored in a tuple at index 1, is matching the current sheet index 
+				if rev_data_entry.revision_index_on_sheet == i:
 					
-					# get the local index...
-					revision_value = rev_data_entry[0].get_revision(seq_by_revision_type)
-					revision_revit_element = rev_data_entry[0].revit_revision
+					# get the actual revision value from the first value in the tuple which is a 
+					# helper class inststance stored in tuple at index 0
+					revision_value = rev_data_entry.get_revision(seq_by_revision_type_counter)
+					
+					# get the revit revision object instance
+					revision_revit_element = rev_data_entry.revit_revision
 					
 					print ("...found match. Revision value: {}".format(revision_value))
 					
@@ -202,7 +272,8 @@ def build_revision_data(rev_dictionary, number_of_revs_on_sheet):
 					found_match = True
 					# time to move to next
 					break
-				seq_by_revision_type = seq_by_revision_type +1
+					
+				seq_by_revision_type_counter = seq_by_revision_type_counter + 1
 			
 			if found_match:
 				break
@@ -212,17 +283,20 @@ def build_revision_data(rev_dictionary, number_of_revs_on_sheet):
 		
 
 def get_revisions(doc, sheet):
+	"""
+	Generates a dictionary where the key is the revision sequence id as an int and the value is a list of duHast sequence utility
+	classes each representing a revision on the sheet belonging to the Revit revision sequence
+	"""
 	revision_ids_on_sheet = sheet.GetAllRevisionIds()
 	
 	# check if any revisions on sheet
 	if (revision_ids_on_sheet.Count == 0):
 		return None
-	print(type(revision_ids_on_sheet))
 	
 	revision_data = []
 	
-	#revision index counter
-	rev_index = 0
+	# revision index counter
+	rev_index_on_sheet = 0
 	
 	#revision data
 	revision_data = {}
@@ -230,24 +304,26 @@ def get_revisions(doc, sheet):
 	for revision_id in revision_ids_on_sheet:
 		revision = doc.GetElement(revision_id)
 		revision_sequence_id = revision.RevisionNumberingSequenceId
-		revision_sequence = doc.GetElement(revision_sequence_id)
-		
-		print(type(revision_sequence))
 		
 		# a class generating the actual revision number
 		rev_sequence_generator = None
 		
-		if (revision_sequence.NumberType ==  RevisionNumberType.Numeric):
-			print ("numeric")
-			rev_sequence_generator = get_sequence_numeric(revision,revision_sequence)
-		elif (revision_sequence.NumberType ==  RevisionNumberType.Alphanumeric):
-			print ("alpha")
-			rev_sequence_generator = get_sequence_alphanumeric(revision,revision_sequence)
+		# check if this is a None revision
+		if revision_sequence_id == ElementId.InvalidElementId:
+			rev_sequence_generator = get_sequence_none(revision,rev_index_on_sheet)
 		else:
-			print("Not supported")
-
-		print(rev_sequence_generator)
+			
+			# get the revision sequence
+			revision_sequence = doc.GetElement(revision_sequence_id)
 		
+			# check what type of sequence and get helper class accordingly
+			if (revision_sequence.NumberType ==  RevisionNumberType.Numeric):
+				rev_sequence_generator = get_sequence_numeric(revision,revision_sequence,rev_index_on_sheet)
+			elif (revision_sequence.NumberType ==  RevisionNumberType.Alphanumeric):
+				rev_sequence_generator = get_sequence_alphanumeric(revision,revision_sequence,rev_index_on_sheet)
+			else:
+				print("Not supported")
+
 		# move to next if there is no revision
 		if (rev_sequence_generator == None):
 			continue
@@ -257,11 +333,10 @@ def get_revisions(doc, sheet):
 			revision_data[rev_sequence_generator.revit_id] = []
 		
 		# add the generator and index to the dictionary
-		revision_data[rev_sequence_generator.revit_id].append((rev_sequence_generator, rev_index))
-		
+		revision_data[rev_sequence_generator.revit_id].append(rev_sequence_generator)
 		
 		# increase revision index counter
-		rev_index = rev_index + 1
+		rev_index_on_sheet = rev_index_on_sheet + 1
 		
 	# build revision data
 	formatted_data = build_revision_data(revision_data, revision_ids_on_sheet.Count)

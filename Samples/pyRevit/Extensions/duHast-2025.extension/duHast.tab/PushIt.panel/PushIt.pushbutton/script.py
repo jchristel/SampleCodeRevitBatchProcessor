29a8bf2be164411020d54cf0141242eb273370c3
Jan Christel <jan.r.christel@gmail.com>
2026-02-06 10:18:52 +1100

    updating pushit
    to work with new dll loader (on demand)

---
+++ Samples/pyRevit/Extensions/duHast-2025.extension/duHast.tab/PushIt.panel/PushIt.pushbutton/script.py
@@ -0,0 +1,20 @@
+"""
+An extension used to place revit rooms in the current Revit model based on pushIt rooms.
+
+
+"""
+
+
+# pyrevit stuff
+from pyrevit import revit, script, forms
+logger = script.get_logger()
+output = script.get_output()
+
+# get the revit document
+doc = revit.doc
+uiapp = __revit__
+
+# create default catalogue type in family
+from pushIt_associated.push_it import push_it_entry
+
+push_it_entry(doc=doc, output=output, forms=forms)

d1d50022c5d5c7ac86277b03662d290cc9733653
jchristel <jan.r.christel@gmail.com>
2026-03-09 21:15:49 +1100

    dll updates

--- VS/_MiscScripts/copy-project-files.ps1
+++ VS/_MiscScripts/copy-project-files.ps1
@@ -93,14 +93,14 @@ function Copy-ProjectFiles {
 # Write-Host "`nCopying files from Project 6..." -ForegroundColor White
 # Copy-ProjectFiles -sourcePath $project6Path -projectName "Project6" -destination $tempFolder
 
-#Write-Host "`nCopying files from Project 7..." -ForegroundColor White
-#Copy-ProjectFiles -sourcePath $project7Path -projectName "Project7" -destination $tempFolder
+Write-Host "`nCopying files from Project 7..." -ForegroundColor White
+Copy-ProjectFiles -sourcePath $project7Path -projectName "Project7" -destination $tempFolder
 
 #Write-Host "`nCopying files from Project 8..." -ForegroundColor White
 #Copy-ProjectFiles -sourcePath $project8Path -projectName "Project8" -destination $tempFolder
 
-Write-Host "`nCopying files from Project 9..." -ForegroundColor White
-Copy-ProjectFiles -sourcePath $project9Path -projectName "Project9" -destination $tempFolder
+#Write-Host "`nCopying files from Project 9..." -ForegroundColor White
+#Copy-ProjectFiles -sourcePath $project9Path -projectName "Project9" -destination $tempFolder
 
 Write-Host "`nAll files copied successfully to: $tempFolder" -ForegroundColor Green
 Write-Host "Opening destination folder..." -ForegroundColor Gray

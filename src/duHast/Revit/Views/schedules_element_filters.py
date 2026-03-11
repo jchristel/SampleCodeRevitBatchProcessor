c3645d0ce2303f912a4bd0f7c393a22f812a56ea
Jan Christel <jan.r.christel@gmail.com>
2026-03-11 17:17:17 +1100

    Update schedules_element_filters.py
    added get_schedule_filters_and_index_by_field_name

--- src/duHast/Revit/Views/schedules_element_filters.py
+++ src/duHast/Revit/Views/schedules_element_filters.py
@@ -56,3 +56,31 @@ def get_schedule_filters_by_field_name(schedule, field_name):
                 filters_for_field.append(filter)
     
     return filters_for_field
+
+
+def get_schedule_filters_and_index_by_field_name(schedule, field_name):
+    """
+    Gets the filters of a schedule by the field name.
+
+    :param schedule: Revit schedule view.
+    :type schedule: Autodesk.Revit.DB.ViewSchedule
+    :param field_name: Name of the field to get the filters for.
+    :type field_name: str
+
+    :return:
+        List of tuples in format(filter, index) that are applied to the schedule for the specified field name. If no filters are found, an empty list is returned.
+    :rtype: list of Autodesk.Revit.DB.ScheduleFilter
+    """
+
+    filters_for_field = []
+    if schedule.Definition.GetFilterCount() > 0:
+        index = 0
+        filters = get_schedule_filters(schedule)
+        for filter in filters:
+            schedule_field = schedule.Definition.GetField(filter.FieldId)
+            schedule_filed_name = schedule_field.GetName()
+            if schedule_filed_name == field_name:
+                filters_for_field.append((filter, index))
+            index += 1
+    
+    return filters_for_field

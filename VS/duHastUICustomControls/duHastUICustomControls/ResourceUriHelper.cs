4fc4344c0a99d62471fcb38fa58eb88a8fe070b3
jchristel <jan.r.christel@gmail.com>
2026-05-17 14:30:29 +1000

    dll version update
    to 25.0.1.2

--- VS/duHastUICustomControls/duHastUICustomControls/ResourceUriHelper.cs
+++ VS/duHastUICustomControls/duHastUICustomControls/ResourceUriHelper.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 
 namespace duHastNet.UI.CustomControls
 {
@@ -6,7 +6,7 @@ namespace duHastNet.UI.CustomControls
     {
         // Assembly info - change VERSION when versioning
         private const string ASSEMBLY_BASE = "duHastUICustomControls";
-        private const string VERSION = "25.0.1.1";
+        private const string VERSION = "25.0.1.2";
         private const string ASSEMBLY_NAME = ASSEMBLY_BASE + "." + VERSION;
 
         // Base URI pattern
